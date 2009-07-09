708fcd3293655c5daad2de10dcdac88013139e88
Philippe Deniel <philippe.deniel@cea.fr>
2009-07-09 13:44:37 +0200

    Web updated
    Signed-off-by: Philippe Deniel <philippe.deniel@cea.fr>
    
    Signed-off-by: Philippe Deniel <philippe.deniel@cea.fr>

--- web/index.php
+++ web/index.php
@@ -98,10 +98,10 @@
                 <td>
 		<table width="80%" border="0" align="center" cellpadding="0" cellspacing="0">
 		  <tr>
-                    <td class="date">Jun 22, 2009</td>
+                    <td class="date">Jul  9, 2009</td>
               	  </tr>
                   <tr>
-                    <td height="50"><a href="http://sourceforge.net/project/showfiles.php?group_id=221673&package_id=267784&release_id=693441" class="smallred">v 0.99.55 is available</a></td>
+                    <td height="50"><a href="http://sourceforge.net/projects/nfs-ganesha/files/" class="smallred">v 0.99.56 is available</a></td>
                   </tr>
 		  <tr>
                     <td class="date">January 2006</td>
