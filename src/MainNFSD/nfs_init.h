740ce591be8659b190ef993e7c4ef4d9715de6e9
Philippe Deniel <philippe.deniel@cea.fr>
2009-07-17 16:15:20 +0200

    Added configuration dump on stdout (in progress)
    Signed-off-by: Philippe Deniel <philippe.deniel@cea.fr>

--- src/MainNFSD/nfs_init.h
+++ src/MainNFSD/nfs_init.h
@@ -96,6 +96,7 @@ extern unsigned int force_flush_by_signal ;
 typedef struct __nfs_start_info
 {
     int                                 flush_datacache_mode;
+    int                                 dump_default_config;
     unsigned int                        nb_flush_threads;
     cache_content_flush_behaviour_t     flush_behaviour;
     int                                 lw_mark_trigger ;
