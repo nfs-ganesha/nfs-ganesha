0e44ec78e4e89eeba3c6d287ca534ef8d2713578
Thomas LEIBOVICI - CEA/DAM <leibovi@petrea3.(none)>
2010-01-07 13:38:26 +0100

    Port to HPSS 7.3

--- src/FSAL/FSAL_HPSS/HPSSclapiExt/api_open.c
+++ src/FSAL/FSAL_HPSS/HPSSclapiExt/api_open.c
@@ -1419,6 +1419,7 @@ int                     LoopCount)      /* IN  - Recursion Counter */
                                     HintsPri,
                                     HintsOut,
                                     AttrsOut,
+                                    HandleOut,
                                     ++LoopCount);
                
 
