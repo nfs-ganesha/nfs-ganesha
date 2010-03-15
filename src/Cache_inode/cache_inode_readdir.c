a2ba18cd5adfbeba659fedcd512f691668fbc8e5
Philippe Deniel <philippe.deniel@cea.fr>
2010-03-15 14:17:39 +0100

    Patches applyed to the repository
    Signed-off-by: Philippe Deniel <philippe.deniel@cea.fr>

--- src/Cache_inode/cache_inode_readdir.c
+++ src/Cache_inode/cache_inode_readdir.c
@@ -1326,8 +1326,7 @@ cache_inode_status_t cache_inode_readdir( cache_entry_t           * dir_pentry,
               if( pentry_to_read->object.dir_begin.end_of_dir == END_OF_DIR )
                 {
                   /* End of dir is reached */
-                  if( pnbfound == 0 )
-                    *peod_met = END_OF_DIR ;
+                  *peod_met = END_OF_DIR ;
 
                   *pstatus  = CACHE_INODE_SUCCESS ;
                   V_r( &dir_pentry->lock ) ;
@@ -1346,8 +1345,7 @@ cache_inode_status_t cache_inode_readdir( cache_entry_t           * dir_pentry,
               if( pentry_to_read->object.dir_cont.end_of_dir == END_OF_DIR )
                 {
                   /* End of dir is reached */
-                  if( pnbfound == 0 )
-                    *peod_met = END_OF_DIR ;
+                  *peod_met = END_OF_DIR ;
 
                   *pstatus  = CACHE_INODE_SUCCESS ;
                   V_r( &dir_pentry->lock ) ;
