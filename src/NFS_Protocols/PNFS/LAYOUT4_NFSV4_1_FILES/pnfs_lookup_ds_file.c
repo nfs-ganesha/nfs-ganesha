3bd9f5c9638d78eca4b49696dd2c033a4aaec335
Philippe Deniel <philippe.deniel@cea.fr>
2010-04-06 10:13:29 +0200

    The file pnfs_lookup_ds_file.c has been added to the repository
    Signed-off-by: Philippe Deniel <philippe.deniel@cea.fr>

---
+++ src/NFS_Protocols/PNFS/LAYOUT4_NFSV4_1_FILES/pnfs_lookup_ds_file.c
@@ -0,0 +1,100 @@
+/*
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ */
+
+/**
+ *
+ * \file    pnfs_lookup_ds_file.c
+ * \brief   pNFS objects lookup functions.
+ *
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef _SOLARIS
+#include "solaris_port.h"
+#endif                          /* _SOLARIS */
+
+#include <string.h>
+#include <signal.h>
+
+#ifdef _USE_GSSRPC
+#include <gssrpc/rpc.h>
+#else
+#include <rpc/rpc.h>
+#endif
+
+#include "stuff_alloc.h"
+#include "nfs_proto_functions.h"
+
+#include "PNFS/LAYOUT4_NFSV4_1_FILES/pnfs_layout4_nfsv4_1_files.h"
+#include "pnfs_nfsv41_macros.h"
+
+#define PNFS_LAYOUTFILE_NB_OP_LOOKUP_DS_FILE 4
+
+#define PNFS_LAYOUTFILE_CREATE_VAL_BUFFER  1024
+
+int pnfs_create_ds_file( pnfs_client_t  * pnfsclient, 
+			 fattr4_fileid    fileid,
+                         pnfs_ds_file_t * pfile ) 
+{
+  COMPOUND4args argnfs4;
+  COMPOUND4res resnfs4;
+  struct timeval timeout = { 25, 0 };
+  nfs_argop4 argoparray_open_ds_file[PNFS_LAYOUTFILE_NB_OP_LOOKUP_DS_FILE];
+  nfs_resop4 resoparray_open_ds_file[PNFS_LAYOUTFILE_NB_OP_LOOKUP_DS_FILE];
+  component4 name;
+  char nameval[MAXNAMLEN];
+  char filename[MAXNAMLEN] ;
+
+#define PNFS_LAYOUTFILE_LOOKUP_IDX_OP_PUTFH    0
+#define PNFS_LAYOUTFILE_LOOKUP_IDX_OP_SEQUENCE 1
+#define PNFS_LAYOUTFILE_LOOKUP_IDX_OP_LOOKUP     2
+#define PNFS_LAYOUTFILE_LOOKUP_IDX_OP_GETFH    3 
+
+  if( !pnfsclient || !pfile )
+    return NFS4ERR_SERVERFAULT ;
+
+  /* Step 1 OP4_OPEN as OPEN4_CREATE */
+  argnfs4.argarray.argarray_val = argoparray_open_ds_file;
+  resnfs4.resarray.resarray_val = resoparray_open_ds_file;
+  argnfs4.minorversion = 1;
+
+  /* argnfs4.tag.utf8string_val = "GANESHA NFSv4 Proxy: Mkdir" ; */
+  argnfs4.tag.utf8string_val = NULL;
+  argnfs4.tag.utf8string_len = 0;
+  argnfs4.argarray.argarray_len = 0;
+
+  name.utf8string_val = nameval;
+  name.utf8string_len = 0;
+
+  snprintf( filename, MAXNAMLEN, "fileid=%llu", (unsigned long long)fileid ) ;
+
+  resnfs4.resarray.resarray_val[PNFS_LAYOUTFILE_LOOKUP_IDX_OP_GETFH].nfs_resop4_u.opgetfh.GETFH4res_u.resok4.object.nfs_fh4_val = (char *)Mem_Alloc( PNFS_LAYOUTFILE_FILEHANDLE_MAX_LEN );
+
+  if (str2utf8(filename, &name) == -1)
+        return NFS4ERR_SERVERFAULT;
+
+  COMPOUNDV41_ARG_ADD_OP_SEQUENCE( argnfs4, pnfsclient->session, pnfsclient->sequence ) ;
+  pnfsclient->sequence += 1 ; /* In all cases, failure or not, increment the sequence counter */
+  COMPOUNDV41_ARG_ADD_OP_PUTFH(argnfs4, pnfsclient->ds_rootfh[0] );
+  COMPOUNDV41_ARG_ADD_OP_LOOKUP(argnfs4, name ) ;
+  COMPOUNDV41_ARG_ADD_OP_GETFH( argnfs4 ) ;
+
+  /* Call the NFSv4 function */
+  if (COMPOUNDV41_EXECUTE_SIMPLE(pnfsclient, argnfs4, resnfs4) != RPC_SUCCESS)
+    {
+      return NFS4ERR_IO;        /* @todo: For wanting of something more appropriate */
+    }
+
+  if (resnfs4.status != NFS4_OK)
+    {
+      return resnfs4.status;
+    }
+
+  return resnfs4.status;
+} /* pnfs_create_ds_file */ 
+
---
+++ src/include/PNFS/LAYOUT4_NFSV4_1_FILES/pnfs_layout4_nfsv4_1_files.h
@@ -0,0 +1,177 @@
+/*
+ *
+ *
+ * Copyright CEA/DAM/DIF  (2010)
+ * contributeur : Philippe DENIEL   philippe.deniel@cea.fr
+ *                Thomas LEIBOVICI  thomas.leibovici@cea.fr
+ *
+ *
+ * Ce logiciel est un serveur implementant le protocole NFS.
+ *
+ * Ce logiciel est régi par la licence CeCILL soumise au droit français et
+ * respectant les principes de diffusion des logiciels libres. Vous pouvez
+ * utiliser, modifier et/ou redistribuer ce programme sous les conditions
+ * de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
+ * sur le site "http://www.cecill.info".
+ *
+ * En contrepartie de l'accessibilité au code source et des droits de copie,
+ * de modification et de redistribution accordés par cette licence, il n'est
+ * offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+ * seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+ * titulaire des droits patrimoniaux et les concédants successifs.
+ *
+ * A cet égard  l'attention de l'utilisateur est attirée sur les risques
+ * associés au chargement,  à l'utilisation,  à la modification et/ou au
+ * développement et à la reproduction du logiciel par l'utilisateur étant
+ * donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+ * manipuler et qui le réserve donc à des développeurs et des professionnels
+ * avertis possédant  des  connaissances  informatiques approfondies.  Les
+ * utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+ * logiciel à leurs besoins dans des conditions permettant d'assurer la
+ * sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+ * à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+ *
+ * Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+ * pris connaissance de la licence CeCILL, et que vous en avez accepté les
+ * termes.
+ *
+ * ---------------------
+ *
+ * Copyright CEA/DAM/DIF (2010)
+ *  Contributor: Philippe DENIEL  philippe.deniel@cea.fr
+ *               Thomas LEIBOVICI thomas.leibovici@cea.fr
+ *
+ *
+ * This software is a server that implements the NFS protocol.
+ * 
+ *
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ * ---------------------------------------
+ */
+
+#ifndef _PNFS_LAYOUT4_NFSV4_1_FILES_H
+#define _PNFS_LAYOUT4_NFSV4_1_FILES_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif                          /* HAVE_CONFIG_H */
+
+#ifdef _SOLARIS
+#include "solaris_port.h"
+#endif                          /* _SOLARIS */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <time.h>
+#include <pthread.h>
+
+#ifdef _USE_GSSRPC
+#include <gssrpc/types.h>
+#include <gssrpc/rpc.h>
+#else
+#include <rpc/types.h>
+#include <rpc/rpc.h>
+#endif
+
+#include "RW_Lock.h"
+#include "LRU_List.h"
+#include "HashData.h"
+#include "HashTable.h"
+#include "fsal.h"
+#include "fsal_types.h"
+#ifdef _USE_MFSL
+#include "mfsl.h"
+#endif
+#include "log_functions.h"
+#include "config_parsing.h"
+#include "nfs23.h"
+#include "nfs4.h"
+
+#define NB_MAX_PNFS_DS 2
+#define PNFS_NFS4      4
+#define PNFS_SENDSIZE 32768
+#define PNFS_RECVSIZE 32768
+
+#define PNFS_LAYOUTFILE_FILEHANDLE_MAX_LEN 128
+#define PNFS_LAYOUTFILE_PADDING_LEN  NFS4_OPAQUE_LIMIT
+#define PNFS_LAYOUTFILE_OWNER_LEN 128
+
+typedef struct pnfs_ds_parameter__
+{
+  unsigned int ipaddr;
+  unsigned short ipport;
+  unsigned int prognum;
+  char rootpath[MAXPATHLEN];
+  unsigned int id;
+} pnfs_ds_parameter_t;
+
+typedef struct pnfs_layoutfile_parameter__
+{
+  unsigned int stripe_size;
+  unsigned int stripe_width;
+  pnfs_ds_parameter_t ds_param[NB_MAX_PNFS_DS];
+} pnfs_layoutfile_parameter_t;
+
+typedef struct pnfs_client__
+{
+  sessionid4 session;
+  sequenceid4 sequence;
+  nfs_fh4 ds_rootfh[NB_MAX_PNFS_DS];
+  CLIENT *rpc_client;
+} pnfs_client_t;
+
+typedef struct pnfs_ds_file__
+{
+  nfs_fh4 handle ;
+  stateid4 stateid ;
+} pnfs_ds_file_t ;
+
+/* Mandatory functions */
+int pnfs_init(pnfs_client_t * pnfsclient,
+              pnfs_layoutfile_parameter_t * pnfs_layout_param);
+
+int pnfs_create_ds_file( pnfs_client_t  * pnfsclient, 
+			 fattr4_fileid    fileid,
+                         pnfs_ds_file_t * pfile ) ;
+
+int pnfs_unlink_ds_file( pnfs_client_t  * pnfsclient, 
+                         fattr4_fileid    fileid,
+                         pnfs_ds_file_t * pfile ) ; 
+
+/* Internal functions */
+int pnfs_connect(pnfs_client_t * pnfsclient,
+                 pnfs_layoutfile_parameter_t * pnfs_layout_param);
+
+int pnfs_do_mount(pnfs_client_t * pnfsclient,
+                  pnfs_ds_parameter_t * pds_param);
+
+int pnfs_lookupPath(pnfs_client_t * pnfsclient, 
+                    char *p_path, 
+                    nfs_fh4 * object_handle);
+
+#endif                          /* _PNFS_LAYOUT4_NFSV4_1_FILES_H */
