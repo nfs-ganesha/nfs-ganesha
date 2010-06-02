a936a29bba9da28e1375ed39a5c400c3247a61da
Philippe Deniel <philippe.deniel@cea.fr>
2010-06-02 10:53:01 +0200

    Add fsal_internal_inum2handle in fsal_internal.[ch]

--- src/FSAL/FSAL_XFS/fsal_internal.h
+++ src/FSAL/FSAL_XFS/fsal_internal.h
@@ -149,3 +149,9 @@ fsal_status_t fsal_internal_testAccess(fsal_op_context_t * p_context,   /* IN */
                                        struct stat *p_buffstat, /* IN, optional */
                                        fsal_attrib_list_t *
                                        p_object_attributes /* IN, optional */ );
+
+fsal_status_t fsal_internal_inum2handle( fsal_op_context_t * p_context,  /* IN */
+                                         ino_t     inum,    /* IN */
+		        	         fsal_handle_t * phandle ) ; /* OUT */
+
+
