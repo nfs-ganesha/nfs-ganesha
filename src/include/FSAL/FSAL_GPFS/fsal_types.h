48e4d3ea2d2ec4c88677e532b674088e63197830
Sean Dague <sdague@linux.vnet.ibm.com>
2010-06-02 15:21:14 -0400

    add a compiling fsal_internal.  There is a gotcha around path to handle though that we'll need to sort out.

--- src/include/FSAL/FSAL_GPFS/fsal_types.h
+++ src/include/FSAL/FSAL_GPFS/fsal_types.h
@@ -115,6 +115,7 @@ static const fsal_name_t FSAL_DOT_DOT = { "..", 2 };
 
 #define OPENHANDLE_HANDLE_LEN 20
 #define OPENHANDLE_DRIVER_MAGIC     'O'
+#define OPENHANDLE_NAME_TO_HANDLE _IOWR(OPENHANDLE_DRIVER_MAGIC, 0, struct name_handle_arg)
 #define OPENHANDLE_OPEN_BY_HANDLE _IOWR(OPENHANDLE_DRIVER_MAGIC, 1, struct open_arg)
 #define OPENHANDLE_LINK_BY_FD     _IOWR(OPENHANDLE_DRIVER_MAGIC, 2, struct link_arg)
 #define OPENHANDLE_READLINK_BY_FD _IOWR(OPENHANDLE_DRIVER_MAGIC, 3, struct readlink_arg)
@@ -125,6 +126,12 @@ struct file_handle {
 	int handle_type;
 	/* file identifier */
 	unsigned char f_handle[0];
+};
+
+struct name_handle_arg {
+	char *name;
+	int flag;
+	struct file_handle *handle;
 };
 
 struct open_arg {
