5e3490bd4ed655ca638ce8b86a7126c19044d20e
Sean Dague <sdague@linux.vnet.ibm.com>
2010-07-20 11:47:14 -0400

    Remove reference to no longer used GPFS parameter
    Signed-off-by: Sean Dague <sdague@linux.vnet.ibm.com>

--- src/FSAL/FSAL_GPFS/fsal_tools.c
+++ src/FSAL/FSAL_GPFS/fsal_tools.c
@@ -836,11 +836,10 @@ fsal_status_t FSAL_load_FS_specific_parameter_from_conf(config_file_t in_config,
         }
     }
 
-  if(out_parameter->fs_specific_info.gpfs_mount_point[0] == '\0'
-     || out_parameter->fs_specific_info.open_by_handle_dev_file[0] == '\0')
+  if(out_parameter->fs_specific_info.open_by_handle_dev_file[0] == '\0')
     {
       DisplayLog
-          ("FSAL LOAD PARAMETER: MountPoint and OpenByHandleDeviceFile MUST be specified in the configuration file",
+          ("FSAL LOAD PARAMETER: OpenByHandleDeviceFile MUST be specified in the configuration file",
            CONF_LABEL_FS_SPECIFIC);
       ReturnCode(ERR_FSAL_NOENT, 0);
     }
