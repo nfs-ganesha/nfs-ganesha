d09778d42d69a7a5f799bfe84feebea9359e7bfd
Sean Dague <sdague@linux.vnet.ibm.com>
2010-07-26 17:00:55 -0400

    fix POSIX for the cleanupexportcontext addition
    fix pointer signature for cleanup in exports
    
    Signed-off-by: Sean Dague <sdague@linux.vnet.ibm.com>

--- src/FSAL/FSAL_POSIX/fsal_context.c
+++ src/FSAL/FSAL_POSIX/fsal_context.c
@@ -90,6 +90,21 @@ fsal_status_t POSIXFSAL_BuildExportContext(posixfsal_export_context_t * p_export
   Return(ERR_FSAL_NO_ERROR, 0, INDEX_FSAL_BuildExportContext);
 }
 
+
+
+/**
+ * FSAL_CleanUpExportContext :
+ * this will clean up and state in an export that was created during
+ * the BuildExportContext phase.  For many FSALs this may be a noop.
+ *
+ * \param p_export_context (in, gpfsfsal_export_context_t)
+ */
+
+fsal_status_t POSIXFSAL_CleanUpExportContext(posixfsal_export_context_t * p_export_context) 
+{
+  Return(ERR_FSAL_NO_ERROR, 0, INDEX_FSAL_CleanUpExportContext);
+}
+
 fsal_status_t POSIXFSAL_InitClientContext(posixfsal_op_context_t * p_thr_context)
 {
 
