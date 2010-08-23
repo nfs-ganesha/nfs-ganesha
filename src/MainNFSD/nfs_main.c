ca89fd0cea7e20745caff978c14172e2fc1e15d6
Jeremy Bongio <jbongio@us.ibm.com>
2010-08-23 01:06:03 -0400

    Add a conditional around SetLogLevelFromEnv so that the environment variables aren't reread when a emergency cache flush thread is started.

--- src/MainNFSD/nfs_main.c
+++ src/MainNFSD/nfs_main.c
@@ -288,7 +288,8 @@ int main(int argc, char *argv[])
     }
 
   /* initialize memory and logging */
-  SetLogLevelFromEnv();
+  if (!my_nfs_start_info.flush_datacache_mode)
+    SetLogLevelFromEnv();
   if(nfs_prereq_init(exec_name, host_name, debug_level, log_path))
     {
       fprintf(stderr, "NFS MAIN: Error initializing NFSd prerequisites\n");
