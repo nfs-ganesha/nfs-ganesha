96e73a22b547af56218d8260cdd90ba768e42a15
Frank S. Filz <ffilzlnx@us.ibm.com>
2010-08-25 16:47:59 -0700

    Add more tests and a log to buffer capability
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>

--- src/Log/test_liblog_functions.c
+++ src/Log/test_liblog_functions.c
@@ -44,12 +44,142 @@ static family_error_t tab_test_err[] = {
   {ERR_NULL, "ERR_NULL", ""}
 };
 
+void TestAlways(int expect, char *buff, log_components_t component, char *string)
+{
+  char compare[2048];
+  sprintf(compare, "%s: %s", LogComponents[component].comp_str, string);
+  buff[0] = '\0';
+  LogAlways(component, "%s", string);
+  if (expect && strcmp(compare, buff) != 0 || !expect && buff[0] != '\0')
+    {
+      LogTest("FAILURE: %s produced %s expected %s", string, buff, compare);
+      exit(1);
+    }
+  LogTest("SUCCESS: %s produced %s", string, buff);
+}
+
+void TestMajor(int expect, char *buff, log_components_t component, char *string)
+{
+  char compare[2048];
+  sprintf(compare, "%s: %s: %s", LogComponents[component].comp_str, tabLogLevel[NIV_MAJOR].str, string);
+  buff[0] = '\0';
+  LogMajor(component, "%s", string);
+  if (expect && strcmp(compare, buff) != 0 || !expect && buff[0] != '\0')
+    {
+      LogTest("FAILURE: %s produced %s expected %s", string, buff, compare);
+      exit(1);
+    }
+  if (expect)
+    LogTest("SUCCESS: %s produced %s", string, buff);
+  else
+    LogTest("SUCCESS: %s didn't produce anything", string);
+}
+
+void TestCrit(int expect, char *buff, log_components_t component, char *string)
+{
+  char compare[2048];
+  sprintf(compare, "%s: %s: %s", LogComponents[component].comp_str, tabLogLevel[NIV_CRIT].str, string);
+  buff[0] = '\0';
+  LogCrit(component, "%s", string);
+  if (expect && strcmp(compare, buff) != 0 || !expect && buff[0] != '\0')
+    {
+      LogTest("FAILURE: %s produced %s expected %s", string, buff, compare);
+      exit(1);
+    }
+  if (expect)
+    LogTest("SUCCESS: %s produced %s", string, buff);
+  else
+    LogTest("SUCCESS: %s didn't produce anything", string);
+}
+
+void TestEvent(int expect, char *buff, log_components_t component, char *string)
+{
+  char compare[2048];
+  sprintf(compare, "%s: %s: %s", LogComponents[component].comp_str, tabLogLevel[NIV_EVENT].str, string);
+  buff[0] = '\0';
+  LogEvent(component, "%s", string);
+  if (expect && strcmp(compare, buff) != 0 || !expect && buff[0] != '\0')
+    {
+      LogTest("FAILURE: %s produced %s expected %s", string, buff, compare);
+      exit(1);
+    }
+  if (expect)
+    LogTest("SUCCESS: %s produced %s", string, buff);
+  else
+    LogTest("SUCCESS: %s didn't produce anything", string);
+}
+
+void TestDebug(int expect, char *buff, log_components_t component, char *string)
+{
+  char compare[2048];
+  sprintf(compare, "%s: %s: %s", LogComponents[component].comp_str, tabLogLevel[NIV_DEBUG].str, string);
+  buff[0] = '\0';
+  LogDebug(component, "%s", string);
+  if (expect && strcmp(compare, buff) != 0 || !expect && buff[0] != '\0')
+    {
+      LogTest("FAILURE: %s produced %s expected %s", string, buff, compare);
+      exit(1);
+    }
+  if (expect)
+    LogTest("SUCCESS: %s produced %s", string, buff);
+  else
+    LogTest("SUCCESS: %s didn't produce anything", string);
+}
+
+void TestFullDebug(int expect, char *buff, log_components_t component, char *string)
+{
+  char compare[2048];
+  sprintf(compare, "%s: %s: %s", LogComponents[component].comp_str, tabLogLevel[NIV_FULL_DEBUG].str, string);
+  buff[0] = '\0';
+  LogFullDebug(component, "%s", string);
+  if (expect && strcmp(compare, buff) != 0 || !expect && buff[0] != '\0')
+    {
+      LogTest("FAILURE: %s produced %s expected %s", string, buff, compare);
+      exit(1);
+    }
+  if (expect)
+    LogTest("SUCCESS: %s produced %s", string, buff);
+  else
+    LogTest("SUCCESS: %s didn't produce anything", string);
+}
+
+#define TestFormat(format, args...)                     \
+  do {                                                  \
+    char compare[2048], buff[2048];                     \
+    sprintf(compare, format, ## args);                  \
+    log_snprintf(buff, 2048, format, ## args);          \
+    if (strcmp(compare, buff) != 0)                     \
+      {                                                 \
+        LogTest("FAILURE: %s produced %s expected %s",  \
+                format, buff, compare);                 \
+        exit(1);                                        \
+      }                                                 \
+    else                                                \
+      LogTest("SUCCESS: %s produced %s", format, buff); \
+  } while (0)
+
+#define TestGaneshaFormat(expect, compare, format, args...) \
+  do {                                                  \
+    char buff[2048];                                    \
+    log_snprintf(buff, 2048, format, ## args);          \
+    if (strcmp(compare, buff) != 0 && expect)           \
+      {                                                 \
+        LogTest("FAILURE: %s produced %s expected %s",  \
+                format, buff, compare);                 \
+        exit(1);                                        \
+      }                                                 \
+    else if (expect)                                    \
+      LogTest("SUCCESS: %s produced %s", format, buff); \
+    else                                                \
+      LogTest("FAILURE (EXPECTED):  %s produced %s", format, buff); \
+  } while (0)
+
 /**
  *  Tests about Log streams and special printf functions.
  */
 int Test1(void *arg)
 {
-  char tempstr[255];
+  char tempstr[2048];
 
   SetNameFunction((char *)arg);
 
@@ -191,6 +321,85 @@ int Test1(void *arg)
     LogFullDebug  (COMPONENT_INIT, "LogFullDebug (shouldn't print)");
   }
 #endif /* _SNMP_ADM_ACTIVE */
+
+  LogTest("------------------------------------------------------");
+  LogTest("Test all levels of log filtering");
+  SetComponentLogBuffer(COMPONENT_LOG, tempstr);
+  SetComponentLogLevel(COMPONENT_LOG, NIV_NULL);
+  TestAlways    (1, tempstr, COMPONENT_LOG, "LogAlways (should print)");
+  TestMajor     (0, tempstr, COMPONENT_LOG, "LogMajor (shouldn't print)");
+  TestCrit      (0, tempstr, COMPONENT_LOG, "LogCrit (shouldn't print)");
+  TestEvent     (0, tempstr, COMPONENT_LOG, "LogEvent (shouldn't print)");
+  TestDebug     (0, tempstr, COMPONENT_LOG, "LogDebug (shouldn't print)");
+  TestFullDebug (0, tempstr, COMPONENT_LOG, "LogFullDebug (shouldn't print)");
+  SetComponentLogLevel(COMPONENT_LOG, NIV_MAJOR);
+  TestAlways    (1, tempstr, COMPONENT_LOG, "LogAlways (should print)");
+  TestMajor     (1, tempstr, COMPONENT_LOG, "LogMajor (should print)");
+  TestCrit      (0, tempstr, COMPONENT_LOG, "LogCrit (shouldn't print)");
+  TestEvent     (0, tempstr, COMPONENT_LOG, "LogEvent (shouldn't print)");
+  TestDebug     (0, tempstr, COMPONENT_LOG, "LogDebug (shouldn't print)");
+  TestFullDebug (0, tempstr, COMPONENT_LOG, "LogFullDebug (shouldn't print)");
+  SetComponentLogLevel(COMPONENT_LOG, NIV_CRIT);
+  TestAlways    (1, tempstr, COMPONENT_LOG, "LogAlways (should print)");
+  TestMajor     (1, tempstr, COMPONENT_LOG, "LogMajor (should print)");
+  TestCrit      (1, tempstr, COMPONENT_LOG, "LogCrit (should print)");
+  TestEvent     (0, tempstr, COMPONENT_LOG, "LogEvent (shouldn't print)");
+  TestDebug     (0, tempstr, COMPONENT_LOG, "LogDebug (shouldn't print)");
+  TestFullDebug (0, tempstr, COMPONENT_LOG, "LogFullDebug (shouldn't print)");
+  SetComponentLogLevel(COMPONENT_LOG, NIV_EVENT);
+  TestAlways    (1, tempstr, COMPONENT_LOG, "LogAlways (should print)");
+  TestMajor     (1, tempstr, COMPONENT_LOG, "LogMajor (should print)");
+  TestCrit      (1, tempstr, COMPONENT_LOG, "LogCrit (should print)");
+  TestEvent     (1, tempstr, COMPONENT_LOG, "LogEvent (should print)");
+  TestDebug     (0, tempstr, COMPONENT_LOG, "LogDebug (shouldn't print)");
+  TestFullDebug (0, tempstr, COMPONENT_LOG, "LogFullDebug (shouldn't print)");
+  SetComponentLogLevel(COMPONENT_LOG, NIV_DEBUG);
+  TestAlways    (1, tempstr, COMPONENT_LOG, "LogAlways (should print)");
+  TestMajor     (1, tempstr, COMPONENT_LOG, "LogMajor (should print)");
+  TestCrit      (1, tempstr, COMPONENT_LOG, "LogCrit (should print)");
+  TestEvent     (1, tempstr, COMPONENT_LOG, "LogEvent (should print)");
+  TestDebug     (1, tempstr, COMPONENT_LOG, "LogDebug (should print)");
+  TestFullDebug (0, tempstr, COMPONENT_LOG, "LogFullDebug (shouldn't print)");
+  SetComponentLogLevel(COMPONENT_LOG, NIV_FULL_DEBUG);
+  TestAlways    (1, tempstr, COMPONENT_LOG, "LogAlways (should print)");
+  TestMajor     (1, tempstr, COMPONENT_LOG, "LogMajor (should print)");
+  TestCrit      (1, tempstr, COMPONENT_LOG, "LogCrit (should print)");
+  TestEvent     (1, tempstr, COMPONENT_LOG, "LogEvent (should print)");
+  TestDebug     (1, tempstr, COMPONENT_LOG, "LogDebug (should print)");
+  TestFullDebug (1, tempstr, COMPONENT_LOG, "LogFullDebug (should print)");
+
+  LogTest("------------------------------------------------------");
+  LogTest("Test various formats");
+  SetComponentLogLevel(COMPONENT_LOG, NIV_EVENT);
+  TestFormat("none");
+  TestFormat("String: %s", "str");
+  errno = EINVAL;
+  TestFormat("strerro: %m");
+  TestFormat("integers: %d %x", 1, 20);
+  LogTest("\nTest %%b, %%B, %%h (doesn't work - overloaded), %%H, %%y, and %%Y. These are odd tags:");
+  LogTest("   %%b, %%B, %%h, and %%H each consume int1, str2, str3 even if not all are printed");
+  LogTest("   %%y and %%Y each consume int1, str2, str3, int4, str5, str6 even if not all are printed");
+  LogTest("   An extra string parameter is printed to demonstrate how the parameters are consumed");
+  TestGaneshaFormat(1, "str2(1) (not part of %b)", "%b %s", 1, "str2", "str3", "(not part of %b)");
+  TestGaneshaFormat(1, "str2(1) : 'str3' (not part of %B)", "%B %s", 1, "str2", "str3", "(not part of %B)");
+  TestGaneshaFormat(0, "str2(1) (not part of %h)", "%h %s", 1, "str2", "(not part of %h)");
+  TestGaneshaFormat(1, "str2(1) : 'str3' (not part of %H)", "%H %s", 1, "str2", "str3", "(not part of %H)");
+  /*
+  LogTest("test %%y %y %s", 1, "str2", "str3", 4, "str5", "str6", "(not part of %y)");
+  LogTest("test %%Y %Y %s", 1, "str2", "str3", 4, "str5", "str6", "(not part of %Y)");
+  LogTest("\nTest new tags for reporting errno values");
+  LogTest("test %%w %w", EINVAL);
+  LogTest("test %%W %W", EINVAL);
+  LogTest("\nTest context sensitive tags");
+  LogTest("%%K, %%V, and %%v go together, defaulting to ERR_SYS");
+  LogTest("%%J, %%R, and %%r go together, defaulting to ERR_POSIX");
+  LogTest("test %%K%%V %%K%%V %K%V %K%V", ERR_SYS, ERR_SIGACTION, ERR_POSIX, EINVAL);
+  LogTest("test %%K%%v %%K%%v %K%v %K%v", ERR_SYS, ERR_SIGACTION, ERR_POSIX, EINVAL);
+  LogTest("test %%J%%R %%J%%R %J%R %J%R", ERR_SYS, ERR_SIGACTION, ERR_POSIX, EINVAL);
+  LogTest("test %%J%%r %%J%%r %J%r %J%r", ERR_SYS, ERR_SIGACTION, ERR_POSIX, EINVAL);
+  LogTest("test %%V %%R %V %R", ERR_SIGACTION, EINVAL);
+  LogTest("test %%v %%r %v %r", ERR_SIGACTION, EINVAL);
+  */
 
   return 0;
 
