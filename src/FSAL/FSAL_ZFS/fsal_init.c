9ee82b792e92a1925febd453faed64030b569144
Philippe Deniel <philippe.deniel@cea.fr>
2010-12-17 15:55:43 +0100

    Pool "realloc"-ed not freed upon failure

--- src/FSAL/FSAL_ZFS/fsal_init.c
+++ src/FSAL/FSAL_ZFS/fsal_init.c
@@ -263,6 +263,7 @@ static void AddSnapshot(libzfswrap_vfs_t *p_vfs, char *psz_name)
     if( ( p_snapshots = realloc(p_snapshots, (i_snapshots + 1) * sizeof(*p_snapshots)) ) == NULL )
      {
        LogMajor(COMPONENT_FSAL, "SNAPSHOTS: recan't allocate memory... exiting");
+       free( p_snapshots ) ;
        exit( 1 ) ;
        return ;
      }
@@ -315,6 +316,7 @@ static void RemoveOldSnapshots(const char *psz_prefix, int number)
     if( ( p_snapshots = realloc(p_snapshots, (i_snapshots + 1) * sizeof(*p_snapshots)) ) == NULL )
      {
        LogMajor(COMPONENT_FSAL, "SNAPSHOTS: recan't allocate memory... exiting");
+       free( p_snapshots ) ;
        exit( 1 ) ;
        return ;
      }
@@ -337,6 +339,8 @@ static void *SnapshotThread(void *data)
     time_t time_now = time(NULL);
     struct tm *now = gmtime(&time_now);
     unsigned int i_wait;
+    char *psz_name;
+
     if(now->tm_min >= fs_info->snap_hourly_time)
       i_wait = 60 - (now->tm_min - fs_info->snap_hourly_time);
     else
@@ -347,7 +351,6 @@ static void *SnapshotThread(void *data)
     sleep(i_wait*60);
 
     /* Create a snapshot */
-    char *psz_name;
     libzfswrap_vfs_t *p_new = TakeSnapshotAndMount(fs_info->psz_zpool,
                                                    fs_info->psz_snap_hourly_prefix,
                                                    &psz_name);
