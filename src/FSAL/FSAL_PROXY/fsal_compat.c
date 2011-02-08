0c9783fd84d7fa660be1c9de13ef1b93623514f6
Jeremy Bongio <jbongio@us.ibm.com>
2011-02-08 08:45:36 -0500

    Added FSAL_sync() stub for PROXY_FSAL.

--- src/FSAL/FSAL_PROXY/fsal_compat.c
+++ src/FSAL/FSAL_PROXY/fsal_compat.c
@@ -211,6 +211,11 @@ fsal_status_t WRAP_PROXYFSAL_write(fsal_file_t * p_file_descriptor,     /* IN */
                          buffer_size, buffer, p_write_amount);
 }
 
+fsal_status_t WRAP_PROXYFSAL_sync(fsal_file_t * p_file_descriptor     /* IN */)
+{
+  return PROXYFSAL_sync((proxyfsal_file_t *) p_file_descriptor);
+}
+
 fsal_status_t WRAP_PROXYFSAL_close(fsal_file_t * p_file_descriptor /* IN */ )
 {
   return PROXYFSAL_close((proxyfsal_file_t *) p_file_descriptor);
@@ -688,6 +693,7 @@ fsal_functions_t fsal_proxy_functions = {
   .fsal_open = WRAP_PROXYFSAL_open,
   .fsal_read = WRAP_PROXYFSAL_read,
   .fsal_write = WRAP_PROXYFSAL_write,
+  .fsal_sync = WRAP_PROXYFSAL_sync,
   .fsal_close = WRAP_PROXYFSAL_close,
   .fsal_open_by_fileid = WRAP_PROXYFSAL_open_by_fileid,
   .fsal_close_by_fileid = WRAP_PROXYFSAL_close_by_fileid,
