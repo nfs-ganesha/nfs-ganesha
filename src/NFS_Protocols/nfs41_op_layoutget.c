182d8bb60f736bef0a199f73ef10ac775fe31f3c
Philippe Deniel <philippe.deniel@cea.fr>
2011-04-20 10:46:28 +0200

    Introduction of function pnfs_layoutget in nfs41_op_layoutget.c for PARALLEL_FS

--- src/NFS_Protocols/nfs41_op_layoutget.c
+++ src/NFS_Protocols/nfs41_op_layoutget.c
@@ -266,12 +266,20 @@ int nfs41_op_layoutget(struct nfs_argop4 *op, compound_data_t * data,
 #ifdef _USE_PNFS_PARALLEL_FS
   buffin = data->currentFH.nfs_fh4_val ;
   lenbuffin = data->currentFH.nfs_fh4_len ;
+
+  
+  if( ( rc = pnfs_layoutget( &data->currentFH, 
+                             &arg_LAYOUTGET4, 
+                             &res_LAYOUTGET4 ) ) != NFS4_OK )
+    {
+       res_LAYOUTGET4.logr_status = rc ;
+       return res_LAYOUTGET4.logr_status;
+    }
 #endif 
 
 #ifdef _USE_PNFS_SPNFS_LIKE
   buffin =  &data->current_entry->object.file.pnfs_file ;
   lenbuffin = sizeof( pnfs_file_t ) ;
-#endif
 
   if( ( rc = pnfs_service_layoutget( buffin,
                                      &lenbuffin,
@@ -287,6 +295,8 @@ int nfs41_op_layoutget(struct nfs_argop4 *op, compound_data_t * data,
       lenbuff,
       res_LAYOUTGET4.LAYOUTGET4res_u.logr_resok4.logr_layout.logr_layout_val[0].
       lo_content.loc_body.loc_body_val = buff;
+#endif 
+
 
   res_LAYOUTGET4.logr_status = NFS4_OK;
   return res_LAYOUTGET4.logr_status;
