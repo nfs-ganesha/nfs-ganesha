ca8735430379643346f754f7e13a0279e828678d
Sean Dague <sdague@linux.vnet.ibm.com>
2011-06-03 11:45:31 -0400

    clean up warnings Signed-off-by: Sean Dague <sdague@linux.vnet.ibm.com>

--- src/support/nfs4_tools.c
+++ src/support/nfs4_tools.c
@@ -10,16 +10,16 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * ---------------------------------------
  */
 
@@ -88,7 +88,9 @@ extern nfs_parameter_t nfs_param;
  */
 int nfs4_is_lease_expired(cache_entry_t * pentry)
 {
-  nfs_client_id_t nfs_clientid;
+  nfs_client_id_t nfs_clientid = {
+    .last_renew = 0
+  };
 
   if(pentry->internal_md.type != REGULAR_FILE)
     return 0;
