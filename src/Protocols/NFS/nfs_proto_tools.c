830fc3ea6f0897bf2266feeea5d89e694dedd33a
Philippe Deniel <philippe.deniel@cea.fr>
2011-08-01 13:39:51 +0200

    Bug Fix: when compiled with --enable-builsharedfsal, tets7 failed because of a badly initialized object handle then Removed debug messages

--- src/Protocols/NFS/nfs_proto_tools.c
+++ src/Protocols/NFS/nfs_proto_tools.c
@@ -172,6 +172,7 @@ cache_entry_t *nfs_FhandleToCache(u_long rq_vers,
   /* Default behaviour */
   *prc = NFS_REQ_OK;
 
+  memset( (char *)&fsal_data, 0, sizeof( fsal_data ) ) ;
   switch (rq_vers)
     {
     case NFS_V4:
