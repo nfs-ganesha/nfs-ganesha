5f5168ab173a92983df339f2d220d90816379cac
Jim Lieb <jlieb@panasas.com>
2011-08-23 15:04:02 -0700

    Use default methods for client context init+get for ZFS
    We no longer need these methods inside the fsall itself.
    
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/FSAL/FSAL_ZFS/fsal_internal.h
+++ src/FSAL/FSAL_ZFS/fsal_internal.h
@@ -88,15 +88,6 @@ fsal_status_t ZFSFSAL_BuildExportContext(fsal_export_context_t * p_export_contex
 
 fsal_status_t ZFSFSAL_CleanUpExportContext(fsal_export_context_t *p_export_context);
 
-fsal_status_t ZFSFSAL_InitClientContext(fsal_op_context_t * p_thr_context);
-
-fsal_status_t ZFSFSAL_GetClientContext(fsal_op_context_t * p_thr_context,    /* IN/OUT  */
-                                       fsal_export_context_t * p_export_context,     /* IN */
-                                       fsal_uid_t uid,  /* IN */
-                                       fsal_gid_t gid,  /* IN */
-                                       fsal_gid_t * alt_groups, /* IN */
-                                       fsal_count_t nb_alt_groups /* IN */ );
-
 fsal_status_t ZFSFSAL_create(fsal_handle_t * p_parent_directory_handle,      /* IN */
                              fsal_name_t * p_filename,  /* IN */
                              fsal_op_context_t * p_context,  /* IN */
