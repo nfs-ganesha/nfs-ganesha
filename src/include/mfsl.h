113d5d50254c3ae6037142f35c95f865ef396a28
Frank S. Filz <ffilzlnx@us.ibm.com>
2011-10-13 11:27:01 -0700

    Remove MFSL_lock, MFSL_unlock, MFSL_changelock, MFSL_getlock
    No one was calling these functions.
    With this, no one calls FSAL_lock, FSAL_changelock, FSAL_unlock, or FSAL_getlock.
    
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>

--- src/include/mfsl.h
+++ src/include/mfsl.h
@@ -349,28 +349,4 @@ fsal_status_t MFSL_rcp_by_fileid(mfsl_object_t * filehandle,    /* IN */
 /* To be called before exiting */
 fsal_status_t MFSL_terminate();
 
-#ifndef _USE_SWIG
-
-/******************************************************
- *                FSAL locks management.
- ******************************************************/
-fsal_status_t MSFL_lock(mfsl_file_t * obj_handle,       /* IN */
-                        fsal_lockdesc_t * ldesc,        /*IN/OUT */
-                        fsal_boolean_t callback /* IN */
-    );
-
-fsal_status_t MFSL_changelock(fsal_lockdesc_t * lock_descriptor,        /* IN / OUT */
-                              fsal_lockparam_t * lock_info      /* IN */
-    );
-
-fsal_status_t MFSL_unlock(mfsl_file_t * obj_handle,     /* IN */
-                          fsal_lockdesc_t * ldesc       /*IN/OUT */
-    );
-
-fsal_status_t MFSL_getlock(mfsl_file_t * obj_handle,    /* IN */
-                           fsal_lockdesc_t * ldesc      /*IN/OUT */
-    );
-
-#endif                          /* ! _USE_SWIG */
-
 #endif                          /* _MFSL_H */
