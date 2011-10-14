d3fa8026048ab8c5352a8243c8e96dce8ad4e159
Frank S. Filz <ffilzlnx@us.ibm.com>
2011-10-14 14:18:38 -0700

    Remove FSAL_static_fsinfo from FSAL_SNMP
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>

--- src/FSAL/FSAL_SNMP/fsal_compat.c
+++ src/FSAL/FSAL_SNMP/fsal_compat.c
@@ -38,7 +38,6 @@ fsal_functions_t fsal_snmp_functions = {
   .fsal_close = SNMPFSAL_close,
   .fsal_open_by_fileid = COMMON_open_by_fileid,
   .fsal_close_by_fileid = COMMON_close_by_fileid,
-  .fsal_static_fsinfo = SNMPFSAL_static_fsinfo,
   .fsal_dynamic_fsinfo = SNMPFSAL_dynamic_fsinfo,
   .fsal_init = SNMPFSAL_Init,
   .fsal_terminate = SNMPFSAL_terminate,
