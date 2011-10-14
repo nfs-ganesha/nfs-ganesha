3ee3f24f304ccf8bf9c54480bca67c9172ac493a
Frank S. Filz <ffilzlnx@us.ibm.com>
2011-10-14 14:17:38 -0700

    Remove FSAL_static_fsinfo from FSAL_FUSELIKE
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>

--- src/FSAL/FSAL_FUSELIKE/fsal_compat.c
+++ src/FSAL/FSAL_FUSELIKE/fsal_compat.c
@@ -40,7 +40,6 @@ fsal_functions_t fsal_fuse_functions = {
   .fsal_close = FUSEFSAL_close,
   .fsal_open_by_fileid = COMMON_open_by_fileid,
   .fsal_close_by_fileid = COMMON_close_by_fileid,
-  .fsal_static_fsinfo = FUSEFSAL_static_fsinfo,
   .fsal_dynamic_fsinfo = FUSEFSAL_dynamic_fsinfo,
   .fsal_init = FUSEFSAL_Init,
   .fsal_terminate = FUSEFSAL_terminate,
--- src/FSAL/FSAL_FUSELIKE/fsal_internal.h
+++ src/FSAL/FSAL_FUSELIKE/fsal_internal.h
@@ -199,10 +199,6 @@ fsal_status_t FUSEFSAL_write(fsal_file_t * p_file_descriptor,       /* IN */
 
 fsal_status_t FUSEFSAL_close(fsal_file_t * p_file_descriptor /* IN */ );
 
-fsal_status_t FUSEFSAL_static_fsinfo(fsal_handle_t * p_filehandle,  /* IN */
-                                     fsal_op_context_t * p_context, /* IN */
-                                     fsal_staticfsinfo_t * p_staticinfo /* OUT */ );
-
 fsal_status_t FUSEFSAL_dynamic_fsinfo(fsal_handle_t * p_filehandle, /* IN */
                                       fsal_op_context_t * p_context,        /* IN */
                                       fsal_dynamicfsinfo_t * p_dynamicinfo /* OUT */ );
