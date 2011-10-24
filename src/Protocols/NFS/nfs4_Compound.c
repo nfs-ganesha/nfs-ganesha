d031a758bea716bdf0e232402040800ecf15ba5c
Philippe Deniel <philippe.deniel@cea.fr>
2011-10-24 10:08:42 +0200

    No more SPNFSD_LIKE

--- src/Protocols/NFS/nfs4_Compound.c
+++ src/Protocols/NFS/nfs4_Compound.c
@@ -268,6 +268,16 @@ int nfs4_Compound(nfs_arg_t * parg /* IN     */ ,
       return NFS_REQ_OK;
     }
 
+  /* Check if this export supports NFSv4 */
+  if( pexport->options & EXPORT_OPTION_NFSV4 )
+   {
+      LogMajor( COMPONENT_NFS_V4,
+                "The export(id=%u) does not support NFSv4... rejecting it" ) ;
+      pres->res_compound4.status = NFS4ERR_PERM ;
+      pres->res_compound4.resarray.resarray_val[0].nfs_resop4_u.opaccess.status = NFS4ERR_PERM ;
+      return NFS_REQ_OK ;
+   }
+
   /* Check for too long request */
   if(COMPOUND4_ARRAY.argarray_len > 30)
     {
--- src/Protocols/PNFS/pnfs_glue.c
+++ src/Protocols/PNFS/pnfs_glue.c
@@ -47,41 +47,6 @@
 #include "pnfs.h" 
 #include "pnfs_service.h" 
 
-#ifdef _USE_PNFS_SPNFS_LIKE
-
-nfsstat4 pnfs_getdevicelist( GETDEVICELIST4args * pargs,  compound_data_t * data, GETDEVICELIST4res * pres ) 
-{
-   return pnfs_spnfs_getdevicelist( pargs, data, pres ) ;
-}
-
-nfsstat4 pnfs_getdeviceinfo( GETDEVICEINFO4args * pargs, compound_data_t * data, GETDEVICEINFO4res * pres ) 
-{
-   return pnfs_spnfs_getdeviceinfo( pargs, data, pres ) ;
-}
-
-nfsstat4 pnfs_layoutget( LAYOUTGET4args   * pargs,
-	                 compound_data_t  * data, 
-			 LAYOUTGET4res    * pres ) 
-{
-   return pnfs_spnfs_layoutget( pargs, data, pres ) ;
-}
-
-nfsstat4 pnfs_layoutcommit( LAYOUTCOMMIT4args * pargs, 
-                            compound_data_t   * data,
-                            LAYOUTCOMMIT4res  * pres ) 
-{
-   return pnfs_spnfs_layoutcommit( pargs, data, pres ) ;
-}
-
-nfsstat4 pnfs_layoutreturn( LAYOUTRETURN4args * pargs, 
-                            compound_data_t   * data, 
-                            LAYOUTRETURN4res  * pres ) 
-{
-   return pnfs_spnfs_layoutreturn( pargs, data, pres ) ;
-}
-
-#endif
-
 #ifdef _USE_PNFS_PARALLEL_FS
 nfsstat4 pnfs_getdevicelist( GETDEVICELIST4args * pargs,  compound_data_t * data, GETDEVICELIST4res * pres ) 
 {
