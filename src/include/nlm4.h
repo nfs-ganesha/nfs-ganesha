b4d7b86467a37bc2aac80d2c51be9c0a5a663940
Frank S. Filz <ffilzlnx@us.ibm.com>
2011-11-11 16:49:23 -0800

    Remove NLM async thread
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>

--- src/include/nlm4.h
+++ src/include/nlm4.h
@@ -185,7 +185,6 @@ extern "C"
   };
   typedef struct nlm4_sm_notifyargs nlm4_sm_notifyargs;
   extern void nlm_init(void);
-  extern void nlm_startup(void);
 
 #define NLMPROG 100021
 #define NLM4_VERS 4
