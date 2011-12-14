ccc6a0dd3d785b1a6aa4ec90af58244f021e7242
rong zeng <rongzeng@oc1354525745.ibm.com>
2011-12-14 15:49:21 -0800

    No need to open before calling GPFSFSAL_getattr in GPFSFSAL_opendir
    Singed-off-by: Rong zeng <rongzeng@us.ibm.com>

--- src/FSAL/FSAL_GPFS/fsal_dirs.c
+++ src/FSAL/FSAL_GPFS/fsal_dirs.c
@@ -159,13 +159,9 @@ fsal_status_t GPFSFSAL_readdir(fsal_dir_t * dir_desc,       /* IN */
   char buff[BUF_SIZE];
   struct linux_dirent *dp = NULL;
   int bpos = 0;
-  int tmpfd = 0;
   gpfsfsal_dir_t *p_dir_descriptor = (gpfsfsal_dir_t *)dir_desc;
   gpfsfsal_cookie_t start_position;
   gpfsfsal_cookie_t *p_end_position = (gpfsfsal_cookie_t *)end_position;
-
-  char d_type;
-  struct stat buffstat;
 
   int rc = 0;
 
@@ -232,8 +228,6 @@ fsal_status_t GPFSFSAL_readdir(fsal_dir_t * dir_desc,       /* IN */
       for(bpos = 0; bpos < rc;)
         {
           dp = (struct linux_dirent *)(buff + bpos);
-          d_type = *(buff + bpos + dp->d_reclen - 1);
-                                                    /** @todo not used for the moment. Waiting for information on symlink management */
           bpos += dp->d_reclen;
 
           /* LogFullDebug(COMPONENT_FSAL,
@@ -255,8 +249,6 @@ fsal_status_t GPFSFSAL_readdir(fsal_dir_t * dir_desc,       /* IN */
                             &(p_pdirent[*p_nb_entries].name))))
             ReturnStatus(st, INDEX_FSAL_readdir);
 
-          d_type = DT_UNKNOWN;
-
           // TODO: there is a race here, because between handle fetch
           // and open at things might change.  we need to figure out if there
           // is another way to open without the pcontext
@@ -270,68 +262,23 @@ fsal_status_t GPFSFSAL_readdir(fsal_dir_t * dir_desc,       /* IN */
                                           &(p_pdirent[*p_nb_entries].handle))))
             ReturnStatus(st, INDEX_FSAL_readdir);
 
-          if(FSAL_IS_ERROR
-             (st =
-              fsal_internal_handle2fd_at(p_dir_descriptor->fd,
-                                         &(p_pdirent[*p_nb_entries].handle), &tmpfd,
-                                         O_RDONLY | O_NOFOLLOW)))
-            {
-              if(errno != ELOOP)        /* ( p_dir_descriptor->fd, dp->d_name) is not a symlink */
-                ReturnStatus(st, INDEX_FSAL_readdir);
-              else
-                d_type = DT_LNK;
-            }
-
-          /* get object handle */
-          TakeTokenFSCall();
-          if(d_type != DT_LNK)
-            {
-              close(tmpfd);
-            }
-          else
-            {
-              if(fstatat(p_dir_descriptor->fd, dp->d_name, &buffstat, AT_SYMLINK_NOFOLLOW)
-                 < 0)
-                {
-                  ReleaseTokenFSCall();
-                  Return(posix2fsal_error(errno), errno, INDEX_FSAL_readdir);
-                }
-
-              p_pdirent[*p_nb_entries].attributes.asked_attributes = get_attr_mask;
-
-              st = posix2fsal_attributes(&buffstat, &p_pdirent[*p_nb_entries].attributes);
-              if(FSAL_IS_ERROR(st))
-                {
-                  ReleaseTokenFSCall();
-                  FSAL_CLEAR_MASK(p_pdirent[*p_nb_entries].attributes.asked_attributes);
-                  FSAL_SET_MASK(p_pdirent[*p_nb_entries].attributes.asked_attributes,
-                                FSAL_ATTR_RDATTR_ERR);
-                  ReturnStatus(st, INDEX_FSAL_getattrs);
-                }
-
-            }
-          ReleaseTokenFSCall();
-
           if(FSAL_IS_ERROR(st))
             ReturnStatus(st, INDEX_FSAL_readdir);
 
     /************************
      * Fills the attributes *
      ************************/
-          if(d_type != DT_LNK)
-            {
-              p_pdirent[*p_nb_entries].attributes.asked_attributes = get_attr_mask;
+          p_pdirent[*p_nb_entries].attributes.asked_attributes = get_attr_mask;
 
-              st = GPFSFSAL_getattrs(&(p_pdirent[*p_nb_entries].handle),
+          st = GPFSFSAL_getattrs(&(p_pdirent[*p_nb_entries].handle),
 				     (fsal_op_context_t *)&p_dir_descriptor->context,
                                  &p_pdirent[*p_nb_entries].attributes);
-              if(FSAL_IS_ERROR(st))
-                {
+          if(FSAL_IS_ERROR(st))
+          {
                   FSAL_CLEAR_MASK(p_pdirent[*p_nb_entries].attributes.asked_attributes);
                   FSAL_SET_MASK(p_pdirent[*p_nb_entries].attributes.asked_attributes,
                                 FSAL_ATTR_RDATTR_ERR);
-                }
-            }
+          }
           ((gpfsfsal_cookie_t *)(&p_pdirent[*p_nb_entries].cookie))->data.cookie = dp->d_off;
           p_pdirent[*p_nb_entries].nextentry = NULL;
           if(*p_nb_entries)
