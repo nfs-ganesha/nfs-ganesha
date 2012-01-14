4e8277b67e06f07047ace1e723dbf6002e80e640
Frank S. Filz <ffilzlnx@us.ibm.com>
2012-01-13 17:15:08 -0800

    Fix stateid check to not accept OTHER=all 0 or OTHER=all 1 with invalud seqid
    And, if special stateids are not allowed (for open and lock for example), reject them as NFS4ERR_BAD_STATEID instead of NFS4ERR_STALE_STATEID
    
    Changes pynfs results of:
    
    **************************************************
    CLOSE4   st_close.testBadStateid                                  : FAILURE
               CLOSE with a bad stateid should return
               NFS4ERR_BAD_STATEID, instead got NFS4ERR_STALE_STATEID
    INIT     st_setclientid.testValid                                 : PASS
    LKU8     st_locku.testBadLockStateid                              : FAILURE
               LOCKU with a bad stateid should return
               NFS4ERR_BAD_STATEID, instead got NFS4ERR_STALE_STATEID
    LOCK11   st_lock.testBadStateid                                   : FAILURE
               LOCK with a bad stateid should return
               NFS4ERR_BAD_STATEID, instead got NFS4ERR_STALE_STATEID
    MKFILE   st_open.testOpen                                         : PASS
    OPCF5    st_openconfirm.testBadStateid                            : FAILURE
               OPEN_CONFIRM with a bad state should return
               NFS4ERR_BAD_STATEID, instead got NFS4ERR_STALE_STATEID
    OPDG5    st_opendowngrade.testBadStateid                          : FAILURE
               OPENDOWNGRADE with bad stateid should return
               NFS4ERR_BAD_STATEID, instead got NFS4ERR_STALE_STATEID
    RD9      st_read.testBadStateid                                   : PASS
    WRT10    st_write.testBadStateid                                  : PASS
    **************************************************
    
    to
    
    **************************************************
    CLOSE4   st_close.testBadStateid                                  : PASS
    INIT     st_setclientid.testValid                                 : PASS
    LKU8     st_locku.testBadLockStateid                              : PASS
    LOCK11   st_lock.testBadStateid                                   : PASS
    MKFILE   st_open.testOpen                                         : PASS
    OPCF5    st_openconfirm.testBadStateid                            : PASS
    OPDG5    st_opendowngrade.testBadStateid                          : PASS
    RD9      st_read.testBadStateid                                   : PASS
    WRT10    st_write.testBadStateid                                  : PASS
    **************************************************
    
    Signed-off-by: Frank Filz <ffilzlnx@us.ibm.com>

--- src/SAL/nfs4_state_id.c
+++ src/SAL/nfs4_state_id.c
@@ -409,9 +409,10 @@ int nfs4_Check_Stateid(stateid4        * pstate,
       sprintf(str + OTHERSIZE * 2, ":%u", (unsigned int) pstate->seqid);
     }
 
-  if((flags & STATEID_SPECIAL_ALL_0) != 0)
+  /* Test for OTHER is all zeros */
+  if(memcmp(pstate->other, all_zero, OTHERSIZE) == 0)
     {
-      if(memcmp(pstate->other, all_zero, OTHERSIZE) == 0 && pstate->seqid == 0)
+      if(pstate->seqid == 0 && (flags & STATEID_SPECIAL_ALL_0) != 0)
         {
           /* All 0 stateid */
           LogDebug(COMPONENT_STATE,
@@ -421,12 +422,26 @@ int nfs4_Check_Stateid(stateid4        * pstate,
            */
           return NFS4_OK;
         }
+      if(pstate->seqid == 1 && (flags & STATEID_SPECIAL_CURRENT) != 0)
+        {
+          /* Special current stateid */
+          LogDebug(COMPONENT_STATE,
+                   "Check %s stateid found special 'current' stateid", tag);
+          /* Copy current stateid in and proceed to checks */
+          *pstate = data->current_stateid;
+        }
+
+      LogDebug(COMPONENT_STATE,
+               "Check %s stateid with OTHER all zeros, seqid %u unexpected",
+               tag, (unsigned int) pstate->seqid);
+      return NFS4ERR_BAD_STATEID;
     }
 
-  if((flags & STATEID_SPECIAL_ALL_1) != 0)
+  /* Test for OTHER is all ones */
+  if(memcmp(pstate->other, all_one, OTHERSIZE) == 0)
     {
-      if(memcmp(pstate->other, all_one, OTHERSIZE) == 0 &&
-         pstate->seqid == 0xFFFFFFFF)
+      /* Test for special all ones stateid */
+      if(pstate->seqid == 0xFFFFFFFF && (flags & STATEID_SPECIAL_ALL_1) != 0)
         {
           /* All 1 stateid */
           LogDebug(COMPONENT_STATE,
@@ -436,18 +451,11 @@ int nfs4_Check_Stateid(stateid4        * pstate,
            */
           return NFS4_OK;
         }
-    }
 
-  if((flags & STATEID_SPECIAL_CURRENT) != 0)
-    {
-      if(memcmp(pstate->other, all_zero, OTHERSIZE) == 0 && pstate->seqid == 1)
-        {
-          /* All 0 stateid */
-          LogDebug(COMPONENT_STATE,
-                   "Check %s stateid found special 'current' stateid", tag);
-          /* Copy current stateid in and proceed to checks */
-          *pstate = data->current_stateid;
-        }
+      LogDebug(COMPONENT_STATE,
+               "Check %s stateid with OTHER all ones, seqid %u unexpected",
+               tag, (unsigned int) pstate->seqid);
+      return NFS4ERR_BAD_STATEID;
     }
 
   /* Check if stateid was made from this server instance */
