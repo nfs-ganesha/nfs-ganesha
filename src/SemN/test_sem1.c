d3f074100c4bff6b1885d40a6450bc8ad47cb17f
Philippe DENIEL <philippe.deniel@cea.fr>
2012-03-08 15:50:41 +0100

    Add "check programs" in src/SemN, based on Max's bug report.

---
+++ src/SemN/test_sem1.c
@@ -0,0 +1,41 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "SemN.h"
+#include "log_macros.h"
+
+semaphore_t x;
+
+void *sem_me(void *arg)
+{
+        int me = (int)arg;
+        semaphore_P(&x);
+        printf("%d: Got it, count is %d\n", me, x.count);
+        sleep(1);
+        semaphore_V(&x);
+        printf("%d: dropped it, count is %d\n", me, x.count);
+}
+
+int main()
+{
+        int i;
+        pthread_t t[3];
+
+        semaphore_init(&x, 1);
+
+        for (i=0; i < 3; i++) {
+                pthread_create(t+i, NULL, sem_me, (void *)i);
+        }
+
+        for (i=0; i < 3; i++) {
+                pthread_join(t[i], NULL);
+        }
+
+        return 0;
+}
