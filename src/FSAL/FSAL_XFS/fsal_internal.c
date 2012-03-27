2352f0ca0318d935cc40b3bf98fc4d215fa549ed
Max Matveev <makc@gmx.co.uk>
2012-03-27 16:37:31 +1100

    XFS FSAL - return correct file descriptor when opening directories
    Fallback for handling directory opening in fsal_internal_handle2fd
    was returning wrong file descriptors: the parenthesis were
    placed incorrectly and was doing the equivalent of
    
    fd = (open_by_handle() < 0);
    
    so we were getting either 0 or 1 as file descriptor for a directory.
    Combined with open/fchmod/close it was creating a land mine for the
    open - a workload which feature a lot of directory making (like git clone)
    was racing and opening unrelated files on FD 0 and 1 and using wrong
    file descriptors for setting attributes. This explains why some permission
    bits for some files were "morphing" between two tree scans by git status.
    
    Signed-off-by: Max Matveev <makc@gmx.co.uk>
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/FSAL/FSAL_XFS/fsal_internal.c
+++ src/FSAL/FSAL_XFS/fsal_internal.c
@@ -360,24 +360,21 @@ fsal_status_t fsal_internal_handle2fd(fsal_op_context_t * p_context,
 {
   int rc = 0;
   int errsv = 0;
+  xfsfsal_handle_t *xh = (xfsfsal_handle_t *)phandle;
 
   if(!phandle || !pfd || !p_context)
     ReturnCode(ERR_FSAL_FAULT, 0);
 
-  rc = open_by_handle(((xfsfsal_handle_t *)phandle)->data.handle_val,
-		      ((xfsfsal_handle_t *)phandle)->data.handle_len,
-		      oflags);
+  rc = open_by_handle(xh->data.handle_val, xh->data.handle_len, oflags);
+  errsv = errno;
   if(rc == -1)
     {
-      errsv = errno;
-
       if(errsv == EISDIR)
         {
-          if((rc =
-              open_by_handle(((xfsfsal_handle_t *)phandle)->data.handle_val,
-			     ((xfsfsal_handle_t *)phandle)->data.handle_len,
-			     O_DIRECTORY) < 0))
-            ReturnCode(posix2fsal_error(errsv), errsv);
+          rc = open_by_handle(xh->data.handle_val, xh->data.handle_len,
+			      O_DIRECTORY);
+          if(rc < 0)
+	     ReturnCode(posix2fsal_error(errsv), errsv);
         }
       else
         ReturnCode(posix2fsal_error(errsv), errsv);
