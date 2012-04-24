8d231a01bf61880d9e47198f85e4b2ba60b9927d
rong zeng <rongzeng@oc1354525745.ibm.com>
2012-04-24 10:35:57 -0700

    Coded OP_SECINFO to properly return auth flavors based on export entry.
    Signed-off-by: rong zeng <rongzeng@us.ibm.com>
    Signed-off-by: Venkateswararao Jujjuri (JV) <jvrao@linux.vnet.ibm.com>

--- src/Protocols/NFS/nfs4_op_secinfo.c
+++ src/Protocols/NFS/nfs4_op_secinfo.c
@@ -62,6 +62,7 @@
 #include "nfs_creds.h"
 #include "nfs_proto_functions.h"
 #include "nfs_tools.h"
+#include "sal_functions.h"
 
 /**
  * nfs4_op_secinfo: The NFS4_OP_SECINFO
@@ -78,6 +79,9 @@
 #define arg_SECINFO4 op->nfs_argop4_u.opsecinfo
 #define res_SECINFO4 resp->nfs_resop4_u.opsecinfo
 
+gss_OID_desc krb5oid =
+   {9, "\052\206\110\206\367\022\001\002\002"};
+
 int nfs4_op_secinfo(struct nfs_argop4 *op,
                     compound_data_t * data, struct nfs_resop4 *resp)
 {
@@ -85,18 +89,122 @@ int nfs4_op_secinfo(struct nfs_argop4 *op,
 
   resp->resop = NFS4_OP_SECINFO;
   res_SECINFO4.status = NFS4_OK;
+  fsal_name_t secinfo_fh_name;
+  cache_inode_status_t cache_status;
+  cache_entry_t *entry_src;
+  fsal_attrib_list_t attr_secinfo;
+  int num_entry = 0;
+
+  /* Read name from uft8 strings, if one is empty then returns NFS4ERR_INVAL */
+  if(arg_SECINFO4.name.utf8string_len == 0)
+    {
+      res_SECINFO4.status = NFS4ERR_INVAL;
+      return NFS4ERR_INVAL;
+    }
+
+  /* If there is no FH */
+  if(nfs4_Is_Fh_Empty(&(data->currentFH)))
+    {
+      res_SECINFO4.status = NFS4ERR_NOFILEHANDLE;
+      return res_SECINFO4.status;
+    }
+
+  /* If the filehandle is invalid */
+  if(nfs4_Is_Fh_Invalid(&(data->currentFH)))
+    {
+      res_SECINFO4.status = NFS4ERR_BADHANDLE;
+      return res_SECINFO4.status;
+    }
+
+  /* Tests if the Filehandle is expired (for volatile filehandle) */
+  if(nfs4_Is_Fh_Expired(&(data->currentFH)))
+    {
+      res_SECINFO4.status = NFS4ERR_FHEXPIRED;
+      return res_SECINFO4.status;
+    }
+
+  if (nfs_in_grace())
+    {
+      res_SECINFO4.status = NFS4ERR_GRACE;
+      return res_SECINFO4.status;
+    }
+
+  if(data->current_filetype != DIRECTORY)
+    {
+      res_SECINFO4.status = NFS4ERR_NOTDIR;
+      return res_SECINFO4.status;
+    }
+
+  /* get the names from the RPC input */
+  if((cache_status =
+      cache_inode_error_convert(FSAL_buffdesc2name
+                                ((fsal_buffdesc_t *) & arg_SECINFO4.name,
+                                 &secinfo_fh_name))) != CACHE_INODE_SUCCESS)
+    {
+      res_SECINFO4.status = NFS4ERR_INVAL;
+      return res_SECINFO4.status;
+    }
+
+  if((entry_src = cache_inode_lookup(data->current_entry,
+                                         &secinfo_fh_name,
+                                         data->pexport->cache_inode_policy,
+                                         &attr_secinfo,
+                                         data->ht,
+                                         data->pclient,
+                                         data->pcontext, &cache_status)) == NULL)
+    {
+      res_SECINFO4.status = nfs4_Errno(cache_status);
+      return res_SECINFO4.status;
+    }
+
+  /* get the number of entries */
+  if (data->pexport->options & EXPORT_OPTION_AUTH_NONE)
+    num_entry++;
+  if (data->pexport->options & EXPORT_OPTION_AUTH_UNIX)
+    num_entry++;
+  if (data->pexport->options & EXPORT_OPTION_RPCSEC_GSS_NONE)
+    num_entry++;
+  if (data->pexport->options & EXPORT_OPTION_RPCSEC_GSS_INTG)
+    num_entry++;
+  if (data->pexport->options & EXPORT_OPTION_RPCSEC_GSS_PRIV)
+    num_entry++;
 
   if((res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val =
-      (secinfo4 *) Mem_Alloc(2 * sizeof(secinfo4))) == NULL)
+      (secinfo4 *) Mem_Alloc(num_entry * sizeof(secinfo4))) == NULL)
     {
       res_SECINFO4.status = NFS4ERR_SERVERFAULT;
       return res_SECINFO4.status;
     }
 
-  /* For the moment, we just have AUTH_NONE and AUTH_UNIX, and this needs no secinfo */
-  res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[0].flavor = AUTH_UNIX;
-  res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[1].flavor = AUTH_NONE;
-  res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_len = 2;
+  sec_oid4 local_oid = {krb5oid.length, (char *)krb5oid.elements};
+  
+  int idx=0;
+  if (data->pexport->options & EXPORT_OPTION_AUTH_NONE)
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx++].flavor = AUTH_NONE;
+  if (data->pexport->options & EXPORT_OPTION_AUTH_UNIX)
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx++].flavor = AUTH_UNIX;
+  if (data->pexport->options & EXPORT_OPTION_RPCSEC_GSS_NONE)
+    {
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].flavor = RPCSEC_GSS;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].secinfo4_u.flavor_info.service = RPCSEC_GSS_SVC_NONE;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].secinfo4_u.flavor_info.qop = GSS_C_QOP_DEFAULT;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx++].secinfo4_u.flavor_info.oid = local_oid;
+    }
+  if (data->pexport->options & EXPORT_OPTION_RPCSEC_GSS_INTG)
+    {
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].flavor = RPCSEC_GSS;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].secinfo4_u.flavor_info.service = RPCSEC_GSS_SVC_INTEGRITY;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].secinfo4_u.flavor_info.qop = GSS_C_QOP_DEFAULT;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx++].secinfo4_u.flavor_info.oid = local_oid;
+    }
+  if (data->pexport->options & EXPORT_OPTION_RPCSEC_GSS_PRIV)
+    {
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].flavor = RPCSEC_GSS;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].secinfo4_u.flavor_info.service = RPCSEC_GSS_SVC_PRIVACY;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx].secinfo4_u.flavor_info.qop = GSS_C_QOP_DEFAULT;
+      res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx++].secinfo4_u.flavor_info.oid = local_oid;
+    }
+  res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_len = idx;
 
   return res_SECINFO4.status;
 }                               /* nfs4_op_secinfo */
