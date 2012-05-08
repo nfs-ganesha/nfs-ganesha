7c3f63d0f39b80dd905d0ec40f11b1f630cc72e3
Lance Russell <lancerus@us.ibm.com>
2012-05-08 11:28:35 -0700

    Close the directory file descriptor on error.

--- src/Cache_inode/cache_inode_readdir.c
+++ src/Cache_inode/cache_inode_readdir.c
@@ -475,7 +475,7 @@ cache_inode_readdir_populate(cache_entry_t *directory,
       if(FSAL_IS_ERROR(fsal_status))
         {
           *status = cache_inode_error_convert(fsal_status);
-          return *status;
+          goto bail;
         }
 
       for(iter = 0; iter < found; iter++)
@@ -514,7 +514,7 @@ cache_inode_readdir_populate(cache_entry_t *directory,
                      if (fsal_status.major == ERR_FSAL_STALE) {
                           cache_inode_kill_entry(directory, client);
                      }
-                     return *status;
+                     goto bail;
                 }
             }
           else
@@ -540,8 +540,7 @@ cache_inode_readdir_populate(cache_entry_t *directory,
                                       context,
                                       CACHE_INODE_FLAG_NONE,
                                       status)) == NULL)
-            return *status;
-
+            goto bail;
           cache_status
             = cache_inode_add_cached_dirent(directory,
                                             &(array_dirent[iter].name),
@@ -557,7 +556,7 @@ cache_inode_readdir_populate(cache_entry_t *directory,
 
           if(cache_status != CACHE_INODE_SUCCESS
              && cache_status != CACHE_INODE_ENTRY_EXISTS)
-            return *status;
+            goto bail;
 
           /*
            * Remember the FSAL readdir cookie associated with this
@@ -597,6 +596,12 @@ cache_inode_readdir_populate(cache_entry_t *directory,
                        CACHE_INODE_TRUST_CONTENT));
   *status = CACHE_INODE_SUCCESS;
   return *status;
+
+bail:
+  /* Close the directory */
+  FSAL_closedir(&dir_handle);
+  return *status;
+
 }                               /* cache_inode_readdir_populate */
 
 /**
