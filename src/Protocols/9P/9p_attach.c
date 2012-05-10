6ae9374651c69ff903c334fd70d687a0edd43684
Philippe DENIEL <philippe.deniel@cea.fr>
2012-05-10 17:07:26 +0200

    Backport new_handle to 9P.

--- src/Protocols/9P/9p_attach.c
+++ src/Protocols/9P/9p_attach.c
@@ -163,8 +163,8 @@ int _9p_attach( _9p_request_data_t * preq9p,
    }
 
   /* Get the related pentry */
-  memcpy( (char *)&fsdata.handle, (char *)pexport->proot_handle, sizeof( fsal_handle_t ) ) ;
-  fsdata.cookie = 0;
+  memcpy( (char *)&fsdata.fh_desc.start, (char *)pexport->proot_handle, sizeof( fsal_handle_t ) ) ;
+  fsdata.fh_desc.len = sizeof( fsal_handle_t ) ;
 
   /* refcount */
   if (pfid->pentry) {
