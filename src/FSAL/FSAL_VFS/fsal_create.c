fa1029fc6e2d87d2f4273e1b7d790bd62202c3b7
Philippe DENIEL <philippe.deniel@cea.fr>
2012-05-21 17:57:37 +0200

    FSAL_VFS: linkat2 does not use procfs anymore

--- src/FSAL/FSAL_VFS/fsal_create.c
+++ src/FSAL/FSAL_VFS/fsal_create.c
@@ -22,6 +22,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/* Dirty work-around to be used for managing NFS related link semantics */
+static int linkat2(int srcfd, int dirdestfd, char *destname)
+{
+   return linkat( srcfd, "", dirdestfd, destname, AT_EMPTY_PATH ) ;
+}
+
 /**
  * FSAL_create:
  * Create a regular file.
