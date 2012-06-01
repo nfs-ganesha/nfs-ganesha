d538693db69731ac7fc3b6bca12c1a394a740c3f
Matt Benjamin <matt@linuxbox.com>
2012-06-01 10:59:01 -0400

    Make tirpc.sh remove any existing libtirpc.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -5,6 +5,14 @@ OPWD=`pwd`
 TIRPC_REPO='git://github.com/mattbenjamin/libtirpc-lbx.git'
 TIRPC_BRANCH='duplex-6'
 
+# remove libtirpc if present;  try to avoid making
+# a mess
+if [ -d ../src -a -d ../contrib ]; then
+    if [ -e libtirpc ]; then
+	rm -rf libtirpc
+    fi
+fi
+
 git clone ${TIRPC_REPO} libtirpc
 cd libtirpc
 git checkout -b ${TIRPC_BRANCH} origin/${TIRPC_BRANCH}
