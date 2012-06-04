0c2f1ce2c4cb27eb58a43196d225caafecb8722c
Matt Benjamin <matt@linuxbox.com>
2012-06-04 12:10:30 -0400

    Specify git commit SHA in tirpc checkout.
    Fix the commit to check out in tirpc.sh.  Suggested by
    Boaz Harrosh <bharrosh@panasas.com>.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -3,7 +3,8 @@
 OPWD=`pwd`
 
 TIRPC_REPO='git://github.com/mattbenjamin/libtirpc-lbx.git'
-TIRPC_BRANCH='duplex-6'
+TIRPC_BRANCH='duplex-7'
+TIRPC_COMMIT='9474853b025725da4fb446415890e8273d847483'
 
 # remove libtirpc if present;  try to avoid making
 # a mess
@@ -15,7 +16,8 @@ fi
 
 git clone ${TIRPC_REPO} libtirpc
 cd libtirpc
-git checkout -b ${TIRPC_BRANCH} origin/${TIRPC_BRANCH}
+git checkout -b origin/${TIRPC_BRANCH}
+git checkout ${TIRPC_COMMIT}
 cd ${OPWD}
 
 ./autogen.sh
