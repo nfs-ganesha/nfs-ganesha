a1459d7dfed6910c3292697193202592881e3556
root <root@localhost.localdomain>
2012-06-15 11:00:41 -0400

    Permit rpmbuild to succeed on 5.x or 6.x.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>
    
    More rpm build changes.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -3,7 +3,7 @@
 OPWD=`pwd`
 
 TIRPC_REPO='git://github.com/mattbenjamin/libtirpc-lbx.git'
-TIRPC_BRANCH_NAME='duplex-7-helgrind'
+TIRPC_BRANCH_NAME='duplex-7'
 TIRPC_COMMIT='d1d73da9eff3616efc74659f9e9708114d861830'
 
 # remove libtirpc if present;  try to avoid making
