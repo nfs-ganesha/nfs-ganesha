e228b2b6dcd9b3bf0c7d729bfc4247f1f71c5386
Jim Lieb <jlieb@panasas.com>
2012-07-19 09:20:42 -0700

    Fix segv in nfs4_op_create
    move to a more reliable path to get the export handle.
    data->current_entry is not reliable yet in the function.
    pexport->export_hdl is there from export setup time.
    
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/Protocols/NFS/nfs4_op_create.c
+++ src/Protocols/NFS/nfs4_op_create.c
@@ -90,7 +90,7 @@ int nfs4_op_create(struct nfs_argop4 *op,
   char                   *name = NULL;
   char                   *link_content = NULL;
 #ifdef _USE_QUOTA
-  struct fsal_export    *exp_hdl = data->current_entry->obj_handle->export;
+  struct fsal_export    *exp_hdl;
   fsal_status_t          fsal_status;
 #endif
   cache_inode_create_arg_t create_arg;
@@ -108,6 +108,7 @@ int nfs4_op_create(struct nfs_argop4 *op,
 #ifdef _USE_QUOTA
   /* if quota support is active, then we should check is the FSAL allows
    * inode creation or not */
+  exp_hdl = data->pexport->export_hdl;
   fsal_status = exp_hdl->ops->check_quota(exp_hdl,
 					  data->pexport->fullpath,
 					  FSAL_QUOTA_INODES,
