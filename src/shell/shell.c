nfs-ganesha/nfs-ganesha

commit d52ea04bbdadb5b732e0aee9fe4256fcdc018f6c

Big commit that removes deprecated doxygen author statement. They credited the wrong person in 99% of the case. I left those who were pointed to actual author

src/shell/shell.c
@@ -25,7 +25,6 @@
 
 /**
  * \file    shell.c
- * \author  $Author: leibovic $
  * \date    $Date: 2006/02/24 08:33:44 $
  * \version $Revision: 1.20 $
  * \brief   Internal routines for the shell.
@@ -1966,7 +1965,6 @@ int shellcmd_set(int argc,      /* IN : number of args in argv */
 
       /* other variables */
 
-      if(!is_authorized_varname(varname))
         {
           snprintf(tracebuff, TRACEBUFFSIZE, "%s: Invalid variable name \"%s\".", argv[0],
                    varname);

src/shell/shell_vars.c
@@ -25,7 +25,6 @@
 
 /**
  * \file    shell_vars.c
- * \author  $Author: leibovic $
  * \date    $Date: 2006/01/24 13:49:33 $
  * \version $Revision: 1.8 $
  * \brief   variables management for the shell.
@@ -213,10 +212,8 @@ static void del_var(shell_variable_t * var)
 #define IS_LETTER_CAP(_c_) (((_c_) >= 'A') && ((_c_) <= 'Z'))
 #define IS_NUMERIC(_c_) (((_c_) >= '0') && ((_c_) <= '9'))
 
-/** indicates whether a name is authorized for a variable.
  *  A variable name must be in [a-zA-Z0-9._:]*
  */
-int is_authorized_varname(char *str)
 {
 
   int len = 0;

src/shell/shell_vars.h
@@ -1,7 +1,6 @@
 /**
  *
  * \file    shell_vars.h
- * \author  $Author: leibovic $
  * \date    $Date: 2005/05/09 12:23:55 $
  * \version $Revision: 1.2 $
  * \brief   variables management for the shell.
@@ -23,10 +22,8 @@
 
 #define MAX_VAR_LEN   32
 
-/** indicates whether a name is authorized for a variable.
  *  A variable name must be in [a-zA-Z0-9._:]*
  */
-int is_authorized_varname(char *str);
 
 /** returns the value for a variable,
  *  NULL if the variable doesn't exist.

commit 7d1d1548199cadbadf5c1d7911b4db7e1053fe2d

Undo a small regression produced by commit d52ea04bbdadb5b732e0aee9fe4256fcdc018f6c

src/shell/shell.c
@@ -1964,7 +1964,7 @@ int shellcmd_set(int argc,      /* IN : number of args in argv */
     {
 
       /* other variables */
-
+      if(!is_authorized_varname(varname))
         {
           snprintf(tracebuff, TRACEBUFFSIZE, "%s: Invalid variable name \"%s\".", argv[0],
                    varname);

src/shell/shell_vars.c
@@ -211,9 +211,11 @@ static void del_var(shell_variable_t * var)
 #define IS_LETTER(_c_) (((_c_) >= 'a') && ((_c_) <= 'z'))
 #define IS_LETTER_CAP(_c_) (((_c_) >= 'A') && ((_c_) <= 'Z'))
 #define IS_NUMERIC(_c_) (((_c_) >= '0') && ((_c_) <= '9'))
-
- *  A variable name must be in [a-zA-Z0-9._:]*
- */
+ 
+/** indicates whether a name is authorized for a variable.
+  *  A variable name must be in [a-zA-Z0-9._:]*
+  */
+int is_authorized_varname(char *str)
 {
 
   int len = 0;

src/shell/shell_vars.h
@@ -22,8 +22,11 @@
 
 #define MAX_VAR_LEN   32
 
+/** indicates whether a name is authorized for a variable.
  *  A variable name must be in [a-zA-Z0-9._:]*
  */
+int is_authorized_varname(char *str);
+
 
 /** returns the value for a variable,
  *  NULL if the variable doesn't exist.