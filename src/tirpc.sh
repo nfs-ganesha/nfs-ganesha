5b32d9e5aea7f4e5d58c09ef956a44474d27bfa1
Matt Benjamin <matt@linuxbox.com>
2012-09-04 16:35:27 -0400

    Track tirpc xp_getargs change, deadlock hotfix attempt.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>
    
    Conflicts:
    	src/tirpc.sh
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -4,7 +4,7 @@ OPWD=`pwd`
 
 TIRPC_REPO='git://github.com/mattbenjamin/libtirpc-lbx.git'
 TIRPC_BRANCH_NAME='duplex-7'
-TIRPC_COMMIT='be1b3df38f2f694d63f42af2a25bae93cffe3dee'
+TIRPC_COMMIT='51aeec8c821c2a12ae75908d1fe6e099dcc9d855'
 
 # remove libtirpc if present;  try to avoid making
 # a mess
