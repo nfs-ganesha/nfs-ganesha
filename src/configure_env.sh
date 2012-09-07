ed91c5795392cdfe289ecee798915c22fce505c5
Boaz Harrosh <bharrosh@panasas.com>
2012-09-07 11:20:33 +0300

    Update README.TIRPC && configure_env.sh
    [README.TIRPC]
    README.TIRPC had stale --enable-foo in it's ./configure command.
    Fix that by referencing to the configure_env.sh script instead.
    
    [configure_env.sh]
    I've put a more complete list of recommended ./configure switches.
    I've also put the full list of choices for the user to choose, as
    comments
    
    Please any one that edits configure.ac please update
    *configure_env.sh* to keep it uptodate as a document.
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>

--- src/configure_env.sh
+++ src/configure_env.sh
@@ -1,3 +1,70 @@
 #!/bin/sh 
 
-./configure --enable-strict-compile $* 
+#if making code changes please run with these at minimum
+
+CONFIG_ON="
+	--enable-strict-compile --enable-nlm --enable-quota --enable-rquota
+	--enable-dbus --enable-debug-symbols
+"
+
+./configure $CONFIG_ON
+
+# Broken
+# --enable-fsal_up
+# --enable-nfs4_acl
+
+# ON in a list
+# --enable-strict-compile
+# --enable-nlm
+# --enable-quota
+# --enable-rquota
+# --enable-dbus
+# --enable-debug-symbols
+
+# All options taken from ./configure --help
+# TODO: I made these by hand I should write an awk script to do this
+# automatically.
+#
+# --disable-libtool-lock
+# --disable-largefile
+# --enable-nlm
+# --enable-quota
+# --enable-rquota
+# --enable-dbus
+# --enable-upcall-simulato
+# --enable-cb-simulator
+# --disable-fsal-proxy
+# --disable-fsal-vfs
+# --disable-fsal-posix
+# --disable-ceph
+# --disable-lustre
+# --disable-shook
+# --enable-fsal-hpss
+# --enable-fsal-fuse
+# --enable-fsal-xfs
+# --enable-fsal-gpfs
+# --enable-fsal-zfs
+# --enable-fsal-shook
+# --enable-fsal-up
+# --enable-ipv6
+# --disable-nfsidmap
+# --enable-snmp-adm
+# --enable-error-injection
+# --enable-stat-exporter
+# --enable-efence
+# --disable-tcp-register
+# --disable-portmapper
+# --disable-xattr-director
+# --enable-debug-memleakse
+# --enable-debug-nfsshell
+# --enable-pl-pgsql
+# --enable-cache-path
+# --enable-handle-mapping
+# --enable-debug-symbols
+# --enable-profiling
+# --enable-strict-compile
+# --enable-nfs4-acl
+# --enable-mspac
+# --enable-9p
+# --enable-9p-rdma
+# --enable-strip-cs-uuid
