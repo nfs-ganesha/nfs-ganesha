a359020b7a9c1a2d3c17b1eeaf9855377d1896ff
Matt Benjamin <matt@linuxbox.com>
2012-09-28 15:04:09 -0400

    Use sys/xattr.h include.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/Protocols/9P/9p_xattrcreate.c
+++ src/Protocols/9P/9p_xattrcreate.c
@@ -18,7 +18,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  *
  * ---------------------------------------
  */
@@ -45,7 +46,7 @@
 #include <pthread.h>
 #include <sys/stat.h> 
 #include <sys/types.h>
-#include <attr/xattr.h>
+#include <sys/xattr.h>
 #include "nfs_core.h"
 #include "log.h"
 #include "cache_inode.h"
--- src/Protocols/9P/9p_xattrwalk.c
+++ src/Protocols/9P/9p_xattrwalk.c
@@ -18,7 +18,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  *
  * ---------------------------------------
  */
@@ -45,13 +46,16 @@
 #include <pthread.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <attr/xattr.h>
+#include <sys/xattr.h>
 #include "nfs_core.h"
 #include "log.h"
 #include "cache_inode.h"
 #include "fsal.h"
 #include "9p.h"
 
+#ifndef ENOATTR
+#define ENOATTR ENODATA
+#endif
 
 int _9p_xattrwalk( _9p_request_data_t * preq9p, 
                    void  * pworker_data,
