401c56bebe1d8ee87e19564c9e6b367a2d9f0990
Venkateswararao Jujjuri (JV) <jvrao@linux.vnet.ibm.com>
2012-10-30 13:29:50 -0700

    Moving to duplex-7 new git repo + fix for thie crash in xdr_CREATE3args
    Signed-off-by: Venkateswararao Jujjuri "<jvrao@linux.vnet.ibm.com>

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -2,9 +2,9 @@
 
 OPWD=`pwd`
 
-TIRPC_REPO='git://github.com/mattbenjamin/libtirpc-lbx.git'
-TIRPC_BRANCH_NAME='duplex-7-for-ah'
-TIRPC_COMMIT='161e4d06c06485a8bfaa30100ac0695b393e62dc'
+TIRPC_REPO='git://github.com/linuxbox2/ntirpc.git'
+TIRPC_BRANCH_NAME='duplex-7'
+TIRPC_COMMIT='0b216c807d416e3fcad6be27831a47162dddac91'
 
 # remove libtirpc if present;  try to avoid making
 # a mess
