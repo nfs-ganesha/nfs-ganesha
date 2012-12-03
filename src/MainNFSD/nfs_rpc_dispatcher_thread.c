10c52679ad88bcb234d8279c4b57a4414db53e35
Matt Benjamin <matt@linuxbox.com>
2012-12-03 15:50:21 -0500

    Initialize svc_params.gss_ctx_max_idle_gen.
    The 1024 value is the default.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/MainNFSD/nfs_rpc_dispatcher_thread.c
+++ src/MainNFSD/nfs_rpc_dispatcher_thread.c
@@ -499,6 +499,7 @@ void nfs_Init_svc()
     svc_params.gss_max_gc = 0;
     svc_params.warnx = NULL;
     svc_params.gss_ctx_hash_partitions = PRIME_ID_MAPPER;
+    svc_params.gss_max_idle_gen = 1024; /* GSS ctx cache expiration */
 
     svc_init(&svc_params);
 
