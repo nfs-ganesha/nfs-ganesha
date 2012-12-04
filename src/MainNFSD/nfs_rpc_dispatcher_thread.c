3e7c49910d00f2724e8b170439ba587bc8964de6
Matt Benjamin <matt@linuxbox.com>
2012-12-04 14:36:36 -0500

    Don't forget to initialize gss_max_gc in svc_param.
    Missed in earlier change.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/MainNFSD/nfs_rpc_dispatcher_thread.c
+++ src/MainNFSD/nfs_rpc_dispatcher_thread.c
@@ -500,6 +500,7 @@ void nfs_Init_svc()
     svc_params.warnx = NULL;
     svc_params.gss_ctx_hash_partitions = PRIME_ID_MAPPER;
     svc_params.gss_max_idle_gen = 1024; /* GSS ctx cache expiration */
+    svc_params.gss_max_gc = 200;
 
     svc_init(&svc_params);
 
