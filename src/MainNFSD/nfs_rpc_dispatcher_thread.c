6737b7bb7d3358e4b7f2fbeda247a5924174ffca
Matt Benjamin <matt@linuxbox.com>
2012-12-19 15:16:09 -0500

    Release xprt->xp_lock before re-arming.
    At present svc_rqst_rearm_events doesn't take xprt->xp_lock, but
    it does take sr_rec->mtx, which in the current order should be
    taken first.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/MainNFSD/nfs_rpc_dispatcher_thread.c
+++ src/MainNFSD/nfs_rpc_dispatcher_thread.c
@@ -862,10 +862,10 @@ thr_stallq(void *arg)
                 glist_del(&xu->stallq);
                 --(nfs_req_st.stallq.stalled);
                 xu->flags &= ~XPRT_PRIVATE_FLAG_STALLED;
+                pthread_mutex_unlock(&xprt->xp_lock);
                 (void) svc_rqst_rearm_events(xprt, SVC_RQST_FLAG_NONE);
                 /* drop stallq ref */
-                gsh_xprt_unref(xprt, XPRT_PRIVATE_FLAG_LOCKED);
-                /* !LOCKED */
+                gsh_xprt_unref(xprt, XPRT_PRIVATE_FLAG_NONE);
                 goto restart;
             }
         }
