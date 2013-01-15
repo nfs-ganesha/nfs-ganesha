91f7d7ba1d1002a9b12de30909e92936b4e4989b
Adam C. Emerson <aemerson@linuxbox.com>
2013-01-15 14:30:23 -0500

    Change FSAL VFS symlink refresh
    As discussed with Boaz, remove the use of a stack buffer and
    associated redundant copy.
    
    TODO: These FSALs currently use a 1K stack buffer in readlink and
          should receive a fix similar to this one:
    
          FSAL_POSIX
          FSAL_LUSTRE
          FSAL_ZFS
    
    Signed-off-by: Adam C. Emerson <aemerson@linuxbox.com>
    Reviewed-by: Boaz Harrosh <bharrosh@panasas.com>

--- src/FSAL/FSAL_VFS/handle.c
+++ src/FSAL/FSAL_VFS/handle.c
@@ -611,7 +611,7 @@ vfs_fsal_readlink(struct vfs_fsal_obj_handle *myself,
         int retval = 0;
         int fd;
         ssize_t retlink;
-        char link_buff[1024];
+	struct stat st;
 
         if(myself->u.symlink.link_content != NULL) {
                 gsh_free(myself->u.symlink.link_content);
@@ -622,25 +622,38 @@ vfs_fsal_readlink(struct vfs_fsal_obj_handle *myself,
         if(fd < 0)
                 return fd;
 
-        retlink = readlinkat(fd, "", link_buff, 1024);
-        retval = -errno;
-        close(fd);
-        if(retlink < 0) {
-                *fsal_error = posix2fsal_error(-retval);
-                return retval;
-        }
+        retval = fstat(fd, &st);
+	if (retval < 0) {
+		goto error;
+	}
 
-        myself->u.symlink.link_content = gsh_malloc(retlink + 1);
-        if(myself->u.symlink.link_content == NULL) {
-                *fsal_error = ERR_FSAL_NOMEM;
-                retval = -ENOMEM;
-        } else {
-                memcpy(myself->u.symlink.link_content, link_buff, retlink);
-                myself->u.symlink.link_content[retlink] = '\0';
-                myself->u.symlink.link_size = retlink + 1;
-                retval = 0;
+	myself->u.symlink.link_size = st.st_size + 1;
+	myself->u.symlink.link_content
+		= gsh_malloc(myself->u.symlink.link_size);
+	if (myself->u.symlink.link_content == NULL) {
+		goto error;
+	}
+
+        retlink = readlinkat(fd, "", myself->u.symlink.link_content,
+			     myself->u.symlink.link_size);
+        if(retlink < 0) {
+		goto error;
         }
+	myself->u.symlink.link_content[retlink] = '\0';
+        close(fd);
+
         return retval;
+
+error:
+        retval = -errno;
+	*fsal_error = posix2fsal_error(errno);
+	close(fd);
+	if (myself->u.symlink.link_content != NULL) {
+		gsh_free(myself->u.symlink.link_content);
+		myself->u.symlink.link_content = NULL;
+		myself->u.symlink.link_size = 0;
+	}
+	return retval;
 }
 
 static fsal_status_t readsymlink(struct fsal_obj_handle *obj_hdl,
