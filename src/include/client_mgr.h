d6e9100427e00ead3ff1f3082ab31f5996e8e7f9
Jim Lieb <jlieb@panasas.com>
2013-01-29 14:56:30 -0800

    Add client manager
    The client manager keeps track of active protocol client hosts
    
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

---
+++ src/include/client_mgr.h
@@ -0,0 +1,60 @@
+/*
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright (C) Panasas Inc., 2013
+ * Author: Jim Lieb jlieb@panasas.com
+ *
+ * contributeur : Philippe DENIEL   philippe.deniel@cea.fr
+ *                Thomas LEIBOVICI  thomas.leibovici@cea.fr
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * -------------
+ */
+
+/**
+ * @defgroup Client host management
+ * @{
+ */
+
+/**
+ * @file client_mgr.h
+ * @author Jim Lieb <jlieb@panasas.com>
+ * @brief Client manager
+ */
+
+#ifndef CLIENT_MGR_H
+#define CLIENT_MGR_H
+
+struct gsh_client {
+	struct avltree_node node_k;
+	pthread_mutex_t lock;
+	struct gsh_buffdesc addr;
+	int64_t refcnt;
+	nsecs_elapsed_t last_update;
+	unsigned char addrbuf[];
+};
+
+void gsh_client_init(void);
+struct gsh_client *get_gsh_client(sockaddr_t *client_ipaddr,
+				  bool lookup_only);
+void put_gsh_client(struct gsh_client *client);
+int foreach_gsh_client(bool (*cb)(struct gsh_client *cl,
+				   void *state),
+		       void *state);
+
+#endif /* !CLIENT_MGR_H */
+/** @} */
