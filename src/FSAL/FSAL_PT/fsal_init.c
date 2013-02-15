b7be3565b00c1b42dea88ac9a1a166d9a570505f
Matt Cleveland <mgclevel@us.ibm.com>
2013-02-15 21:51:37 +0000

    Addressed review of dynamic ccl open branch. Signed-off-by: Matt Cleveland <mgclevel@us.ibm.com>

--- src/FSAL/FSAL_PT/fsal_init.c
+++ src/FSAL/FSAL_PT/fsal_init.c
@@ -76,6 +76,8 @@ struct acl_handles_struct_t  * g_fsi_acl_handles_fsal;
 
 #define COMPONENT_FSAL_PT  5   // COMPONENT_FSAL
 
+#define CCL_SO_PATH "/usr/lib64/libfsi_ipc_ccl.so"
+
 int PTFSAL_log(int level, const char * message)
 {
   DisplayLogComponentLevel(COMPONENT_FSAL_PT, "FSAL_PT", level,
@@ -194,7 +196,7 @@ PTFSAL_Init(fsal_parameter_t * init_info    /* IN */)
   Return(ERR_FSAL_NO_ERROR, 0, INDEX_FSAL_Init);
 }
 
-void
+char *
 check_dl_error(const char * func_name)
 {
   char * error = dlerror();
@@ -204,12 +206,15 @@ check_dl_error(const char * func_name)
       func_name = "UNKNOWN";
     }
 
-    FSI_TRACE(FSI_FATAL, "Failed to dynamically load function: %s",
-              func_name);
+    FSI_TRACE(FSI_FATAL, "Failed to dynamically load function: %s, error: %s",
+              func_name, error);
+    return error;
+  } else {
+    return NULL;
   }
 }
 
-void
+char *
 load_dynamic_function(void       * fn_map_ptr,
                       const char * func_name)
 {
@@ -222,158 +227,152 @@ load_dynamic_function(void       * fn_map_ptr,
   *(void **)(fn_map_ptr) = dlsym(g_ccl_lib_handle, func_name);
   
   /* check for error */
-  check_dl_error(func_name);
+  char * error_string = check_dl_error(func_name);
+  
+  return error_string;
 }
 
 int
 pt_ganesha_fsal_ccl_init()
 {
-  g_ccl_lib_handle = dlopen("/usr/lib64/libfsi_ipc_ccl.so", RTLD_LAZY);
+  g_ccl_lib_handle = dlopen(CCL_SO_PATH, RTLD_LAZY);
   
   /* clearn any existing error */
   dlerror();
 
   /* load all CCL function pointers */
-  load_dynamic_function(&g_ccl_function_map.init_fn,
-                        "ccl_init");
-  load_dynamic_function(&g_ccl_function_map.check_handle_index_fn,
-                        "ccl_check_handle_index");
-  load_dynamic_function(&g_ccl_function_map.find_handle_by_name_and_export_fn,
-                        "ccl_find_handle_by_name_and_export");
-  load_dynamic_function(&g_ccl_function_map.stat_fn,
-                        "ccl_stat");
-  load_dynamic_function(&g_ccl_function_map.fstat_fn,
-                        "ccl_fstat");
-  load_dynamic_function(&g_ccl_function_map.stat_by_handle_fn,
-                        "ccl_stat_by_handle");
-  load_dynamic_function(&g_ccl_function_map.rcv_msg_nowait_fn,
-                        "rcv_msg_nowait");
-  load_dynamic_function(&g_ccl_function_map.rcv_msg_wait_fn,
-                        "rcv_msg_wait");
-  load_dynamic_function(&g_ccl_function_map.rcv_msg_wait_block_fn,
-                        "rcv_msg_wait_block");
-  load_dynamic_function(&g_ccl_function_map.send_msg_fn,
-                        "send_msg");
-  load_dynamic_function(&g_ccl_function_map.chmod_fn,
-                        "ccl_chmod");
-  load_dynamic_function(&g_ccl_function_map.chown_fn,
-                        "ccl_chown");
-  load_dynamic_function(&g_ccl_function_map.ntimes_fn,
-                        "ccl_ntimes");
-  load_dynamic_function(&g_ccl_function_map.mkdir_fn,
-                        "ccl_mkdir");
-  load_dynamic_function(&g_ccl_function_map.rmdir_fn,
-                        "ccl_rmdir");
-  load_dynamic_function(&g_ccl_function_map.get_real_filename_fn,
-                        "ccl_get_real_filename");
-  load_dynamic_function(&g_ccl_function_map.disk_free_fn,
-                        "ccl_disk_free");
-  load_dynamic_function(&g_ccl_function_map.unlink_fn,
-                        "ccl_unlink");
-  load_dynamic_function(&g_ccl_function_map.rename_fn,
-                        "ccl_rename");
-  load_dynamic_function(&g_ccl_function_map.opendir_fn,
-                        "ccl_opendir");
-  load_dynamic_function(&g_ccl_function_map.closedir_fn,
-                        "ccl_closedir");
-  load_dynamic_function(&g_ccl_function_map.readdir_fn,
-                        "ccl_readdir");
-  load_dynamic_function(&g_ccl_function_map.seekdir_fn,
-                        "ccl_seekdir");
-  load_dynamic_function(&g_ccl_function_map.telldir_fn,
-                        "ccl_telldir");
-  load_dynamic_function(&g_ccl_function_map.chdir_fn,
-                        "ccl_chdir");
-  load_dynamic_function(&g_ccl_function_map.fsync_fn,
-                        "ccl_fsync");
-  load_dynamic_function(&g_ccl_function_map.ftruncate_fn,
-                        "ccl_ftruncate");
-  load_dynamic_function(&g_ccl_function_map.pread_fn,
-                        "ccl_pread");
-  load_dynamic_function(&g_ccl_function_map.pwrite_fn,
-                        "ccl_pwrite");
-  load_dynamic_function(&g_ccl_function_map.open_fn,
-                        "ccl_open");
-  load_dynamic_function(&g_ccl_function_map.close_fn,
-                        "ccl_close");
-  load_dynamic_function(&g_ccl_function_map.get_any_io_responses_fn,
-                        "get_any_io_responses");
-  load_dynamic_function(&g_ccl_function_map.ipc_stats_logger_fn,
-                        "ccl_ipc_stats_logger");
-  load_dynamic_function(&g_ccl_function_map.update_stats_fn,
-                        "update_stats");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_get_entry_fn,
-                        "ccl_sys_acl_get_entry");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_get_tag_type_fn,
-                        "ccl_sys_acl_get_tag_type");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_get_permset_fn,
-                        "ccl_sys_acl_get_permset");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_get_qualifier_fn,
-                        "ccl_sys_acl_get_qualifier");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_get_file_fn,
-                        "ccl_sys_acl_get_file");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_clear_perms_fn,
-                        "ccl_sys_acl_clear_perms");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_add_perm_fn,
-                        "ccl_sys_acl_add_perm");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_init_fn,
-                        "ccl_sys_acl_init");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_create_entry_fn,
-                        "ccl_sys_acl_create_entry");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_set_tag_type_fn,
-                        "ccl_sys_acl_set_tag_type");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_set_qualifier_fn,
-                        "ccl_sys_acl_set_qualifier");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_set_permset_fn,
-                        "ccl_sys_acl_set_permset");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_set_file_fn,
-                        "ccl_sys_acl_set_file");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_delete_def_file_fn,
-                        "ccl_sys_acl_delete_def_file");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_get_perm_fn,
-                        "ccl_sys_acl_get_perm");
-  load_dynamic_function(&g_ccl_function_map.sys_acl_free_acl_fn,
-                        "ccl_sys_acl_free_acl");
-  load_dynamic_function(&g_ccl_function_map.name_to_handle_fn,
-                        "ccl_name_to_handle");
-  load_dynamic_function(&g_ccl_function_map.handle_to_name_fn,
-                        "ccl_handle_to_name");
-  load_dynamic_function(&g_ccl_function_map.dynamic_fsinfo_fn,
-                        "ccl_dynamic_fsinfo");
-  load_dynamic_function(&g_ccl_function_map.readlink_fn,
-                        "ccl_readlink");
-  load_dynamic_function(&g_ccl_function_map.symlink_fn,
-                        "ccl_symlink");
-  load_dynamic_function(&g_ccl_function_map.update_handle_nfs_state_fn,
-                        "ccl_update_handle_nfs_state");
-  load_dynamic_function(&g_ccl_function_map.fsal_try_stat_by_index_fn,
-                        "ccl_fsal_try_stat_by_index");
-  load_dynamic_function(&g_ccl_function_map.fsal_try_fastopen_by_index_fn,
-                        "ccl_fsal_try_fastopen_by_index");
-  load_dynamic_function(&g_ccl_function_map.find_oldest_handle_fn,
-                        "ccl_find_oldest_handle");
-  load_dynamic_function(&g_ccl_function_map.can_close_handle_fn,
-                        "ccl_can_close_handle");
-  load_dynamic_function(&g_ccl_function_map.up_mutex_lock_fn,
-                        "ccl_up_mutex_lock");
-  load_dynamic_function(&g_ccl_function_map.up_mutex_unlock_fn,
-                        "ccl_up_mutex_unlock");
-  load_dynamic_function(&g_ccl_function_map.log_fn,
-                        "ccl_log");
-  load_dynamic_function(&g_fsal_fsi_handles,
-                        "g_fsi_handles");
+#define DL_LOAD(func_ptr, func_name)                                   \
+  ((load_return = load_dynamic_function(func_ptr, func_name)) == NULL)
+
+  char * load_return = NULL;
+
+  if (DL_LOAD(&g_ccl_function_map.init_fn, "ccl_init")                     &&
+      DL_LOAD(&g_ccl_function_map.check_handle_index_fn,
+	      "ccl_check_handle_index")                                    &&
+      DL_LOAD(&g_ccl_function_map.find_handle_by_name_and_export_fn,
+	      "ccl_find_handle_by_name_and_export")                        &&
+      DL_LOAD(&g_ccl_function_map.stat_fn, "ccl_stat")                     &&
+      DL_LOAD(&g_ccl_function_map.fstat_fn, "ccl_fstat")                   &&
+      DL_LOAD(&g_ccl_function_map.stat_by_handle_fn, "ccl_stat_by_handle") &&
+      DL_LOAD(&g_ccl_function_map.rcv_msg_nowait_fn, "rcv_msg_nowait")     &&
+      DL_LOAD(&g_ccl_function_map.rcv_msg_wait_fn, "rcv_msg_wait")         &&
+      DL_LOAD(&g_ccl_function_map.rcv_msg_wait_block_fn,
+	      "rcv_msg_wait_block")                                        &&
+      DL_LOAD(&g_ccl_function_map.send_msg_fn, "send_msg")                 &&
+      DL_LOAD(&g_ccl_function_map.chmod_fn, "ccl_chmod")                   &&
+      DL_LOAD(&g_ccl_function_map.chown_fn, "ccl_chown")                   &&
+      DL_LOAD(&g_ccl_function_map.ntimes_fn, "ccl_ntimes")                 &&
+      DL_LOAD(&g_ccl_function_map.mkdir_fn, "ccl_mkdir")                   &&
+      DL_LOAD(&g_ccl_function_map.rmdir_fn, "ccl_rmdir")                   &&
+      DL_LOAD(&g_ccl_function_map.get_real_filename_fn,
+	      "ccl_get_real_filename")                                     &&
+      DL_LOAD(&g_ccl_function_map.disk_free_fn, "ccl_disk_free")           &&
+      DL_LOAD(&g_ccl_function_map.unlink_fn, "ccl_unlink")                 &&
+      DL_LOAD(&g_ccl_function_map.rename_fn, "ccl_rename")                 &&
+      DL_LOAD(&g_ccl_function_map.opendir_fn, "ccl_opendir")               &&
+      DL_LOAD(&g_ccl_function_map.closedir_fn, "ccl_closedir")             &&
+      DL_LOAD(&g_ccl_function_map.readdir_fn, "ccl_readdir")               &&
+      DL_LOAD(&g_ccl_function_map.seekdir_fn, "ccl_seekdir")               &&
+      DL_LOAD(&g_ccl_function_map.telldir_fn, "ccl_telldir")               &&
+      DL_LOAD(&g_ccl_function_map.chdir_fn, "ccl_chdir")                   &&
+      DL_LOAD(&g_ccl_function_map.fsync_fn, "ccl_fsync")                   &&
+      DL_LOAD(&g_ccl_function_map.ftruncate_fn, "ccl_ftruncate")           &&
+      DL_LOAD(&g_ccl_function_map.pread_fn, "ccl_pread")                   &&
+      DL_LOAD(&g_ccl_function_map.pwrite_fn, "ccl_pwrite")                 &&
+      DL_LOAD(&g_ccl_function_map.open_fn, "ccl_open")                     &&
+      DL_LOAD(&g_ccl_function_map.close_fn, "ccl_close")                   &&
+      DL_LOAD(&g_ccl_function_map.get_any_io_responses_fn,
+	      "get_any_io_responses")                                      &&
+      DL_LOAD(&g_ccl_function_map.ipc_stats_logger_fn,
+	      "ccl_ipc_stats_logger")                                      &&
+      DL_LOAD(&g_ccl_function_map.update_stats_fn, "update_stats")         &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_get_entry_fn,
+	      "ccl_sys_acl_get_entry")                                     &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_get_tag_type_fn,
+	      "ccl_sys_acl_get_tag_type")                                  &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_get_permset_fn,
+	      "ccl_sys_acl_get_permset")                                   &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_get_qualifier_fn,
+	      "ccl_sys_acl_get_qualifier")                                 &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_get_file_fn,
+	      "ccl_sys_acl_get_file")                                      &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_clear_perms_fn,
+	      "ccl_sys_acl_clear_perms")                                   &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_add_perm_fn,
+	      "ccl_sys_acl_add_perm")                                      &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_init_fn, "ccl_sys_acl_init")     &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_create_entry_fn,
+	      "ccl_sys_acl_create_entry")                                  &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_set_tag_type_fn,
+	      "ccl_sys_acl_set_tag_type")                                  &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_set_qualifier_fn,
+	      "ccl_sys_acl_set_qualifier")                                 &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_set_permset_fn,
+	      "ccl_sys_acl_set_permset")                                   &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_set_file_fn,
+	      "ccl_sys_acl_set_file")                                      &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_delete_def_file_fn,
+	      "ccl_sys_acl_delete_def_file")                               &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_get_perm_fn,
+	      "ccl_sys_acl_get_perm")                                      &&
+      DL_LOAD(&g_ccl_function_map.sys_acl_free_acl_fn,
+	      "ccl_sys_acl_free_acl")                                      &&
+      DL_LOAD(&g_ccl_function_map.name_to_handle_fn, "ccl_name_to_handle") &&
+      DL_LOAD(&g_ccl_function_map.handle_to_name_fn, "ccl_handle_to_name") &&
+      DL_LOAD(&g_ccl_function_map.dynamic_fsinfo_fn, "ccl_dynamic_fsinfo") &&
+      DL_LOAD(&g_ccl_function_map.readlink_fn, "ccl_readlink")             &&
+      DL_LOAD(&g_ccl_function_map.symlink_fn, "ccl_symlink")               &&
+      DL_LOAD(&g_ccl_function_map.update_handle_nfs_state_fn,
+	      "ccl_update_handle_nfs_state")                               &&
+      DL_LOAD(&g_ccl_function_map.fsal_try_stat_by_index_fn,
+	      "ccl_fsal_try_stat_by_index")                                &&
+      DL_LOAD(&g_ccl_function_map.fsal_try_fastopen_by_index_fn,
+	      "ccl_fsal_try_fastopen_by_index")                            &&
+      DL_LOAD(&g_ccl_function_map.find_oldest_handle_fn,
+	      "ccl_find_oldest_handle")                                    &&
+      DL_LOAD(&g_ccl_function_map.can_close_handle_fn,
+	      "ccl_can_close_handle")                                      &&
+      DL_LOAD(&g_ccl_function_map.up_mutex_lock_fn, "ccl_up_mutex_lock")   &&
+      DL_LOAD(&g_ccl_function_map.up_mutex_unlock_fn,
+	      "ccl_up_mutex_unlock")                                       &&
+      DL_LOAD(&g_ccl_function_map.log_fn, "ccl_log")                       &&
+      DL_LOAD(&g_fsal_fsi_handles, "g_fsi_handles")                        
+      ) {
+    FSI_TRACE(FSI_NOTICE, "Successfully loaded CCL function pointers");
+  } else {
+    FSI_TRACE(FSI_FATAL, "Failed to load function: %s", load_return);
+    return -1;
+  }
+    
+#undef DL_LOAD
 
   /* load and map variables that reside in the CCL shared library */
   void * g_shm_at_obj = dlsym(g_ccl_lib_handle, "g_shm_at");
+  if (!g_shm_at_obj) {
+    FSI_TRACE(FSI_FATAL, "Failed to load symbol g_shm_at");
+    return -1;
+  }
   g_shm_at_fsal = (char *)g_shm_at_obj;
 
   void * g_fsi_handles_obj = dlsym(g_ccl_lib_handle, "g_fsi_handles");
+  if (!g_fsi_handles_obj) {
+    FSI_TRACE(FSI_FATAL, "Failed to load symbol g_fsi_handles");
+    return -1;
+  }
   g_fsi_handles_fsal = (struct file_handles_struct_t *)g_fsi_handles_obj;
 
   void * g_fsi_dir_handles_obj = dlsym(g_ccl_lib_handle, "g_fsi_dir_handles");
-  g_fsi_dir_handles_fsal = (struct file_handles_struct_t *)g_fsi_handles_obj;
+  if (!g_fsi_dir_handles_obj) {
+    FSI_TRACE(FSI_FATAL, "Failed to load symbol g_fsi_dir_handles");
+    return -1;
+  }
+  g_fsi_dir_handles_fsal = (struct file_handles_struct_t *)g_fsi_dir_handles_obj;
 
   void * g_fsi_acl_handles_obj = dlsym(g_ccl_lib_handle, "g_fsi_acl_handles");
+  if (!g_fsi_acl_handles_obj) {
+    FSI_TRACE(FSI_FATAL, "Failed to load g_fsi_acl_handles");
+    return -1;
+  }
   g_fsi_acl_handles_fsal = (struct file_handles_struct_t *)g_fsi_handles_obj;
   
   return 0;
