5e890b9b8afb05befdd7487fc7ed792e826d4f47
Philippe DENIEL <philippe.deniel@cea.fr>
2013-05-06 16:40:01 +0200

    Jenkins: Add sigmund's test (cherry picked from commit d7c335a74a88f8d1bca92b65c7749c15417c9890)
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- run_jenkins_compilation.sh
+++ run_jenkins_compilation.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh 
 #
 # External variables (to be set by Jenkins) are:
 #  - FSAL (VFS,XFS,ZFS,GPFS,POSIX,HPSS,LUSTRE,CEPH,PROXY,FUSE,DYNFSAL)
@@ -6,48 +6,7 @@
 #  - sharedfsal (dynamic,static) 
 #
 BASE=`pwd`
+mkdir -p build 
+cd build 
+cmake CCMAKE_BUILD_TYPE=Debug $BASE/src && make && make cppcheck-xml
 
-cd src
-autoreconf --install || exit 1 
-
-failed="FALSE"
-
-if [[  $sharedfsal == "dynamic" ]] ; then
-  DYNOPT=" --enable-buildsharedfsal"
-else
-  DYNOPT=""
-fi
-
-# Deal with MORE_OPT
-
-MORE_OPT=""
-
-if [[ $FSAL == "GPFS" ]] ; then
-  MORE_OPT=" --enable-fsal-up"
-fi
-
-if [[ $FSAL == "PROXY" ]] ; then 
-  MORE_OPT=" --enable-handle-mapping"
-fi
-  
-
-REGULAR_OPT="--with-nfs4-minorversion=1 --enable-nlm --enable-snmp-adm"
-
-./configure --with-fsal=$FSAL --with-mfsl=$MFSL  $REGULAR_OPT $MORE_OPT $DYNOPT|| exit 1 
-
-
-make -j 2 || make -j 2 || make || failed="TRUE"
-
-make clean
-
-cd ..
-
-# If many compilation occurs, clean the workspace
-rm -fr ./*
-
-
-if [[ $failed == "TRUE" ]] ; then
-  exit 1
-else
-  exit 0 
-fi
