e84c9d50aca4a9e7452bda0a1c77977d6bc5a3bb
Matt Benjamin <matt@linuxbox.com>
2013-06-10 11:56:06 -0700

    Declare nfs_rpc_rdvs mtx static.
    This was always intended to be static.  Reported by Maxim
    Gorbachyov <maxim.gorbachyov@gmail.com>.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>
    Signed-off-by: Venkateswararao Jujjuri (JV) <jvrao@linux.vnet.ibm.com>

--- src/MainNFSD/nfs_rpc_dispatcher_thread.c
+++ src/MainNFSD/nfs_rpc_dispatcher_thread.c
@@ -764,7 +764,7 @@ static u_int nfs_rpc_rdvs(SVCXPRT *xprt, SVCXPRT *newxprt, const u_int flags,
                           void *u_data)
 {
     static uint32_t next_chan = TCP_EVCHAN_0;
-    pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+    static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
     uint32_t tchan;
 
     pthread_mutex_lock(&mtx);
