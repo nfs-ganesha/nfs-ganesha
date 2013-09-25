c040e044dd66cdfce54c23b3423ee6c2105de89a
Frank S. Filz <ffilzlnx@mindspring.com>
2013-09-25 12:06:55 -0400

    Do minimum implementation for RECLAIM_COMPLETE
    We do not shorten grace period, nor do we handle RECLAIM_COMPLETE for
    individual file system, but we now pass all existing pynfs 4.1
    reclaim_complete tests.
    
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/Protocols/NFS/nfs4_op_open.c
+++ src/Protocols/NFS/nfs4_op_open.c
@@ -385,7 +385,9 @@ open4_validate_claim(compound_data_t   * data,
 
         switch (claim) {
         case CLAIM_NULL:
-                if (nfs_in_grace()) {
+                if (nfs_in_grace() ||
+                    ((data->minorversion > 0) &&
+                      !clientid->cid_cb.v41.cid_reclaim_complete)) {
                         status = NFS4ERR_GRACE;
                 }
                 break;
@@ -401,7 +403,9 @@ open4_validate_claim(compound_data_t   * data,
 
         case CLAIM_PREVIOUS:
                 if ((clientid->cid_allow_reclaim != 1) ||
-                    !nfs_in_grace()) {
+                    !nfs_in_grace() ||
+                    ((data->minorversion > 0) &&
+                      clientid->cid_cb.v41.cid_reclaim_complete)) {
                         status = NFS4ERR_NO_GRACE;
                 }
                 break;
