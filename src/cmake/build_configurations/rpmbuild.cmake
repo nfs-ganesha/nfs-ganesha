90eaf49768f1d68c131155e374be009429b79c2b
Philippe DENIEL <philippe.deniel@cea.fr>
2013-11-20 16:03:02 +0100

    RPM: new way of managing RPM.
    The rpm spec file is no more generated on the fly.
    It becomes a configure_file managed by CMake (name of the source tarball has to be set in it).
    The spec file is now part of the generated tarball, making it possible to run "rpmbuild -ta <tgz file>"
    
    By default, the specfile will generate libs only for FSAL_VFS and FSAL_PROXY. More fsals can be compiled
    and packaged by using the "--with" option in rpmbuild.
    For example: rpmbuild --with lustre -ta <tgz file>
    
    This option works for rpmbuild --rebuild : rpmbuild --with xfs --rebuild <src.rpm file>
    
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- src/cmake/build_configurations/rpmbuild.cmake
+++ src/cmake/build_configurations/rpmbuild.cmake
@@ -1,20 +1,3 @@
 # Turn on everything in the options for a complete build
 
-set(_HANDLE_MAPPING ON)
-set(_NO_XATTRD OFF)
-set(USE_DBUS OFF)
-set(USE_DBUS_STATS OFF)
-
-# Turn all FSALs ON, if one can't be
-# built, cmake will turn it OFF
-set( USE_FSAL_XFS ON)
-set( USE_FSAL_ZFS ON)
-set( USE_FSAL_CEPH ON)
-set( USE_FSAL_GPFS OFF)
-set( USE_FSAL_LUSTRE ON)
-set( USE_FSAL_SHOOK ON)
-
-# FSAL HPSS is still a special case for the moment
-set(USE_FSAL_HPSS OFF)
-
 message(STATUS "Building RPM")
