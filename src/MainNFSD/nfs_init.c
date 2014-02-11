f13b79aa15224769b16ea7f4696fa7596ed6ec37
Dominique Martinet <dominique.martinet@cea.fr>
2014-02-11 08:51:25 +0100

    nfs_init: s/ifdef LINUX/ifdef USE_CAPS/ to include libcap headers
    Signed-off-by: Dominique Martinet <dominique.martinet@cea.fr>

--- src/MainNFSD/nfs_init.c
+++ src/MainNFSD/nfs_init.c
@@ -70,7 +70,7 @@
 #include "delayed_exec.h"
 #include "client_mgr.h"
 #include "export_mgr.h"
-#ifdef LINUX
+#ifdef USE_CAPS
 #include <sys/capability.h>	/* For capget/capset */
 #endif
 #include "uid2grp.h"
