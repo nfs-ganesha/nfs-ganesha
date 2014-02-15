8138f329274dbfca82beff34b6302583d7a80e3b
Jim Lieb <jlieb@panasas.com>
2014-02-14 18:09:16 -0800

    Convert LUSTRE fsal to replace fs_options with parse_node
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/FSAL/FSAL_LUSTRE/export.c
+++ src/FSAL/FSAL_LUSTRE/export.c
@@ -550,7 +550,7 @@ void lustre_export_ops_init(struct export_ops *ops)
 
 fsal_status_t lustre_create_export(struct fsal_module *fsal_hdl,
 				   const char *export_path,
-				   const char *fs_options,
+				   void *parse_node,
 				   struct exportlist *exp_entry,
 				   struct fsal_module *next_fsal,
 				   const struct fsal_up_vector *up_ops,
