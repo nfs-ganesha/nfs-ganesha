87a7d09094180c77364901e7a4fb4441acc6cf54
Jim Lieb <jlieb@panasas.com>
2014-03-03 14:11:18 -0800

    Fix quota.h handling in LUSTRE
    This is fixed based on examination of headers in lustre 2.4.2.
    CMakeLists.txt is fixed so config.h sets its defines properly.
    Built with lustre-client-2.4.2-2.6.32_358.23.2.el6 from whamcloud.com.
    
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/FSAL/FSAL_LUSTRE/export.c
+++ src/FSAL/FSAL_LUSTRE/export.c
@@ -49,13 +49,13 @@
 
 #ifdef HAVE_INCLUDE_LUSTREAPI_H
 #include <lustre/lustreapi.h>
-#include <lustre/lustre_user.h>
 #else
 #ifdef HAVE_INCLUDE_LIBLUSTREAPI_H
 #include <lustre/liblustreapi.h>
+#endif
+#endif
 #include <lustre/lustre_user.h>
-#endif
-#endif
+#include <linux/quota.h>
 
 /*
  * VFS internal export
