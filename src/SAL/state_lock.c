045658d5d9d04b75905a06564bccb3d92a4a83d2
Malahal Naineni <malahal@us.ibm.com>
2014-03-28 16:55:47 -0500

    state_test doesn't need to open a file in read-write mode.
    This just gets details about an existing lock, if any. There is no need
    to open a file in read-write mode here. RDONLY mode should be just fine.
    
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

--- src/SAL/state_lock.c
+++ src/SAL/state_lock.c
@@ -2397,7 +2397,7 @@ state_status_t state_test(cache_entry_t *entry, exportlist_t *export,
 		return status;
 	}
 
-	cache_status = cache_inode_open(entry, FSAL_O_RDWR, req_ctx, 0);
+	cache_status = cache_inode_open(entry, FSAL_O_READ, req_ctx, 0);
 	if (cache_status != CACHE_INODE_SUCCESS) {
 		status = cache_inode_status_to_state_status(cache_status);
 		LogFullDebug(COMPONENT_STATE, "Could not open file");
