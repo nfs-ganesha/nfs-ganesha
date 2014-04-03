518b5c05833959f0e30d52a9ed8de93fac64b29d
Frank S. Filz <ffilzlnx@mindspring.com>
2014-04-03 13:24:19 -0400

    Add open_dir_by_path_walk for safe implementation of lookup_path
    FSAL lookup_path implementations (which are called to get the root
    handle of an export at init time AND for v3 mounts of a sub-directory of
    the export) are vulnerable to two attacks:
    
    - First, if a symlink is in the path other than as the last element, the
    old code does not reject the lookup.
    
    - Second, and worse, the .. entry is not looked for...
    
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/include/FSAL/fsal_commonlib.h
+++ src/include/FSAL/fsal_commonlib.h
@@ -1,4 +1,43 @@
 /*
+ *
+ *
+ *
+ * Copyright CEA/DAM/DIF  (2008)
+ * contributeur : Philippe DENIEL   philippe.deniel@cea.fr
+ *                Thomas LEIBOVICI  thomas.leibovici@cea.fr
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * ---------------------------------------
+ */
+
+/**
+ * @defgroup FSAL File-System Abstraction Layer
+ * @{
+ */
+
+/**
+ * @file  fsal_commomnlib.h
+ * @brief Miscelaneous FSAL common library routines
+ */
+
+#ifndef FSAL_COMMONLIB_H
+#define FSAL_COMMONLIB_H
+
+/*
  * fsal common utility functions
  */
 
@@ -34,3 +73,7 @@ int fsal_obj_handle_uninit(struct fsal_obj_handle *obj);
 void fsal_ds_handle_init(struct fsal_ds_handle *, struct fsal_ds_ops *,
 			 struct fsal_module *);
 int fsal_ds_handle_uninit(struct fsal_ds_handle *ds);
+
+int open_dir_by_path_walk(int first_fd, const char *path, struct stat *stat);
+
+#endif				/* FSAL_COMMONLIB_H */
