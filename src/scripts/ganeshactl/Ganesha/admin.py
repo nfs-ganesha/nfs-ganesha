0926252b287029ede5acdd408ed63e3af995bc18
Jim Lieb <jlieb@panasas.com>
2014-04-24 19:13:13 -0700

    Add GPL banner to ganeshactl python code
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/scripts/ganeshactl/Ganesha/admin.py
+++ src/scripts/ganeshactl/Ganesha/admin.py
@@ -1,3 +1,24 @@
+#
+# admin.py - AdminInterface DBUS object class.
+#
+# Copyright (C) 2014 Panasas Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Jim Lieb <jlieb@panasas.com>
+#-*- coding: utf-8 -*-
+
 from PyQt4 import QtCore, QtDBus
 
 class AdminInterface(QtDBus.QDBusAbstractInterface):
