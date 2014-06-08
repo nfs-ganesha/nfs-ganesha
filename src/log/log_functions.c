nfs-ganesha/nfs-ganesha

commit 344de4ab632713145194e936a8fb3568b9538321

Check return values from pthread_rwlock_xxlock() functions
Converted pthread_rwlock_xxlock functions with PTHREAD_RWLOCK_xxlock
macros that check the return value. Covers coverity CID# 30226, 30227,
60332, 60333

Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
Reviewed-by: Frank S. Filz <ffilzlnx@mindspring.com>

src/log/log_functions.c
@@ -677,12 +677,12 @@ int create_log_facility(char *name,
 			return -rc;
 		}
 	}
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 
 	facility = find_log_facility(name);
 
 	if (facility != NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 
 		LogInfo(COMPONENT_LOG, "Facility %s already exists", name);
 
@@ -692,7 +692,7 @@ int create_log_facility(char *name,
 	facility = gsh_calloc(1, sizeof(*facility));
 
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 
 		LogCrit(COMPONENT_LOG, "Can not allocate a facility for %s",
 			 name);
@@ -706,7 +706,7 @@ int create_log_facility(char *name,
 	if (log_func == log_to_file && private != NULL) {
 		facility->lf_private = gsh_strdup(private);
 		if (facility->lf_private == NULL) {
-			pthread_rwlock_unlock(&log_rwlock);
+			PTHREAD_RWLOCK_unlock(&log_rwlock);
 			gsh_free(facility);
 
 			return -ENOMEM;
@@ -715,7 +715,7 @@ int create_log_facility(char *name,
 		facility->lf_private = private;
 	glist_add_tail(&facility_list, &facility->lf_list);
 
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 
 	LogInfo(COMPONENT_LOG, "Created log facility %s",
 		facility->lf_name);
@@ -740,17 +740,17 @@ void release_log_facility(char *name)
 {
 	struct log_facility *facility;
 
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 	facility = find_log_facility(name);
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "Attempting release of non-existant log facility (%s)",
 			 name);
 		return;
 	}
 	if (facility == default_facility) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "Attempting to release default log facility (%s)",
 			 name);
@@ -759,7 +759,7 @@ void release_log_facility(char *name)
 	if (!glist_null(&facility->lf_active))
 		glist_del(&facility->lf_active);
 	glist_del(&facility->lf_list);
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 	if (facility->lf_func == log_to_file &&
 	    facility->lf_private != NULL)
 		gsh_free(facility->lf_private);
@@ -785,15 +785,15 @@ int enable_log_facility(char *name)
 
 	if (name == NULL || *name == '\0')
 		return -EINVAL;
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 	facility = find_log_facility(name);
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogInfo(COMPONENT_LOG, "Facility %s does not exist", name);
 		return -EEXIST;
 	}
 	if (!glist_null(&facility->lf_active)) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "Log facility (%s) is already enabled",
 			 name);
@@ -802,7 +802,7 @@ int enable_log_facility(char *name)
 	glist_add_tail(&active_facility_list, &facility->lf_active);
 	if (facility->lf_headers > max_headers)
 		max_headers = facility->lf_headers;
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 	return 0;
 }
 
@@ -824,22 +824,22 @@ int disable_log_facility(char *name)
 
 	if (name == NULL || *name == '\0')
 		return -EINVAL;
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 	facility = find_log_facility(name);
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogInfo(COMPONENT_LOG, "Facility %s already exists", name);
 		return -EEXIST;
 	}
 	if (glist_null(&facility->lf_active)) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "Log facility (%s) is already disabled",
 			 name);
 		return -EINVAL;
 	}
 	if (facility == default_facility) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "Cannot disable the default logger (%s)",
 			 default_facility->lf_name);
@@ -858,7 +858,7 @@ int disable_log_facility(char *name)
 				max_headers = found->lf_headers;
 		}
 	}
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 	return 0;
 }
 
@@ -880,10 +880,10 @@ static int set_default_log_facility(const char *name)
 	if (name == NULL || *name == '\0')
 		return -EINVAL;
 
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 	facility = find_log_facility(name);
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG, "Facility %s does not exist", name);
 		return -EEXIST;
 	}
@@ -911,7 +911,7 @@ static int set_default_log_facility(const char *name)
 		max_headers = facility->lf_headers;
 	default_facility = facility;
 out:
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 	return 0;
 }
 
@@ -940,10 +940,10 @@ int set_log_destination(char *name, char *dest)
 			 "New log file path empty or too long");
 		return -EINVAL;
 	}
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 	facility = find_log_facility(name);
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "No such log facility (%s)",
 			 name);
@@ -957,7 +957,7 @@ int set_log_destination(char *name, char *dest)
 		dir = dirname(dir);
 		rc = access(dir, W_OK);
 		if (rc != 0) {
-			pthread_rwlock_unlock(&log_rwlock);
+			PTHREAD_RWLOCK_unlock(&log_rwlock);
 			LogCrit(COMPONENT_LOG,
 				"Cannot create new log file (%s), because: %s",
 				dest, strerror(errno));
@@ -965,7 +965,7 @@ int set_log_destination(char *name, char *dest)
 		}
 		logfile = gsh_strdup(dest);
 		if (logfile == NULL) {
-			pthread_rwlock_unlock(&log_rwlock);
+			PTHREAD_RWLOCK_unlock(&log_rwlock);
 			LogCrit(COMPONENT_LOG,
 				"No memory for log file name (%s) for %s",
 				dest, facility->lf_name);
@@ -982,7 +982,7 @@ int set_log_destination(char *name, char *dest)
 		} else if (strcasecmp(dest, "stderr") == 0) {
 			out = stderr;
 		} else {
-			pthread_rwlock_unlock(&log_rwlock);
+			PTHREAD_RWLOCK_unlock(&log_rwlock);
 			LogCrit(COMPONENT_LOG,
 				"Expected STDERR or STDOUT, not (%s)",
 				dest);
@@ -990,13 +990,13 @@ int set_log_destination(char *name, char *dest)
 		}
 		facility->lf_private = out;
 	} else {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "Log facility %s destination is not changable",
 			facility->lf_name);
 		return -EINVAL;
 	}
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 	return 0;
 }
 
@@ -1018,17 +1018,17 @@ int set_log_level(char *name, log_levels_t max_level)
 		return -EINVAL;
 	if (max_level < NIV_NULL || max_level >= NB_LOG_LEVEL)
 		return -EINVAL;
-	pthread_rwlock_wrlock(&log_rwlock);
+	PTHREAD_RWLOCK_wrlock(&log_rwlock);
 	facility = find_log_facility(name);
 	if (facility == NULL) {
-		pthread_rwlock_unlock(&log_rwlock);
+		PTHREAD_RWLOCK_unlock(&log_rwlock);
 		LogCrit(COMPONENT_LOG,
 			 "No such log facility (%s)",
 			 name);
 		return -ENOENT;
 	}
 	facility->lf_max_level = max_level;
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 	return 0;
 }
 
@@ -1397,7 +1397,7 @@ void display_log_component_level(log_components_t component, char *file,
 	if (b_left > 0)
 		b_left = display_vprintf(&dsp_log, format, arguments);
 
-	pthread_rwlock_rdlock(&log_rwlock);
+	PTHREAD_RWLOCK_rdlock(&log_rwlock);
 
 	glist_for_each(glist, &active_facility_list) {
 		facility = glist_entry(glist, struct log_facility, lf_active);
@@ -1410,7 +1410,7 @@ void display_log_component_level(log_components_t component, char *file,
 					  compstr, message);
 	}
 
-	pthread_rwlock_unlock(&log_rwlock);
+	PTHREAD_RWLOCK_unlock(&log_rwlock);
 
 	if (level == NIV_FATAL)
 		Fatal();

src/idmapper/idmapper.c
@@ -111,7 +111,7 @@ static bool xdr_encode_nfs4_princ(XDR *xdrs, uint32_t id, bool group)
 	uint32_t not_a_size_t;
 	bool success = false;
 
-	pthread_rwlock_rdlock(group ? &idmapper_group_lock :
+	PTHREAD_RWLOCK_rdlock(group ? &idmapper_group_lock :
 			      &idmapper_user_lock);
 	if (group)
 		success = idmapper_lookup_by_gid(id, &found);
@@ -126,11 +126,11 @@ static bool xdr_encode_nfs4_princ(XDR *xdrs, uint32_t id, bool group)
 		success =
 		    inline_xdr_bytes(xdrs, (char **)&found->addr, &not_a_size_t,
 				     UINT32_MAX);
-		pthread_rwlock_unlock(group ? &idmapper_group_lock :
+		PTHREAD_RWLOCK_unlock(group ? &idmapper_group_lock :
 				      &idmapper_user_lock);
 		return success;
 	} else {
-		pthread_rwlock_unlock(group ? &idmapper_group_lock :
+		PTHREAD_RWLOCK_unlock(group ? &idmapper_group_lock :
 				      &idmapper_user_lock);
 		int rc;
 		bool looked_up = false;
@@ -220,14 +220,14 @@ static bool xdr_encode_nfs4_princ(XDR *xdrs, uint32_t id, bool group)
 		}
 
 		/* Add to the cache and encode the result. */
-		pthread_rwlock_wrlock(group ? &idmapper_group_lock :
+		PTHREAD_RWLOCK_wrlock(group ? &idmapper_group_lock :
 				      &idmapper_user_lock);
 		if (group)
 			success = idmapper_add_group(&new_name, id);
 		else
 			success = idmapper_add_user(&new_name, id, NULL, false);
 
-		pthread_rwlock_unlock(group ? &idmapper_group_lock :
+		PTHREAD_RWLOCK_unlock(group ? &idmapper_group_lock :
 				      &idmapper_user_lock);
 		if (unlikely(!success)) {
 			LogMajor(COMPONENT_IDMAPPER, "%s failed.",
@@ -455,13 +455,13 @@ static bool name2id(const struct gsh_buffdesc *name, uint32_t *id, bool group,
 {
 	bool success;
 
-	pthread_rwlock_rdlock(group ? &idmapper_group_lock :
+	PTHREAD_RWLOCK_rdlock(group ? &idmapper_group_lock :
 			      &idmapper_user_lock);
 	if (group)
 		success = idmapper_lookup_by_gname(name, id);
 	else
 		success = idmapper_lookup_by_uname(name, id, NULL, false);
-	pthread_rwlock_unlock(group ? &idmapper_group_lock :
+	PTHREAD_RWLOCK_unlock(group ? &idmapper_group_lock :
 			      &idmapper_user_lock);
 
 	if (success)
@@ -504,7 +504,7 @@ static bool name2id(const struct gsh_buffdesc *name, uint32_t *id, bool group,
 			*id = anon;
 		}
 
-		pthread_rwlock_wrlock(group ? &idmapper_group_lock :
+		PTHREAD_RWLOCK_wrlock(group ? &idmapper_group_lock :
 				      &idmapper_user_lock);
 		if (group)
 			success = idmapper_add_group(name, *id);
@@ -513,7 +513,7 @@ static bool name2id(const struct gsh_buffdesc *name, uint32_t *id, bool group,
 			    idmapper_add_user(name, *id, got_gid ? &gid : NULL,
 					      false);
 
-		pthread_rwlock_unlock(group ? &idmapper_group_lock :
+		PTHREAD_RWLOCK_unlock(group ? &idmapper_group_lock :
 				      &idmapper_user_lock);
 
 		if (!success)
@@ -594,12 +594,12 @@ bool principal2uid(char *principal, uid_t *uid, gid_t *gid)
 		return false;
 
 #ifdef USE_NFSIDMAP
-	pthread_rwlock_rdlock(&idmapper_user_lock);
+	PTHREAD_RWLOCK_rdlock(&idmapper_user_lock);
 	success =
 	    idmapper_lookup_by_uname(&princbuff, &gss_uid, &gss_gidres, true);
 	if (success && gss_gidres)
 		gss_gid = *gss_gidres;
-	pthread_rwlock_unlock(&idmapper_user_lock);
+	PTHREAD_RWLOCK_unlock(&idmapper_user_lock);
 	if (unlikely(!success)) {
 		if ((princbuff.len >= 4)
 		    && (!memcmp(princbuff.addr, "nfs/", 4)
@@ -695,10 +695,10 @@ bool principal2uid(char *principal, uid_t *uid, gid_t *gid)
  principal_found:
 #endif
 
-		pthread_rwlock_wrlock(&idmapper_user_lock);
+		PTHREAD_RWLOCK_wrlock(&idmapper_user_lock);
 		success =
 		    idmapper_add_user(&princbuff, gss_uid, &gss_gid, true);
-		pthread_rwlock_unlock(&idmapper_user_lock);
+		PTHREAD_RWLOCK_unlock(&idmapper_user_lock);
 
 		if (!success) {
 			LogMajor(COMPONENT_IDMAPPER,

src/idmapper/idmapper_cache.c
@@ -676,8 +676,8 @@ void idmapper_clear_cache(void)
 {
 	struct avltree_node *node;
 
-	pthread_rwlock_wrlock(&idmapper_user_lock);
-	pthread_rwlock_wrlock(&idmapper_group_lock);
+	PTHREAD_RWLOCK_wrlock(&idmapper_user_lock);
+	PTHREAD_RWLOCK_wrlock(&idmapper_group_lock);
 
 	memset(uid_cache, 0, id_cache_size * sizeof(struct avltree_node *));
 	memset(gid_cache, 0, id_cache_size * sizeof(struct avltree_node *));
@@ -710,8 +710,8 @@ void idmapper_clear_cache(void)
 
 	assert(avltree_first(&gid_tree) == NULL);
 
-	pthread_rwlock_unlock(&idmapper_group_lock);
-	pthread_rwlock_unlock(&idmapper_user_lock);
+	PTHREAD_RWLOCK_unlock(&idmapper_group_lock);
+	PTHREAD_RWLOCK_unlock(&idmapper_user_lock);
 }
 
 /** @} */

src/support/uid2grp.c
@@ -230,32 +230,32 @@ bool name2grp(const struct gsh_buffdesc *name, struct group_data **gdata)
 	bool success = false;
 	uid_t uid = -1;
 
-	pthread_rwlock_rdlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_rdlock(&uid2grp_user_lock);
 	success = uid2grp_lookup_by_uname(name, &uid, gdata);
 
 	/* Handle common case first */
 	if (success && !uid2grp_expired(*gdata)) {
 		uid2grp_hold_group_data(*gdata);
-		pthread_rwlock_unlock(&uid2grp_user_lock);
+		PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 		return success;
 	}
-	pthread_rwlock_unlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 
 	if (success) {
 		/* Cache entry is expired */
-		pthread_rwlock_wrlock(&uid2grp_user_lock);
+		PTHREAD_RWLOCK_wrlock(&uid2grp_user_lock);
 		uid2grp_remove_by_uname(name);
-		pthread_rwlock_unlock(&uid2grp_user_lock);
+		PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 	}
 
 	*gdata = uid2grp_allocate_by_name(name);
-	pthread_rwlock_wrlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_wrlock(&uid2grp_user_lock);
 	if (*gdata)
 		uid2grp_add_user(*gdata);
 	success = uid2grp_lookup_by_uname(name, &uid, gdata);
 	if (success)
 		uid2grp_hold_group_data(*gdata);
-	pthread_rwlock_unlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 
 	return success;
 }
@@ -272,32 +272,32 @@ bool uid2grp(uid_t uid, struct group_data **gdata)
 {
 	bool success = false;
 
-	pthread_rwlock_rdlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_rdlock(&uid2grp_user_lock);
 	success = uid2grp_lookup_by_uid(uid, gdata);
 
 	/* Handle common case first */
 	if (success && !uid2grp_expired(*gdata)) {
 		uid2grp_hold_group_data(*gdata);
-		pthread_rwlock_unlock(&uid2grp_user_lock);
+		PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 		return success;
 	}
-	pthread_rwlock_unlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 
 	if (success) {
 		/* Cache entry is expired */
-		pthread_rwlock_wrlock(&uid2grp_user_lock);
+		PTHREAD_RWLOCK_wrlock(&uid2grp_user_lock);
 		uid2grp_remove_by_uid(uid);
-		pthread_rwlock_unlock(&uid2grp_user_lock);
+		PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 	}
 
 	*gdata = uid2grp_allocate_by_uid(uid);
-	pthread_rwlock_wrlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_wrlock(&uid2grp_user_lock);
 	if (*gdata)
 		uid2grp_add_user(*gdata);
 	success = uid2grp_lookup_by_uid(uid, gdata);
 	if (success)
 		uid2grp_hold_group_data(*gdata);
-	pthread_rwlock_unlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 
 	return success;
 }

src/support/uid2grp_cache.c
@@ -419,14 +419,14 @@ void uid2grp_remove_by_uname(const struct gsh_buffdesc *name)
 }
 
 /**
- * @brief Wipe out the idmapper cache
+ * @brief Wipe out the uid2grp cache
  */
 
 void uid2grp_clear_cache(void)
 {
 	struct avltree_node *node;
 
-	pthread_rwlock_wrlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_wrlock(&uid2grp_user_lock);
 
 	while ((node = avltree_first(&uname_tree))) {
 		struct cache_info *info = avltree_container_of(node,
@@ -438,7 +438,7 @@ void uid2grp_clear_cache(void)
 
 	assert(avltree_first(&uid_tree) == NULL);
 
-	pthread_rwlock_unlock(&uid2grp_user_lock);
+	PTHREAD_RWLOCK_unlock(&uid2grp_user_lock);
 }
 
 /** @} */

commit 67239e58b773b0dda4cf411eb59e8e7d647c96ac

Fix DisplayLogComponentLevel recursion with NIV_FULL_DEBUG
If ganesha is started with NIV_FULL_DEBUG, DisplayLogComponentLevel
calls PTHREAD_RWLOCK_xxx() functions which call DisplayLogComponentLevel
to log messages. This continues in a loop until segmentation fault
happens!  A regression introduced by commit
344de4ab632713145194e936a8fb3568b9538321

Redefine PTHREAD_RWLOCK_xxx macros in logging file to avoid logging
inside PTHREAD_RWLOCK_xxx macros.

Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
Reported-by: Soumya Koduri <skoduri@redhat.com>
Reviewed-by: Frank S. Filz <ffilzlnx@mindspring.com>

src/log/log_functions.c
@@ -57,6 +57,40 @@
 #include "nfs_core.h"
 #include "config_parsing.h"
 
+/*
+ * The usual PTHREAD_RWLOCK_xxx macros log messages for tracing if FULL
+ * DEBUG is enabled. If such a macro is called from this logging file as
+ * part of logging a message, it generates endless loop of lock tracing
+ * messages. The following code redefines these lock macros to avoid the
+ * loop.
+ */
+#ifdef PTHREAD_RWLOCK_wrlock
+#undef PTHREAD_RWLOCK_wrlock
+#endif
+#define PTHREAD_RWLOCK_wrlock(_lock)					\
+	do {								\
+		if (pthread_rwlock_wrlock(_lock) != 0)			\
+			assert(0);					\
+	} while (0)
+
+#ifdef PTHREAD_RWLOCK_rdlock
+#undef PTHREAD_RWLOCK_rdlock
+#endif
+#define PTHREAD_RWLOCK_rdlock(_lock)					\
+	do {								\
+		if (pthread_rwlock_rdlock(_lock) != 0)			\
+			assert(0);					\
+	} while (0)
+
+#ifdef PTHREAD_RWLOCK_unlock
+#undef PTHREAD_RWLOCK_unlock
+#endif
+#define PTHREAD_RWLOCK_unlock(_lock)					\
+	do {								\
+		if (pthread_rwlock_unlock(_lock) != 0)			\
+			assert(0);					\
+	} while (0)
+
 pthread_rwlock_t log_rwlock = PTHREAD_RWLOCK_INITIALIZER;
 
 /* Variables to control log fields */