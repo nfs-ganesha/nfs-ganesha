84c740b5da08e7ee3554d17d5bc9978df0285807
Philippe DENIEL <philippe.deniel@cea.fr>
2014-08-05 14:06:27 +0200

    Port to Lustre 2.6, is compatible with Lustre 2.1, 2.4 and 2.5
    FSAL_LUSTRE is accessing LUSTRE via the "fid path" way. Every accessed object is associated with a fid (similar to a NFS FH in some ways).
    For every object, a special and immutable path can be build to access the object. This path is "<mount point>/.lustre/fids/<object's fid>".
    When trying to lookup parent from <mntpoint>/.lustre/fid the standard POSIX API points to <mntpoint>/.lustre/fid, making this special
    directory the parent of every object.
    To deal with that, FSAL_LUSTRE embeds a very small piece of Lustre kernel code to compute the paren correctly with proper consideration
    of the filestsyem trees. The related code is located in src/FSAL/FSAL_LUSTRE/lustre_extended_types.h .
    From version 2.4 to 2.6, the required code to embed has change, but with backward compatibilty.
    This commits updates the embedded piece of code and makes it compatible with very version of Lustre 2.x from 2.1 to 2.6 .
    
    Change-Id: I8680d27cbf155f6e0c5b3115fb1b276742fb4aed
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- src/FSAL/FSAL_LUSTRE/lustre_extended_types.h
+++ src/FSAL/FSAL_LUSTRE/lustre_extended_types.h
@@ -92,13 +92,95 @@ extern int llapi_get_poolmembers(const char *poolname,
 typedef void *lnet_nid_t;
 typedef time_t cfs_time_t;
 
-/* lustre_idl.h references many undefined symbols
- * in functions or structures we don't need.
- * So ignore the warnings. */
-#pragma GCC push_options
-#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
-#include <lustre/lustre_idl.h>
-#pragma GCC pop_options
+/* Workaround for lustre 2.6.0 (waiting for LU-3613 to land):
+ *  * definitions and structures to handle link EA.
+ *   */
+#ifndef XATTR_NAME_LINK
+#define XATTR_NAME_LINK "trusted.link"
+#endif
+
+struct link_ea_header {
+	__u32 leh_magic;
+	__u32 leh_reccount;
+	__u64 leh_len;      /* total size */
+	/* future use */
+	__u32 padding1;
+	__u32 padding2;
+};
+
+/** Hardlink data is name and parent fid.
+ *  * Stored in this crazy struct for maximum packing and endian-neutrality
+ *   */
+struct link_ea_entry {
+	/** __u16 stored big-endian, unaligned */
+	unsigned char      lee_reclen[2];
+	unsigned char      lee_parent_fid[sizeof(struct lu_fid)];
+	char               lee_name[0];
+} __attribute__((packed));
+
+static inline void fid_be_to_cpu(struct lu_fid *dst, const struct lu_fid *src)
+{
+	dst->f_seq = be64_to_cpu(src->f_seq);
+	dst->f_oid = be32_to_cpu(src->f_oid);
+	dst->f_ver = be32_to_cpu(src->f_ver);
+}
+
+enum fid_seq {
+	FID_SEQ_OST_MDT0    = 0,
+	FID_SEQ_LLOG        = 1, /* unnamed llogs */
+	FID_SEQ_ECHO        = 2,
+	FID_SEQ_OST_MDT1    = 3,
+	FID_SEQ_OST_MAX     = 9, /* Max MDT count before OST_on_FID */
+	FID_SEQ_LLOG_NAME   = 10, /* named llogs */
+	FID_SEQ_RSVD        = 11,
+	FID_SEQ_IGIF        = 12,
+	FID_SEQ_IGIF_MAX    = 0x0ffffffffULL,
+	FID_SEQ_IDIF        = 0x100000000ULL,
+	FID_SEQ_IDIF_MAX    = 0x1ffffffffULL,
+	FID_SEQ_START       = 0x200000000ULL,
+	FID_SEQ_LOCAL_FILE  = 0x200000001ULL,
+	FID_SEQ_DOT_LUSTRE  = 0x200000002ULL,
+	FID_SEQ_LOCAL_NAME  = 0x200000003ULL,
+	FID_SEQ_SPECIAL     = 0x200000004ULL,
+	FID_SEQ_QUOTA       = 0x200000005ULL,
+	FID_SEQ_QUOTA_GLB   = 0x200000006ULL,
+	FID_SEQ_ROOT        = 0x200000007ULL,  /* Located on MDT0 */
+	FID_SEQ_NORMAL      = 0x200000400ULL,
+	FID_SEQ_LOV_DEFAULT = 0xffffffffffffffffULL
+};
+
+static inline int fid_seq_is_rsvd(const __u64 seq)
+{
+	return (seq > FID_SEQ_OST_MDT0 && seq <= FID_SEQ_RSVD);
+};
+
+static inline int fid_seq_is_idif(const __u64 seq)
+{
+	return seq >= FID_SEQ_IDIF && seq <= FID_SEQ_IDIF_MAX;
+}
+
+static inline int fid_is_idif(const struct lu_fid *fid)
+{
+	return fid_seq_is_idif(fid->f_seq);
+}
+
+static inline int fid_seq_is_igif(const __u64 seq)
+{
+	return seq >= FID_SEQ_IGIF && seq <= FID_SEQ_IGIF_MAX;
+}
+
+static inline int fid_is_igif(const struct lu_fid *fid)
+{
+	return fid_seq_is_igif(fid->f_seq);
+}
+
+static inline int fid_is_sane(const struct lu_fid *fid)
+{
+	return fid != NULL &&
+		((fid->f_seq >= FID_SEQ_START && fid->f_ver == 0) ||
+		fid_is_igif(fid) || fid_is_idif(fid) ||
+		fid_seq_is_rsvd(fid->f_seq));
+}
 
 struct lu_buf {
 	void   *lb_buf;
