e65376771aa7f263f6947f1630168d7ddcb4b4e1
Jim Lieb <lieb@sea-troll.net>
2015-02-05 11:05:07 -0800

    Exit server on startup if there are fatal errors in an export config.
    Fatal errors in an export leaves the data structures seriously messed
    up.  Don't even try...
    
    Signed-off-by: Jim Lieb <lieb@sea-troll.net>
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/MainNFSD/nfs_main.c
+++ src/MainNFSD/nfs_main.c
@@ -468,6 +468,8 @@ int main(int argc, char *argv[])
 	if (rc < 0) {
 		LogCrit(COMPONENT_INIT,
 			  "Error while parsing export entries");
+		if (config_error_is_fatal(&err_type))
+			goto fatal_die;
 	}
 	if (rc == 0 && dsc == 0)
 		LogWarn(COMPONENT_INIT,
