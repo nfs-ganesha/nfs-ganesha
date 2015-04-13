674f265cfe92ce58468bbd277227a7d93823c357
Orit Wasserman <owasserm@redhat.com>
2015-04-13 11:33:06 +0200

    introduce FSAL_RGW
    This change provides a FSAL consuming a new file-based
    interface to Ceph's RADOS Gateway service, consumed as a
    library.
    
    The FSAL is in a preliminary state, a variety of interfaces are
    not implemented (e.g., upcalls).
    
    The change is a concatenation of the following commits:
        * Basic sekleton for RGW FSAL
        * build FSAL_RGW
        * Add rgw file functions
        * Add nfs_handle
        * remove fsal2ceph error function and inode
        * mount command wip
    
    Change-Id: I239bfd051f049feb77c8deff440f7be64513ddba
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ src/FSAL/FSAL_RGW/export.c
@@ -0,0 +1,500 @@
+/*
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright (C) Red Hat, 2015
+ * Author: Orit Wasserman <owasserm@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/* export.c
+ * RGW FSAL export object
+ */
+
+#include <limits.h>
+#include <stdint.h>
+#include <sys/statvfs.h>
+#include <rados/rgw_file.h>
+#include "abstract_mem.h"
+#include "fsal.h"
+#include "fsal_types.h"
+#include "fsal_api.h"
+#include "FSAL/fsal_commonlib.h"
+#include "FSAL/fsal_config.h"
+#include "internal.h"
+
+/**
+ * @brief Clean up an export
+ *
+ * This function cleans up an export after the last reference is
+ * released.
+ *
+ * @param[in,out] export The export to be released
+ *
+ * @retval ERR_FSAL_NO_ERROR on success.
+ * @retval ERR_FSAL_BUSY if the export is in use.
+ */
+
+static void release(struct fsal_export *export_pub)
+{
+	/* The private, expanded export */
+	struct rgw_export *export =
+	    container_of(export_pub, struct rgw_export, export);
+
+	deconstruct_handle(export->root);
+	export->root = 0;
+
+	fsal_detach_export(export->export.fsal, &export->export.exports);
+	free_export_ops(&export->export);
+
+	rgw_shutdown(export->cmount);
+	export->cmount = NULL;
+	gsh_free(export);
+	export = NULL;
+}
+
+/**
+ * @brief Return a handle corresponding to a path
+ *
+ * This function looks up the given path and supplies an FSAL object
+ * handle.  Because the root path specified for the export is a Ceph
+ * style root as supplied to mount -t ceph of ceph-fuse (of the form
+ * host:/path), we check to see if the path begins with / and, if not,
+ * skip until we find one.
+ *
+ * @param[in]  export_pub The export in which to look up the file
+ * @param[in]  path       The path to look up
+ * @param[out] pub_handle The created public FSAL handle
+ *
+ * @return FSAL status.
+ */
+
+static fsal_status_t lookup_path(struct fsal_export *export_pub,
+				 const char *path,
+				 struct fsal_obj_handle **pub_handle)
+{
+	/* The 'private' full export handle */
+	struct rgw_export *export =
+	    container_of(export_pub, struct rgw_export, export);
+	/* The 'private' full object handle */
+	struct rgw_handle *handle = NULL;
+	/* FSAL status structure */
+	fsal_status_t status = { ERR_FSAL_NO_ERROR, 0 };
+	/* The buffer in which to store stat info */
+	struct stat st;
+	/* Return code from Ceph */
+	int rc;
+	/* Find the actual path in the supplied path */
+	const char *realpath;
+	uint64_t i;
+
+	if (*path != '/') {
+		realpath = strchr(path, ':');
+		if (realpath == NULL) {
+			status.major = ERR_FSAL_INVAL;
+			return status;
+		}
+		if (*(++realpath) != '/') {
+			status.major = ERR_FSAL_INVAL;
+			return status;
+		}
+	} else {
+		realpath = path;
+	}
+
+	*pub_handle = NULL;
+
+	if (strcmp(realpath, "/") == 0) {
+		assert(export->root);
+		*pub_handle = &export->root->handle;
+		return status;
+	}
+
+	rc = rgw_lookup(export->cmount, realpath, &i, &st);
+	if (rc < 0)
+		return rgw2fsal_error(rc);
+
+	rc = construct_handle(&st, i, export, &handle);
+	if (rc < 0) {
+		return rgw2fsal_error(rc);
+	}
+
+	*pub_handle = &handle->handle;
+	return status;
+}
+
+/**
+ * @brief Decode a digested handle
+ *
+ * This function decodes a previously digested handle.
+ *
+ * @param[in]  exp_handle  Handle of the relevant fs export
+ * @param[in]  in_type  The type of digest being decoded
+ * @param[out] fh_desc  Address and length of key
+ */
+static fsal_status_t extract_handle(struct fsal_export *exp_hdl,
+				    fsal_digesttype_t in_type,
+				    struct gsh_buffdesc *fh_desc,
+				    int flags)
+{
+	switch (in_type) {
+		/* Digested Handles */
+	case FSAL_DIGEST_NFSV3:
+	case FSAL_DIGEST_NFSV4:
+		/* wire handles */
+		fh_desc->len = sizeof(vinodeno_t);
+		break;
+	default:
+		return fsalstat(ERR_FSAL_SERVERFAULT, 0);
+	}
+
+	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+}
+
+/**
+ * @brief Create a handle object from a wire handle
+ *
+ * The wire handle is given in a buffer outlined by desc, which it
+ * looks like we shouldn't modify.
+ *
+ * @param[in]  export_pub Public export
+ * @param[in]  desc       Handle buffer descriptor
+ * @param[out] pub_handle The created handle
+ *
+ * @return FSAL status.
+ */
+static fsal_status_t create_handle(struct fsal_export *export_pub,
+				   struct gsh_buffdesc *desc,
+				   struct fsal_obj_handle **pub_handle)
+{
+	/* Full 'private' export structure */
+	struct rgw_export *export =
+	    container_of(export_pub, struct rgw_export, export);
+	/* FSAL status to return */
+	fsal_status_t status = { ERR_FSAL_NO_ERROR, 0 };
+	/* The FSAL specific portion of the handle received by the
+	   client */
+	int rc = 0;
+	/* Stat buffer */
+	struct stat st;
+	/* Handle to be created */
+	struct rgw_handle *handle = NULL;
+	uint64_t nfs_handle = *(uint64_t *)(desc->addr);
+
+
+	*pub_handle = NULL;
+
+	if (desc->len != sizeof(vinodeno_t)) {
+		status.major = ERR_FSAL_INVAL;
+		return status;
+	}
+
+	rc = rgw_check_handle(export, desc, &nfs_handle);
+	if (!rc) {
+		return rgw2fsal_error(-ESTALE);
+	}
+
+	rc = rgw_getattr(export, nfs_handle, &st);
+	if (rc < 0)
+		return rgw2fsal_error(rc);
+
+	rc = construct_handle(&st, nfs_handle, export, &handle);
+	if (rc < 0) {
+		return rgw2fsal_error(rc);
+	}
+
+	*pub_handle = &handle->handle;
+	return status;
+}
+
+/**
+ * @brief Get dynamic filesystem info
+ *
+ * This function returns dynamic filesystem information for the given
+ * export.
+ *
+ * @param[in]  export_pub The public export handle
+ * @param[out] info       The dynamic FS information
+ *
+ * @return FSAL status.
+ */
+
+static fsal_status_t get_fs_dynamic_info(struct fsal_export *export_pub,
+					 struct fsal_obj_handle *obj_hdl,
+					 fsal_dynamicfsinfo_t *info)
+{
+	/* Full 'private' export */
+	struct rgw_export *export =
+	    container_of(export_pub, struct rgw_export, export);
+	/* Return value from Ceph calls */
+	int rc = 0;
+	/* Filesystem stat */
+	struct statvfs vfs_st;
+
+	rc = rgw_statfs(export->cmount, export->root->i, &vfs_st);
+	if (rc < 0)
+		return rgw2fsal_error(rc);
+
+	memset(info, 0, sizeof(fsal_dynamicfsinfo_t));
+	info->total_bytes = vfs_st.f_frsize * vfs_st.f_blocks;
+	info->free_bytes = vfs_st.f_frsize * vfs_st.f_bfree;
+	info->avail_bytes = vfs_st.f_frsize * vfs_st.f_bavail;
+	info->total_files = vfs_st.f_files;
+	info->free_files = vfs_st.f_ffree;
+	info->avail_files = vfs_st.f_favail;
+	info->time_delta.tv_sec = 1;
+	info->time_delta.tv_nsec = 0;
+
+	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+}
+
+/**
+ * @brief Query the FSAL's capabilities
+ *
+ * This function queries the capabilities of an FSAL export.
+ *
+ * @param[in] export_pub The public export handle
+ * @param[in] option     The option to check
+ *
+ * @retval true if the option is supported.
+ * @retval false if the option is unsupported (or unknown).
+ */
+
+static bool fs_supports(struct fsal_export *export_pub,
+			fsal_fsinfo_options_t option)
+{
+	struct fsal_staticfsinfo_t *info = rgw_staticinfo(export_pub->fsal);
+	return fsal_supports(info, option);
+}
+
+/**
+ * @brief Return the longest file supported
+ *
+ * This function returns the length of the longest file supported.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return UINT64_MAX.
+ */
+
+static uint64_t fs_maxfilesize(struct fsal_export *export_pub)
+{
+	return UINT64_MAX;
+}
+
+/**
+ * @brief Return the longest read supported
+ *
+ * This function returns the length of the longest read supported.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return 4 mebibytes.
+ */
+
+static uint32_t fs_maxread(struct fsal_export *export_pub)
+{
+	return 0x400000;
+}
+
+/**
+ * @brief Return the longest write supported
+ *
+ * This function returns the length of the longest write supported.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return 4 mebibytes.
+ */
+
+static uint32_t fs_maxwrite(struct fsal_export *export_pub)
+{
+	return 0x400000;
+}
+
+/**
+ * @brief Return the maximum number of hard links to a file
+ *
+ * This function returns the maximum number of hard links supported to
+ * any file.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return 1024.
+ */
+
+static uint32_t fs_maxlink(struct fsal_export *export_pub)
+{
+	/* Ceph does not like hard links.  See the anchor table
+	   design.  We should fix this, but have to do it in the Ceph
+	   core. */
+	return 1024;
+}
+
+/**
+ * @brief Return the maximum size of a Ceph filename
+ *
+ * This function returns the maximum filename length.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return UINT32_MAX.
+ */
+
+static uint32_t fs_maxnamelen(struct fsal_export *export_pub)
+{
+	/* Ceph actually supports filenames of unlimited length, at
+	   least according to the protocol docs.  We may wish to
+	   constrain this later. */
+	return UINT32_MAX;
+}
+
+/**
+ * @brief Return the maximum length of a Ceph path
+ *
+ * This function returns the maximum path length.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return UINT32_MAX.
+ */
+
+static uint32_t fs_maxpathlen(struct fsal_export *export_pub)
+{
+	/* Similarly unlimited int he protocol */
+	return UINT32_MAX;
+}
+
+/**
+ * @brief Return the lease time
+ *
+ * This function returns the lease time.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return five minutes.
+ */
+
+static struct timespec fs_lease_time(struct fsal_export *export_pub)
+{
+	struct timespec lease = { 300, 0 };
+
+	return lease;
+}
+
+/**
+ * @brief Return ACL support
+ *
+ * This function returns the export's ACL support.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return FSAL_ACLSUPPORT_DENY.
+ */
+
+static fsal_aclsupp_t fs_acl_support(struct fsal_export *export_pub)
+{
+	return FSAL_ACLSUPPORT_DENY;
+}
+
+/**
+ * @brief Return the attributes supported by this FSAL
+ *
+ * This function returns the mask of attributes this FSAL can support.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return supported_attributes as defined in internal.c.
+ */
+
+static attrmask_t fs_supported_attrs(struct fsal_export *export_pub)
+{
+	return rgw_supported_attributes;
+}
+
+/**
+ * @brief Return the mode under which the FSAL will create files
+ *
+ * This function modifies the default mode on any new file created.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return 0 (usually).  Bits set here turn off bits in created files.
+ */
+
+static uint32_t fs_umask(struct fsal_export *export_pub)
+{
+	return fsal_umask(rgw_staticinfo(export_pub->fsal));
+}
+
+/**
+ * @brief Return the mode for extended attributes
+ *
+ * This function returns the access mode applied to extended
+ * attributes.  Dubious.
+ *
+ * @param[in] export_pub The public export
+ *
+ * @return 0644.
+ */
+
+static uint32_t fs_xattr_access_rights(struct fsal_export *export_pub)
+{
+	return fsal_xattr_access_rights(rgw_staticinfo(export_pub->fsal));
+}
+
+/**
+ * @brief Set operations for exports
+ *
+ * This function overrides operations that we've implemented, leaving
+ * the rest for the default.
+ *
+ * @param[in,out] ops Operations vector
+ */
+
+void export_ops_init(struct export_ops *ops)
+{
+	ops->release = release;
+	ops->lookup_path = lookup_path;
+	ops->extract_handle = extract_handle;
+	ops->create_handle = create_handle;
+	ops->get_fs_dynamic_info = get_fs_dynamic_info;
+	ops->fs_supports = fs_supports;
+	ops->fs_maxfilesize = fs_maxfilesize;
+	ops->fs_maxread = fs_maxread;
+	ops->fs_maxwrite = fs_maxwrite;
+	ops->fs_maxlink = fs_maxlink;
+	ops->fs_maxnamelen = fs_maxnamelen;
+	ops->fs_maxpathlen = fs_maxpathlen;
+	ops->fs_lease_time = fs_lease_time;
+	ops->fs_acl_support = fs_acl_support;
+	ops->fs_supported_attrs = fs_supported_attrs;
+	ops->fs_umask = fs_umask;
+	ops->fs_xattr_access_rights = fs_xattr_access_rights;
+}
+
+static struct config_item export_params[] = {
+	CONF_ITEM_NOOP("name"),
+	CONF_ITEM_STRING("rgw_user_id", 0, MAXUIDLEN, NULL,
+		       rgw_fsal_export, rgw_export_uid),
+	CONF_ITEM_STRING("rgw_access_key_id", 0, MAXKEYLEN, NULL,
+		       rgw_fsal_export, rgw_access_key_id),
+	CONF_ITEM_STRING("rgw_secret_access_key", 0, MAXSECRETLEN, NULL,
+		       rgw_fsal_export, rgw_secret_access_key),
+	CONFIG_EOL
+};
---
+++ src/FSAL/FSAL_RGW/internal.c
@@ -0,0 +1,301 @@
+/*
+ * Copyright © Red Hat, 2015
+ * Author: ORit Wasserman <owasserm@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file   internal.c
+ * @brief Internal definitions for the RGW FSAL
+ *
+ * This file includes internal function definitions, constants, and
+ * variable declarations used to impelment the RGW FSAL, but not
+ * exposed as part of the API.
+ */
+
+#include <sys/stat.h>
+#include "fsal_types.h"
+#include "fsal.h"
+#include "fsal_convert.h"
+#include "FSAL/fsal_commonlib.h"
+
+#define RGW_INTERNAL_C
+#include "internal.h"
+
+/**
+ * The attributes tis FSAL can interpret or supply.
+ */
+
+const attrmask_t supported_attributes = (
+	ATTR_TYPE      | ATTR_SIZE     | ATTR_FSID  | ATTR_FILEID |
+	ATTR_MODE      | ATTR_NUMLINKS | ATTR_OWNER | ATTR_GROUP  |
+	ATTR_ATIME     | ATTR_RAWDEV   | ATTR_CTIME | ATTR_MTIME  |
+	ATTR_SPACEUSED | ATTR_CHGTIME);
+
+/**
+ * The attributes this FSAL can set.
+ */
+
+const attrmask_t settable_attributes = (
+	ATTR_MODE  | ATTR_OWNER | ATTR_GROUP | ATTR_ATIME	 |
+	ATTR_CTIME | ATTR_MTIME | ATTR_SIZE  | ATTR_MTIME_SERVER |
+	ATTR_ATIME_SERVER);
+
+/**
+ * @brief FSAL status from RGW error
+ *
+ * This function returns a fsal_status_t with the FSAL error as the
+ * major, and the posix error as minor.	 (RGW's error codes are just
+ * negative signed versions of POSIX error codes.)
+ *
+ * @param[in] rgw_errorcode RGW error (negative Posix)
+ *
+ * @return FSAL status.
+ */
+
+fsal_status_t rgw2fsal_error(const int rgw_errorcode)
+{
+	fsal_status_t status;
+	status.minor = -rgw_errorcode;
+
+	switch (-rgw_errorcode) {
+
+	case 0:
+		status.major = ERR_FSAL_NO_ERROR;
+		break;
+
+	case EPERM:
+		status.major = ERR_FSAL_PERM;
+		break;
+
+	case ENOENT:
+		status.major = ERR_FSAL_NOENT;
+		break;
+
+	case ECONNREFUSED:
+	case ECONNABORTED:
+	case ECONNRESET:
+	case EIO:
+	case ENFILE:
+	case EMFILE:
+	case EPIPE:
+		status.major = ERR_FSAL_IO;
+		break;
+
+	case ENODEV:
+	case ENXIO:
+		status.major = ERR_FSAL_NXIO;
+		break;
+
+	case EBADF:
+		/**
+		 * @todo: The EBADF error also happens when file is
+		 *	  opened for reading, and we try writting in
+		 *	  it.  In this case, we return
+		 *	  ERR_FSAL_NOT_OPENED, but it doesn't seems to
+		 *	  be a correct error translation.
+		 */
+		status.major = ERR_FSAL_NOT_OPENED;
+		break;
+
+	case ENOMEM:
+		status.major = ERR_FSAL_NOMEM;
+		break;
+
+	case EACCES:
+		status.major = ERR_FSAL_ACCESS;
+		break;
+
+	case EFAULT:
+		status.major = ERR_FSAL_FAULT;
+		break;
+
+	case EEXIST:
+		status.major = ERR_FSAL_EXIST;
+		break;
+
+	case EXDEV:
+		status.major = ERR_FSAL_XDEV;
+		break;
+
+	case ENOTDIR:
+		status.major = ERR_FSAL_NOTDIR;
+		break;
+
+	case EISDIR:
+		status.major = ERR_FSAL_ISDIR;
+		break;
+
+	case EINVAL:
+		status.major = ERR_FSAL_INVAL;
+		break;
+
+	case EFBIG:
+		status.major = ERR_FSAL_FBIG;
+		break;
+
+	case ENOSPC:
+		status.major = ERR_FSAL_NOSPC;
+		break;
+
+	case EMLINK:
+		status.major = ERR_FSAL_MLINK;
+		break;
+
+	case EDQUOT:
+		status.major = ERR_FSAL_DQUOT;
+		break;
+
+	case ENAMETOOLONG:
+		status.major = ERR_FSAL_NAMETOOLONG;
+		break;
+
+	case ENOTEMPTY:
+		status.major = ERR_FSAL_NOTEMPTY;
+		break;
+
+	case ESTALE:
+		status.major = ERR_FSAL_STALE;
+		break;
+
+	case EAGAIN:
+	case EBUSY:
+		status.major = ERR_FSAL_DELAY;
+		break;
+
+	default:
+		status.major = ERR_FSAL_SERVERFAULT;
+		break;
+	}
+
+	return status;
+}
+
+/**
+ * @brief Convert a struct stat from RGW to a struct attrlist
+ *
+ * This function writes the content of the supplied struct stat to the
+ * struct fsalsattr.
+ *
+ * @param[in]  buffstat Stat structure
+ * @param[out] fsalattr FSAL attributes
+ */
+
+void rgw2fsal_attributes(const struct stat *buffstat,
+			  struct attrlist *fsalattr)
+{
+	FSAL_CLEAR_MASK(fsalattr->mask);
+
+	/* Fills the output struct */
+	fsalattr->type = posix2fsal_type(buffstat->st_mode);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_TYPE);
+
+	fsalattr->filesize = buffstat->st_size;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_SIZE);
+
+	fsalattr->fsid = posix2fsal_fsid(buffstat->st_dev);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_FSID);
+
+	fsalattr->fileid = buffstat->st_ino;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_FILEID);
+
+	fsalattr->mode = unix2fsal_mode(buffstat->st_mode);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_MODE);
+
+	fsalattr->numlinks = buffstat->st_nlink;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_NUMLINKS);
+
+	fsalattr->owner = buffstat->st_uid;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_OWNER);
+
+	fsalattr->group = buffstat->st_gid;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_GROUP);
+
+	fsalattr->atime = posix2fsal_time(buffstat->st_atime, 0);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_ATIME);
+
+	fsalattr->ctime = posix2fsal_time(buffstat->st_ctime, 0);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_CTIME);
+
+	fsalattr->mtime = posix2fsal_time(buffstat->st_mtime, 0);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_MTIME);
+
+	fsalattr->chgtime =
+	    posix2fsal_time(MAX(buffstat->st_mtime, buffstat->st_ctime), 0);
+	fsalattr->change = fsalattr->chgtime.tv_sec;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_CHGTIME);
+
+	fsalattr->spaceused = buffstat->st_blocks * S_BLKSIZE;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_SPACEUSED);
+
+	fsalattr->rawdev = posix2fsal_devt(buffstat->st_rdev);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_RAWDEV);
+}
+
+/**
+ * @brief Construct a new filehandle
+ *
+ * This function constructs a new RGW FSAL object handle and attaches
+ * it to the export.  After this call the attributes have been filled
+ * in and the handdle is up-to-date and usable.
+ *
+ * @param[in]  st     Stat data for the file
+ * @param[in]  export Export on which the object lives
+ * @param[out] obj    Object created
+ *
+ * @return 0 on success, negative error codes on failure.
+ */
+
+int construct_handle(const struct stat *st, uint64_t nfs_handle,
+		     struct rgw_export *export, struct rgw_handle **obj)
+{
+	/* Poitner to the handle under construction */
+	struct rgw_handle *constructing = NULL;
+	*obj = NULL;
+
+	constructing = gsh_calloc(1, sizeof(struct rgw_handle));
+	if (constructing == NULL)
+		return -ENOMEM;
+
+	constructing->nfs_handle = nfs_handle;
+	constructing->up_ops = export->export.up_ops;
+
+	rgw2fsal_attributes(st, &constructing->handle.attributes);
+
+	fsal_obj_handle_init(&constructing->handle, &export->export,
+			     constructing->handle.attributes.type);
+
+	constructing->export = export;
+
+	*obj = constructing;
+
+	return 0;
+}
+
+/**
+ * @brief Release all resrouces for a handle
+ *
+ * @param[in] obj Handle to release
+ */
+
+void deconstruct_handle(struct rgw_handle *obj)
+{
+	fsal_obj_handle_uninit(&obj->handle);
+	gsh_free(obj);
+}
