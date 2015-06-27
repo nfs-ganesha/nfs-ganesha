8b9c319204ed26a8a55d8698aa1e423c7a9a46ac
Aihua Zhang <zhangaihua1@huawei.com>
2015-06-27 23:14:51 +0800

    mnt_Mnt: can't export path "/" while the nfs version is 3
    if the export path is "/", it will be got rid of, will become "",
    so can't be exported.
    
    Change-Id: I72add5d92070071ce99e236f8fe98aa4aa2f6f89
    Signed-off-by: Aihua Zhang <zhangaihua1@huawei.com>
    (cherry picked from commit 413b6d292e5c1e64d7db6c84e95d117b8fa6ef64)

--- src/Protocols/NFS/mnt_Mnt.c
+++ src/Protocols/NFS/mnt_Mnt.c
@@ -88,7 +88,8 @@ int mnt_Mnt(nfs_arg_t *arg,
 
 	/* If the path ends with a '/', get rid of it */
 	/** @todo: should it be a while()?? */
-	if (arg->arg_mnt[strlen(arg->arg_mnt) - 1] == '/')
+	if ((strlen(arg->arg_mnt) > 1) &&
+	    (arg->arg_mnt[strlen(arg->arg_mnt) - 1] == '/'))
 		arg->arg_mnt[strlen(arg->arg_mnt) - 1] = '\0';
 
 	/*  Find the export for the dirname (using as well Path or Tag) */
