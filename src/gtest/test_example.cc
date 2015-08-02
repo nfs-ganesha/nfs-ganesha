430e92b5b2bb024274200dff2d207d072ad108f4
Matt Benjamin <mbenjamin@redhat.com>
2015-08-02 13:12:45 -0400

    testing: introduce gmock and gtest framework
    Google's implementations of these frameworks have specific
    features we require, in particular, thread safety.
    
    In addition, the ability to use c++ facilities (data structures,
    random distributions, etc) for prototyping test code looked
    likely to be a large time saver.
    
    Change-Id: Ibcfb956f1074428ef57cda0a1c82c2819f95d4c4
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ src/gtest/test_example.cc
@@ -0,0 +1,49 @@
+// -*- mode:C; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Copyright (C) 2015 Red Hat, Inc.
+ * Contributor : Matt Benjamin <mbenjamin@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+#include <sys/types.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <random>
+#include "gtest/gtest.h"
+
+extern "C" {
+  /* Ganesha headers */
+}
+
+namespace {
+  bool global_decls = false;
+} /* namespace */
+
+TEST(EXAMPLE, INIT)
+{
+  ASSERT_EQ(0, 0);
+}
+
+int main(int argc, char *argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
