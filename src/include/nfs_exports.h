3378be80a34544fcad0d23b3952c7d31323871e8
Matt Benjamin <mbenjamin@redhat.com>
2015-08-04 18:17:10 -0400

    c++ headers: avoid export identifiers in nfs_exports.h
    Change-Id: I20df60d646877a223dc5860f6352fadff9b68b6c
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/include/nfs_exports.h
+++ src/include/nfs_exports.h
@@ -216,14 +216,16 @@ int init_export_root(struct gsh_export *exp);
 fsal_status_t nfs_export_get_root_entry(struct gsh_export *export,
 					struct fsal_obj_handle **obj);
 void unexport(struct gsh_export *export);
+/* XXX */
 /*void kill_export_root_entry(cache_entry_t *entry);*/
 /*void kill_export_junction_entry(cache_entry_t *entry);*/
 
 int ReadExports(config_file_t in_config,
 		struct config_error_type *err_type);
 int reread_exports(config_file_t in_config,
-		   struct config_error_type *err_type);
-void free_export_resources(struct gsh_export *export);
+		struct config_error_type *err_type);
+void free_export_resources(struct gsh_export *exp);
+
 void exports_pkginit(void);
 
 #endif				/* !NFS_EXPORTS_H */
