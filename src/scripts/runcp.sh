8e896ad13d85835164d25b07e9f0b2322b93b84d
Frank S. Filz <ffilzlnx@mindspring.com>
2015-08-11 13:27:49 -0700

    Add copyright and license to runcp.sh
    Change-Id: I77da584ddd4f78a4db608bcc994b1e7fd51a29d6
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/scripts/runcp.sh
+++ src/scripts/runcp.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+#-------------------------------------------------------------------------------
+# Copyright Panasas, 2013
+# Contributor: Frank Filz  <ffilzlnx@mindspring.com>
+#
+#
+# This software is a server that implements the NFS protocol.
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#-------------------------------------------------------------------------------
+
 # Find directory where runcp.sh lives for checkpatch will be there too
 # We keep that in CURDIR
 CURDIR=$(dirname $(readlink -m $0))
