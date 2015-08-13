11118522529681d2cc62b862dafcb4339bbeff2b
Jim Lieb <lieb@sea-troll.net>
2015-08-12 18:02:51 -0700

    Relicense scripts/ganeshactl files to GPL/LGPLv3
    Makes sense to LGPL these since they are import files, not main scripts.
    Also onvert Python control scripts to GPLv3 to comply with the rest of
    the code
    
    Change-Id: I5ee6d1b5ae827fd8035729b86c89dd253adfa296
    Signed-off-by: Jim Lieb <lieb@sea-troll.net>

--- src/scripts/ganeshactl/Ganesha/admin.py
+++ src/scripts/ganeshactl/Ganesha/admin.py
@@ -4,8 +4,8 @@
 # Copyright (C) 2014 Panasas Inc.
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# it under the terms of the GNU Lessor General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
