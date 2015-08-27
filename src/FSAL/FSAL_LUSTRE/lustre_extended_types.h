d86fc4deae1fda12daf297e69f74b42f5f0dacb8
Dominique Martinet <dominique.martinet@cea.fr>
2015-08-27 15:34:45 +0200

    Relicense lustre_extended_types.h to GPLv2
    It was wrongly licensed as CeCILL
    
    Change-Id: Ia1511b50277cd0783d3da25d8941535f0df57935
    Signed-off-by: Dominique Martinet <dominique.martinet@cea.fr>

--- src/FSAL/FSAL_LUSTRE/lustre_extended_types.h
+++ src/FSAL/FSAL_LUSTRE/lustre_extended_types.h
@@ -1,15 +1,22 @@
-/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil; -*-
- * vim:noexpandtab:shiftwidth=4:tabstop=4:
- */
 /*
- * Copyright (C) 2009 CEA/DAM
+ * vim:noexpandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright CEA/DAM/DIF  (2013-2014)
+ * authors: Philippe DENIEL   philippe.deniel@cea.fr
+ *
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the CeCILL License.
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
  *
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license (http://www.cecill.info) and that you
- * accept its terms.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
