2d49b5d68b7d4efce9ff1631f10f0156c87699ac
Malahal Naineni <malahal@us.ibm.com>
2015-09-02 19:03:19 -0500

    Use request creds if root fails to open a file.
    Under some environments (IBM Active File Management Disaster Recovery
    secondary fileset) "root" is denied write access, so opening files for
    "write" with "root" creds will fail. If that happens, we use the request
    credentials to open the file.
    
    Change-Id: I33cee97b3cd628b2f02c4f9eca87dded8b18f3f6
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

--- src/FSAL/FSAL_GPFS/fsal_fileop.c
+++ src/FSAL/FSAL_GPFS/fsal_fileop.c
@@ -101,6 +101,19 @@ fsal_status_t GPFSFSAL_open(struct fsal_obj_handle *obj_hdl,
 	status = fsal_internal_handle2fd(gpfs_fs->root_fd, myself->handle,
 					 file_desc, posix_flags, reopen);
 
+	if (FSAL_IS_ERROR(status)) {
+		/* In some environments, "root" is denied write access,
+		 * so try with the request credentials if the above call
+		 * fails.
+		 */
+		fsal_set_credentials(p_context->creds);
+		status = fsal_internal_handle2fd(gpfs_fs->root_fd,
+						 myself->handle,
+						 file_desc, posix_flags,
+						 reopen);
+		fsal_restore_ganesha_credentials();
+	}
+
 	if (FSAL_IS_ERROR(status))
 		return status;
 
