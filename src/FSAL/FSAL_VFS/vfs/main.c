f0feb29dc4ea4f69422a364795c34b24a71e0bf1
Frank S. Filz <ffilzlnx@mindspring.com>
2015-09-09 11:05:17 -0700

    Enable support_ex for FSAL_VFS
    Change-Id: Id618c8dd867e2ff4ddc803d2562a6da5ed08c7ee
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/FSAL/FSAL_VFS/vfs/main.c
+++ src/FSAL/FSAL_VFS/vfs/main.c
@@ -72,7 +72,7 @@ static struct fsal_staticfsinfo_t default_posix_info = {
 	.case_insensitive = false,
 	.case_preserving = true,
 	.lock_support = false,
-	.lock_support_owner = false,
+	.lock_support_owner = true,
 	.lock_support_async_block = false,
 	.named_attr = true,
 	.unique_handles = true,
@@ -204,6 +204,18 @@ fsal_status_t vfs_create_export(struct fsal_module *fsal_hdl,
 				struct config_error_type *err_type,
 				const struct fsal_up_vector *up_ops);
 
+
+/**
+ * @brief Indicate support for extended operations.
+ *
+ * @retval true if extended operations are supported.
+ */
+
+bool vfs_support_ex(void)
+{
+	return true;
+}
+
 /* Module initialization.
  * Called by dlopen() to register the module
  * keep a private pointer to me in myself
@@ -230,6 +242,7 @@ MODULE_INIT void vfs_init(void)
 	}
 	myself->m_ops.create_export = vfs_create_export;
 	myself->m_ops.init_config = init_config;
+	myself->m_ops.support_ex = vfs_support_ex;
 }
 
 MODULE_FINI void vfs_unload(void)
