8e65b46de6337a00bfefff81bbd8a8a0f6288e45
Frank S. Filz <ffilzlnx@mindspring.com>
2015-10-05 11:32:10 -0700

    Fix minor spelling errors in license text
    Change-Id: I54ae6416f6cda5b472d497ea466401b5bfdc1517
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/scripts/ganeshactl/Ganesha/admin.py
+++ src/scripts/ganeshactl/Ganesha/admin.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2014 Panasas Inc.
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lessor General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
