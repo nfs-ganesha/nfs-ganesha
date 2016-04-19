168a1732620ff50333883f49d0878a5883eedde0
Soumya Koduri <skoduri@redhat.com>
2016-04-19 17:26:19 +0530

    FSAL_GLUSTER: enable support for extended fops
    Change-Id: I6ddce4977c65eabb183fcf9ed5379e7566b1c137
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL/FSAL_GLUSTER/main.c
+++ src/FSAL/FSAL_GLUSTER/main.c
@@ -121,6 +121,17 @@ static fsal_status_t init_config(struct fsal_module *fsal_hdl,
 	return fsalstat(ERR_FSAL_NO_ERROR, 0);
 }
 
+/**
+ * @brief Indicate support for extended operations.
+ *
+ * @retval true if extended operations are supported.
+ */
+
+bool glusterfs_support_ex(struct fsal_obj_handle *obj)
+{
+	return true;
+}
+
 /* Module methods
  */
 
@@ -142,6 +153,8 @@ MODULE_INIT void glusterfs_init(void)
 	/* setup global handle internals */
 	glfsal_module->fsal.m_ops.init_config = init_config;
 
+	/* Enable extended fop support */
+	glfsal_module->fsal.m_ops.support_ex = glusterfs_support_ex;
 	/*
 	 * Following inits needed for pNFS support
 	 * get device info will used by pnfs meta data server
