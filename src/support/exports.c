187e17a1ec753591d32bb32b5df08dea78185dd5
Malahal Naineni <malahal@us.ibm.com>
2016-07-14 18:07:58 -0500

    Explicitly set privilegedport to false in default PseudoFS export
    Things defined in export_defaults block are applied to all exports
    including the default pseudo export. If someone sets privilegedport to
    true in the export_defaults block, then it is not possible to allow any
    individual export to be accessible with non-privilegedport! In other
    words, the following config won't work today. Clients trying to mount
    "/export1" with non-privilegedport will fail as putrootfh will fail with
    NFS4ERR_ACCESS error.
    
    	export_defauls { privilegedport = true; }
    	export { path = /export1; privilegedport = false; }
    
    We should set the pseudo export with privilegedport to false, and let
    the value in export_defaults block apply only to regular exports.
    
    Change-Id: Ic499b3f7199ab212e66b851e0dd8b0b0406782ca
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 4cf28f1bad0aba00d3bad350bda81f6f47719202)

--- src/support/exports.c
+++ src/support/exports.c
@@ -1360,6 +1360,8 @@ static int build_default_root(struct config_error_type *err_type)
 	 * Root is allowed
 	 * MD Read Access
 	 * Allow use of default auth types
+	 *
+	 * Allow non-privileged client ports to access pseudo export.
 	 */
 	export->export_perms.options = EXPORT_OPTION_ROOT |
 					EXPORT_OPTION_MD_READ_ACCESS |
@@ -1371,7 +1373,8 @@ static int build_default_root(struct config_error_type *err_type)
 				    EXPORT_OPTION_ACCESS_MASK |
 				    EXPORT_OPTION_PROTOCOLS |
 				    EXPORT_OPTION_TRANSPORTS |
-				    EXPORT_OPTION_AUTH_TYPES;
+				    EXPORT_OPTION_AUTH_TYPES |
+				    EXPORT_OPTION_PRIVILEGED_PORT;
 
 	export->options = EXPORT_OPTION_USE_COOKIE_VERIFIER;
 	export->options_set = EXPORT_OPTION_FSID_SET |
