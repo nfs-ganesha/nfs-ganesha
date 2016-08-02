3da3129dc3cb8f8afb61f3ff40db31ba69eeadee
Daniel Gryniewicz <dang@redhat.com>
2016-08-02 15:02:06 -0400

    find_package implementation for CEPH FSALs
    Add find_package for CephFS and RGW.  These will properly override a
    system install (or even a prefix install) if CEPH_PREFIX or RGW_PREFIX
    are given.
    
    Change-Id: Ie40abee501980080d0543babcb42256fbdceeb94
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>
    (cherry picked from commit 399da8850cab9f4ad9ffb1ccff198847975b7de5)
    
    Conflicts:
    	src/CMakeLists.txt
    	src/tools/multilock/CMakeLists.txt
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

---
+++ src/cmake/modules/FindRGW.cmake
@@ -0,0 +1,70 @@
+# - Find RGW
+# Find the Linux Trace Toolkit - next generation with associated includes path.
+# See http://ceph.org/
+#
+# This module accepts the following optional variables:
+#    RGW_PREFIX   = A hint on RGW install path.
+#
+# This module defines the following variables:
+#    RGW_FOUND       = Was RGW found or not?
+#    RGW_LIBRARIES   = The list of libraries to link to when using RGW
+#    RGW_INCLUDE_DIR = The path to RGW include directory
+#
+# On can set RGW_PREFIX before using find_package(RGW) and the
+# module with use the PATH as a hint to find RGW.
+#
+# The hint can be given on the command line too:
+#   cmake -DRGW_PREFIX=/DATA/ERIC/RGW /path/to/source
+
+if(RGW_PREFIX)
+  message(STATUS "FindRGW: using PATH HINT: ${RGW_PREFIX}")
+  # Try to make the prefix override the normal paths
+  find_path(RGW_INCLUDE_DIR
+    NAMES include/rados/librgw.h
+    PATHS ${RGW_PREFIX}
+    NO_DEFAULT_PATH
+    DOC "The RGW include headers")
+    message("RGW_INCLUDE_DIR ${RGW_INCLUDE_DIR}")
+
+  find_path(RGW_LIBRARY_DIR
+    NAMES librgw.so
+    PATHS ${RGW_PREFIX}
+    PATH_SUFFIXES lib lib64
+    NO_DEFAULT_PATH
+    DOC "The RGW libraries")
+endif()
+
+if (NOT RGW_INCLUDE_DIR)
+  find_path(RGW_INCLUDE_DIR
+    NAMES include/rados/librgw.h
+    PATHS ${RGW_PREFIX}
+    DOC "The RGW include headers")
+endif (NOT RGW_INCLUDE_DIR)
+
+if (NOT RGW_LIBRARY_DIR)
+  find_path(RGW_LIBRARY_DIR
+    NAMES librgw.so
+    PATHS ${RGW_PREFIX}
+    PATH_SUFFIXES lib lib64
+    DOC "The RGW libraries")
+endif (NOT RGW_LIBRARY_DIR)
+
+find_library(RGW_LIBRARY rgw PATHS ${RGW_LIBRARY_DIR})
+check_library_exists(rgw rgw_mount ${RGW_LIBRARY_DIR} RGWLIB)
+if (NOT RGWLIB)
+  unset(RGW_LIBRARY_DIR CACHE)
+  unset(RGW_INCLUDE_DIR CACHE)
+endif (NOT RGWLIB)
+
+set(RGW_LIBRARIES ${RGW_LIBRARY})
+
+# handle the QUIETLY and REQUIRED arguments and set PRELUDE_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(RGW
+  REQUIRED_VARS RGW_INCLUDE_DIR RGW_LIBRARY_DIR)
+# VERSION FPHSA options not handled by CMake version < 2.8.2)
+#                                  VERSION_VAR)
+mark_as_advanced(RGW_INCLUDE_DIR)
+mark_as_advanced(RGW_LIBRARY_DIR)
+
