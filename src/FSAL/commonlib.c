9bbd1c833d52d035acb1577ad050e2bc489441b6
Malahal Naineni <malahal@us.ibm.com>
2016-08-17 11:34:52 -0500

    Avoid unnecessary CRIT message in claim_posix_filesystems()
    commit 7067bf09249d891c0204128ae199407874e406fd added rescanning file
    systems if not found. callers of this function already log CRIT message
    if needed after rescanning.
    
    Change-Id: Ie97e621b10ed2a8ecc354dfea72e2e283e620d5b
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>

--- src/FSAL/commonlib.c
+++ src/FSAL/commonlib.c
@@ -1569,9 +1569,6 @@ int claim_posix_filesystems(const char *path,
 
 	/* Check if we found a filesystem */
 	if (root == NULL) {
-		LogCrit(COMPONENT_FSAL,
-			"No file system for export path %s",
-			path);
 		retval = ENOENT;
 		goto out;
 	}
