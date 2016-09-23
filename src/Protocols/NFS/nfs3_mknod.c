ab83cd1565cf6a7d2210a66151c31f22232b64d1
Soumya Koduri <skoduri@redhat.com>
2016-09-23 11:14:06 +0530

    MKNOD: Invalid check in nfs3_mknod
    nfs3_FSALToFhandle() returns true in case of successful creation of
    NFSv3 file handle. But the present check in nfs3_mknod considers this
    as error. Fixed the same.
    
    Change-Id: Icbfb7f5dd789af23b068c2d61a82b664c09e36d9
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/Protocols/NFS/nfs3_mknod.c
+++ src/Protocols/NFS/nfs3_mknod.c
@@ -209,7 +209,7 @@ int nfs3_mknod(nfs_arg_t *arg, struct svc_req *req, nfs_res_t *res)
 	if (!nfs3_FSALToFhandle(true,
 				&rok->obj.post_op_fh3_u.handle,
 				node_obj,
-				op_ctx->ctx_export) == 0) {
+				op_ctx->ctx_export)) {
 		res->res_mknod3.status = NFS3ERR_BADHANDLE;
 		rc = NFS_REQ_OK;
 		goto out;
