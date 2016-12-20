44f9a0f56aaa258d2221729e878e7479342aa70f
Malahal Naineni <malahal@us.ibm.com>
2016-12-20 15:28:18 -0500

    drc: (try to) restore the original intent of 8abb27f5
    The version of Malahal's change that was committed in the last
    merge cycle is less effective than the original change.  This
    change recreates the original behavior to compare the request
    hash values, not the partition number.
    
    (Revised to save the value of ov->hk across drc unlock section.)
    
    Change-Id: I8f5c5597ab2cc6a0c04521182568e45201068858
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/RPCAL/nfs_dupreq.c
+++ src/RPCAL/nfs_dupreq.c
@@ -1078,7 +1078,6 @@ dupreq_status_t nfs_dupreq_finish(struct svc_req *req, nfs_res_t *res_nfs)
 	dupreq_entry_t *ov = NULL, *dv = (dupreq_entry_t *)req->rq_u1;
 	dupreq_status_t status = DUPREQ_SUCCESS;
 	struct rbtree_x_part *t;
-	struct rbtree_x_part *saved_part;
 	drc_t *drc = NULL;
 	int16_t cnt = 0;
 
@@ -1131,7 +1130,7 @@ dq_retry:
 
 			/* remove dict entry */
 			t = rbtx_partition_of_scalar(&drc->xt, ov->hk);
-			saved_part = t;
+			uint64_t ov_hk = ov->hk;
 
 			/* Need to acquire partition lock, but the lock
 			 * order is partition lock followed by drc lock.
@@ -1146,15 +1145,11 @@ dq_retry:
 			 * dupreq entry from the list again.
 			 */
 			ov = TAILQ_FIRST(&drc->dupreq_q);
-			t = rbtx_partition_of_scalar(&drc->xt, ov->hk);
 
-			/* Make sure that we have the correct partition
-			 * lock for the dupreq we are going to work on.
-			 * If the saved partition is different from the
-			 * partition of the current dupreq, we need to
-			 * backout.
+			/* Make sure that we are removing the entry we
+			 * expected (imperfect, but harmless).
 			 */
-			if (t != saved_part) {
+			if (ov->hk != ov_hk) {
 				PTHREAD_MUTEX_unlock(&t->mtx);
 				goto unlock;
 			}
