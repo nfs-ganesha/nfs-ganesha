21b1b07a7c36667c57284db5c99a1be3e02a9f40
Soumya Koduri <skoduri@redhat.com>
2017-01-05 15:52:33 +0530

    FSAL_GLUSTER: Return appropriate error in case if lock acquisition fails
    FSAL_GLUSTER seem to be returning success even in case if posix
    lock acquisition fails. Fixed the same.
    
    Change-Id: I061ca05b49ff32fac00b0cbe50b6926a4569e5b5
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL/FSAL_GLUSTER/handle.c
+++ src/FSAL/FSAL_GLUSTER/handle.c
@@ -2106,6 +2106,7 @@ static fsal_status_t glusterfs_lock_op2(struct fsal_obj_handle *obj_hdl,
 
 	if (retval /* && lock_op == FSAL_OP_LOCK */) {
 		retval = errno;
+		int rc = 0;
 
 		LogDebug(COMPONENT_FSAL,
 			 "fcntl returned %d %s",
@@ -2113,10 +2114,10 @@ static fsal_status_t glusterfs_lock_op2(struct fsal_obj_handle *obj_hdl,
 
 		if (conflicting_lock != NULL) {
 			/* Get the conflicting lock */
-			retval = glfs_posix_lock(my_fd.glfd, F_GETLK,
+			rc = glfs_posix_lock(my_fd.glfd, F_GETLK,
 						 &lock_args);
 
-			if (retval) {
+			if (rc) {
 				retval = errno; /* we lose the initial error */
 				LogCrit(COMPONENT_FSAL,
 					"After failing a lock request, I couldn't even get the details of who owns the lock.");
