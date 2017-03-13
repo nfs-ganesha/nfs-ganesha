bdd2befe7cbc3961d1814830d9d44e7ed8cacf24
Patrice LUCAS <patrice.lucas@cea.fr>
2017-03-13 13:00:27 +0100

    FSAL_PROXY : fix pxy_close in support_ex case
    mdcache_lru_clean calls fsal_close and fsal_close calls fsal close
    function, even if we are in support_ex case.
    
    Change-Id: Ic8d22349fe0d6baf03e74dde0c91105cb5089fb2
    Signed-off-by: Patrice LUCAS <patrice.lucas@cea.fr>

--- src/FSAL/FSAL_PROXY/handle.c
+++ src/FSAL/FSAL_PROXY/handle.c
@@ -2086,17 +2086,12 @@ static fsal_status_t pxy_commit(struct fsal_obj_handle *obj_hdl,
 	return fsalstat(ERR_FSAL_NO_ERROR, 0);
 }
 
+/*
+ * In this first FSAL_PROXY support_ex version without state
+ * nothing to do on close.
+ */
 static fsal_status_t pxy_close(struct fsal_obj_handle *obj_hdl)
 {
-	struct pxy_obj_handle *ph;
-
-	if (!obj_hdl)
-		return fsalstat(ERR_FSAL_FAULT, EINVAL);
-
-	ph = container_of(obj_hdl, struct pxy_obj_handle, obj);
-	if (ph->openflags == FSAL_O_CLOSED)
-		return fsalstat(ERR_FSAL_NOT_OPENED, EBADF);
-	ph->openflags = FSAL_O_CLOSED;
 	return fsalstat(ERR_FSAL_NO_ERROR, 0);
 }
 
