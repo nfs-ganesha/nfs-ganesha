7af518f248295e0d99ed8caf4487b1153aae04af
Patrice LUCAS <patrice.lucas@cea.fr>
2017-03-14 13:41:27 +0100

    9P : fix open O_CREAT to set mode
    To set mode when creating a file through open2, mode should be pass
    through valid_mask instead of request_mask.
    
    Change-Id: I02c570662bce79bce33b5b1e0dc74b69ab5fe70f
    Signed-off-by: Patrice LUCAS <patrice.lucas@cea.fr>

--- src/Protocols/9P/9p_lcreate.c
+++ src/Protocols/9P/9p_lcreate.c
@@ -111,9 +111,10 @@ int _9p_lcreate(struct _9p_request_data *req9p, u32 *plenout, char *preply)
 		fsal_verifier_t verifier;
 		enum fsal_create_mode createmode = FSAL_UNCHECKED;
 
-		fsal_prepare_attrs(&sattr, ATTR_MODE | ATTR_GROUP);
 		memset(&verifier, 0, sizeof(verifier));
 
+		memset(&sattr, 0, sizeof(sattr));
+		sattr.valid_mask = ATTR_MODE | ATTR_GROUP;
 		sattr.mode = *mode;
 		sattr.group = *gid;
 
