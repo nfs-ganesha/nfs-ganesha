419727d477ad9fd7b0107dd7875c6c65deeb384e
Niels de Vos <ndevos@redhat.com>
2017-06-06 18:51:52 +0530

    rpm: include correct embedded libntirpc version
    There has been a pull-up for libntirpc, but the %files section in the
    .spec was not updated with the new version. This causes building the
    RPMs to fail in case libntirpc is embedded/bundled.
    
    Change-Id: I4b2a166644e6632b22642921e43845109532df59
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -482,7 +482,7 @@ exit 0
 %{_bindir}/ganesha.nfsd
 %if ! %{with system_ntirpc}
 %{_libdir}/libntirpc.so.@NTIRPC_VERSION_EMBED@
-%{_libdir}/libntirpc.so.1.4
+%{_libdir}/libntirpc.so.1.5
 %{_libdir}/libntirpc.so
 %{_libdir}/pkgconfig/libntirpc.pc
 %{_includedir}/ntirpc/
