a5fc5c1ad16ee1ad0102aab4cde6ffee30248b1f
Swen Schillig <swen@vnet.ibm.com>
2017-09-05 10:55:52 +0200

    setclientid: free clientid if client_r_addr is too long.
    In case of a too long client_r_addr the just created client_id
    needs to be free'd before returning.
    
    Change-Id: I72c343df7ab2e6a4c0974aec195bb51dab25054e
    Signed-off-by: Swen Schillig <swen@vnet.ibm.com>

--- src/Protocols/NFS/nfs4_op_setclientid.c
+++ src/Protocols/NFS/nfs4_op_setclientid.c
@@ -314,6 +314,7 @@ int nfs4_op_setclientid(struct nfs_argop4 *op, compound_data_t *data,
 		      sizeof(unconf->cid_cb.v40.cb_client_r_addr)) == -1) {
 		LogCrit(COMPONENT_CLIENTID, "Callback r_addr %s too long",
 			arg_SETCLIENTID4->callback.cb_location.r_addr);
+		free_client_id(unconf);
 		res_SETCLIENTID4->status = NFS4ERR_INVAL;
 
 		goto out;
