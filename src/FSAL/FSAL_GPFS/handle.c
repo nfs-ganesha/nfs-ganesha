084439d1d6e07aaf2e656a2055e82116264097dc
Swen Schillig <swen@vnet.ibm.com>
2017-09-07 13:29:36 +0200

    [GPFS] read_dirents: check status of FD gathering instead of FD itself.
    The status of fsal_internal_handle2fd has to be verified before
    processing with the otherwise uninitialized FD(dirfd).
    
    Change-Id: I15506090fca2a4a5f392e87f962e2ade73332cb4
    Signed-off-by: Swen Schillig <swen@vnet.ibm.com>
    (cherry picked from commit 32c723a0c583416099cbb40c744df5745e388a71)

--- src/FSAL/FSAL_GPFS/handle.c
+++ src/FSAL/FSAL_GPFS/handle.c
@@ -505,7 +505,7 @@ static fsal_status_t read_dirents(struct fsal_obj_handle *dir_hdl,
 	status = fsal_internal_handle2fd(export_fd, myself->handle,
 					 &dirfd, O_RDONLY | O_DIRECTORY);
 
-	if (dirfd < 0)
+	if (FSAL_IS_ERROR(status))
 		return status;
 
 	seekloc = lseek(dirfd, seekloc, SEEK_SET);
