013f249e9adaa9dff789a11e79ff846c92776998
Malahal Naineni <malahal@us.ibm.com>
2017-09-22 13:14:52 +0530

    Fix dec_client_record_ref accessing freed memory
    Let us say thread1 in dec_client_record_ref() decremented the refcount
    and found it to be 0. It is ready to delete from the hashtable but got
    scheduled out before acquiring the hash latch!
    
    Another thread, thread2, that needs this record, found the record in the
    hash table and incremented the refcount from 0 to 1.
    
    Also Assume that thread2 is done with the record and eventually called
    dec_client_record_ref() to release it. It decremented the refcount to
    zero. Now we have 2 threads, both finding the refcount as 0!
    
    One of the threads will win to acquire the hash latch and actually find
    the entry in the hash and free it. The other will use the freed memory
    to find the entry in the hash table! Worse yet, it may find a record
    inserted by some other thread (not its own) and free it!
    
    Fixed so that the refcount never goes up from 0 like other nlm/nsm hash
    tables.
    
    Change-Id: Iafcd46efa70c86c4ae0bc4d289e03b11c6b6f95d
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 7d0629b3bc647aa897eb70705858e5759b9911a6)

--- src/SAL/nfs4_clientid.c
+++ src/SAL/nfs4_clientid.c
@@ -1441,9 +1441,12 @@ int32_t dec_client_record_ref(nfs_client_record_t *record)
 	struct gsh_buffdesc old_value;
 	struct gsh_buffdesc old_key;
 	int32_t refcount;
+	bool str_valid = false;
 
-	if (isDebug(COMPONENT_CLIENTID))
+	if (isDebug(COMPONENT_CLIENTID)) {
 		display_client_record(&dspbuf, record);
+		str_valid = true;
+	}
 
 	refcount = atomic_dec_int32_t(&record->cr_refcount);
 
@@ -1455,48 +1458,49 @@ int32_t dec_client_record_ref(nfs_client_record_t *record)
 		return refcount;
 	}
 
+	assert(refcount == 0);
+
 	LogFullDebug(COMPONENT_CLIENTID, "Try to remove {%s}", str);
 
 	buffkey.addr = record;
 	buffkey.len = sizeof(*record);
 
-	/* Get the hash table entry and hold latch */
+	/* Since the refcount is zero, another thread that needs this
+	 * record might have deleted ours, so expect not to find one or
+	 * find someone else's record!
+	 */
 	rc = hashtable_getlatch(ht_client_record, &buffkey, &old_value, true,
 				&latch);
 
-	if (rc != HASHTABLE_SUCCESS) {
-		if (rc == HASHTABLE_ERROR_NO_SUCH_KEY)
-			hashtable_releaselatched(ht_client_record, &latch);
+	switch (rc) {
+	case HASHTABLE_SUCCESS:
+		/* If ours, delete from hash table */
+		if (old_value.addr == record) {
+			hashtable_deletelatched(ht_client_record, &buffkey,
+						&latch, &old_key, &old_value);
+		}
+		break;
 
-		display_client_record(&dspbuf, record);
+	case HASHTABLE_ERROR_NO_SUCH_KEY:
+		break;
 
+	default:
+		if (!str_valid) {
+			display_client_record(&dspbuf, record);
+		}
 		LogCrit(COMPONENT_CLIENTID, "Error %s, could not find {%s}",
 			hash_table_err_to_str(rc), str);
 
 		return refcount;
 	}
 
-	refcount = atomic_fetch_int32_t(&record->cr_refcount);
-
-	if (refcount > 0) {
-		LogDebug(COMPONENT_CLIENTID,
-			 "Did not release refcount now=%" PRId32 " {%s}",
-			 refcount, str);
-
-		hashtable_releaselatched(ht_client_record, &latch);
-		return refcount;
-	}
-
-	/* use the key to delete the entry */
-	hashtable_deletelatched(ht_client_record, &buffkey, &latch,
-				&old_key, &old_value);
-
 	/* Release the latch */
 	hashtable_releaselatched(ht_client_record, &latch);
 
-	LogFullDebug(COMPONENT_CLIENTID, "Free {%s}", str);
+	if (str_valid)
+		LogFullDebug(COMPONENT_CLIENTID, "Free {%s}", str);
 
-	free_client_record(old_value.addr);
+	free_client_record(record);
 
 	return refcount;
 }
@@ -1640,17 +1644,14 @@ nfs_client_record_t *get_client_record(const char *const value,
 				       const uint32_t server_addr)
 {
 	nfs_client_record_t *record;
+	nfs_client_record_t *old;
 	struct gsh_buffdesc buffkey;
 	struct gsh_buffdesc buffval;
 	struct hash_latch latch;
 	hash_error_t rc;
+	int32_t refcount;
 
-
-	if (len == 0) {
-		LogInfo(COMPONENT_CLIENTID,
-			"Refusing to create a record with an empty client_val, stopping.");
-		return NULL;
-	}
+	assert(len);
 
 	record = gsh_malloc(sizeof(nfs_client_record_t) + len);
 
@@ -1664,37 +1665,39 @@ nfs_client_record_t *get_client_record(const char *const value,
 	buffkey.addr = record;
 	buffkey.len = sizeof(*record);
 
-	/* If we found it, return it, if we don't care, return NULL */
 	rc = hashtable_getlatch(ht_client_record, &buffkey, &buffval, true,
 				&latch);
 
-	if (rc == HASHTABLE_SUCCESS) {
-		/* Discard the key we created and return the found
-		 * Client Record.  Directly free since we didn't
-		 * complete initialization.
-		 */
-		gsh_free(record);
-		record = buffval.addr;
-		inc_client_record_ref(record);
+	switch (rc) {
+	case HASHTABLE_SUCCESS:
+		old = buffval.addr;
+		refcount = atomic_inc_int32_t(&old->cr_refcount);
+		if (refcount == 1) {
+			/* This record is in the process of getting freed.
+			 * Delete from the hash table and pretend as
+			 * though we didn't find it!
+			 */
+			(void)atomic_dec_int32_t(&old->cr_refcount);
+			hashtable_deletelatched(ht_client_record, &buffkey,
+						&latch, NULL, NULL);
+			break;
+		}
+
+		/* Use the existing record */
 		hashtable_releaselatched(ht_client_record, &latch);
-		return record;
-	}
+		gsh_free(record);
+		return old;
 
-	/* Any other result other than no such key is an error */
-	if (rc != HASHTABLE_ERROR_NO_SUCH_KEY) {
-		/* Discard the key we created and return.  Directly
-		 * free since we didn't complete initialization.
-		 */
+	case HASHTABLE_ERROR_NO_SUCH_KEY:
+		break;
 
+	default:
 		LogFatal(COMPONENT_CLIENTID,
 			 "Client record hash table corrupt.");
-		gsh_free(record);
-		return NULL;
 	}
 
+	/* Initialize and insert the new record */
 	PTHREAD_MUTEX_init(&record->cr_mutex, NULL);
-
-	/* Use same record for record and key */
 	buffval.addr = record;
 	buffval.len = sizeof(nfs_client_record_t) + len;
 
@@ -1704,8 +1707,6 @@ nfs_client_record_t *get_client_record(const char *const value,
 	if (rc != HASHTABLE_SUCCESS) {
 		LogFatal(COMPONENT_CLIENTID,
 			 "Client record hash table corrupt.");
-		free_client_record(record);
-		return NULL;
 	}
 
 	return record;
