90815e3ffc599b7bac2a85b2c5c5264a41670eb0
Madhu Thorat <madhu.punjabi@in.ibm.com>
2017-10-05 14:42:43 -0400

    Set attributes_follow=false if 'valid_mask==ATTR_RDATTR_ERR' in readdirplus
    Currently in nfs3_readdirplus_callback() function nfs3_FSALattr_To_Fattr()
    is called and if "attr->valid_mask == ATTR_RDATTR_ERR" is true then
    nfs3_FSALattr_To_Fattr() logs a CRIT message as attributes are missing.
    Fixed this by adding a check for "attr->valid_mask == ATTR_RDATTR_ERR" in
    nfs3_readdirplus_callback(), and if this condition is true then
    "attributes_follow = false" is set. Else we call nfs3_FSALattr_To_Fattr().
    
    Change-Id: I4bb91bfa58df44ed613d00c5558432ce4f1e9248
    Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>

--- src/Protocols/NFS/nfs3_readdirplus.c
+++ src/Protocols/NFS/nfs3_readdirplus.c
@@ -468,11 +468,16 @@ fsal_errors_t nfs3_readdirplus_callback(void *opaque,
 		tracker->mem_left -=
 		    ep3->name_handle.post_op_fh3_u.handle.data.data_len + 12;
 
-		ep3->name_attributes.attributes_follow = true;
-
-		nfs3_FSALattr_To_Fattr(
-			obj, attr,
-			&ep3->name_attributes.post_op_attr_u.attributes);
+		if (attr->valid_mask == ATTR_RDATTR_ERR) {
+			/* Indicate no attributes follow */
+			ep3->name_attributes.attributes_follow = false;
+		} else {
+			/* Place the attributes that follow */
+			ep3->name_attributes.attributes_follow = true;
+			nfs3_FSALattr_To_Fattr(
+				obj, attr, &ep3->name_attributes.
+				post_op_attr_u.attributes);
+		}
 	} else {
 		ep3->name_handle.handle_follows = false;
 		ep3->name_attributes.attributes_follow = false;
