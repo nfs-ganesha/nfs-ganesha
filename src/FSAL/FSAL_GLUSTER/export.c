0819dc4a84a5ff231feae2e68a52eb8a3218b8d6
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2017-11-07 14:19:20 -0500

    fsal_gluster: glusterfs_create_export() SEGV for typo ganesha.conf
    small refactor to prevent a SEGV if calloc fails.
    
    Change-Id: I97a3e808858cb34a06db39bd40a11a14aaf4f5fd
    Signed-off-by: Kaleb S. KEITHLEY <kkeithle@redhat.com>

--- src/FSAL/FSAL_GLUSTER/export.c
+++ src/FSAL/FSAL_GLUSTER/export.c
@@ -875,6 +875,8 @@ fsal_status_t glusterfs_create_export(struct fsal_module *fsal_hdl,
 	LogDebug(COMPONENT_FSAL, "In args: export path = %s",
 		 op_ctx->ctx_export->fullpath);
 
+	glfsexport = gsh_calloc(1, sizeof(struct glusterfs_export));
+
 	rc = load_config_from_node(parse_node,
 				   &export_param,
 				   &params,
@@ -889,8 +891,6 @@ fsal_status_t glusterfs_create_export(struct fsal_module *fsal_hdl,
 	}
 	LogEvent(COMPONENT_FSAL, "Volume %s exported at : '%s'",
 		 params.glvolname, params.glvolpath);
-
-	glfsexport = gsh_calloc(1, sizeof(struct glusterfs_export));
 
 	fsal_export_init(&glfsexport->export);
 	export_ops_init(&glfsexport->export.exp_ops);
