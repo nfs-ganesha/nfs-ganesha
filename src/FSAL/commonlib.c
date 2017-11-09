afe231949ff7d2f39520f6295c687135cb2d1a65
Madhu Thorat <madhu.punjabi@in.ibm.com>
2017-11-09 07:58:17 -0500

    Give temp fd in fsal_reopen_obj when verification fails for a fd's openflags
    Currently in fsal_reopen_obj() if verification fails for a file
    descriptor's 'openflags' we return error 'EBADF'. This leads to EIO error.
    To avoid EIO error, in fsal_reopen_obj() if verification fails for a
    file descriptor we attempt to provide a temporary file descriptor.
    
    Change-Id: I8368b9e861df5fc5cb18ae03c7f3b01122868581
    Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>
    (cherry picked from commit 482672a78b9b85093204efccf369ab1fa421367f)

--- src/FSAL/commonlib.c
+++ src/FSAL/commonlib.c
@@ -2469,29 +2469,36 @@ again:
 
 	if (not_open_usable(my_fd->openflags, openflags)) {
 
-		/* Drop the rwlock */
+		/* Drop the read lock */
 		PTHREAD_RWLOCK_unlock(&obj_hdl->obj_lock);
 
 		if (retried) {
-			/* This really should never occur, it could occur
-			 * if there was some race with closing the file.
+			/* Since we drop write lock for 'obj_hdl->obj_lock'
+			 * and acquire read lock for 'obj_hdl->obj_lock' after
+			 * opening the global file descriptor, some other
+			 * thread could have closed the file causing
+			 * verification of 'openflags' to fail.
+			 *
+			 * We will now attempt to just provide a temporary
+			 * file descriptor
 			 */
 			LogDebug(COMPONENT_FSAL,
-				 "Retry failed, returning EBADF");
+				 "Retry failed.");
 			*has_lock = false;
-			return fsalstat(posix2fsal_error(EBADF), EBADF);
+		} else {
+			/* Switch to write lock on object to protect file
+			 * descriptor.
+			 * By using trylock, we don't block if another thread
+			 * is using the file descriptor right now. In that
+			 * case, we just open a temporary file descriptor.
+			 *
+			 * This prevents us from blocking for the duration of
+			 * an I/O request.
+			 */
+			rc = pthread_rwlock_trywrlock(&obj_hdl->obj_lock);
 		}
 
-		/* Switch to write lock on object to protect file descriptor.
-		 * By using trylock, we don't block if another thread is using
-		 * the file descriptor right now. In that case, we just open
-		 * a temporary file descriptor.
-		 *
-		 * This prevents us from blocking for the duration of an
-		 * I/O request.
-		 */
-		rc = pthread_rwlock_trywrlock(&obj_hdl->obj_lock);
-		if (rc == EBUSY) {
+		if (retried || rc == EBUSY) {
 			/* Someone else is using the file descriptor.
 			 * Just provide a temporary file descriptor.
 			 * We still take a read lock so we can protect the
