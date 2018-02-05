e5624ce44f86e149620a57281bd97436bea807eb
Girjesh Rajoria <grajoria@redhat.com>
2018-02-05 14:28:40 +0530

    gtest/test_link_latency: link latency microbenchmark
    gtest unit test that run link on single case, large loops
    and bypass cases to find average latency of the calls.
    
    Change-Id: Ie49bbf29e53a00a08dd2f03730988d22a3a29f9a
    Signed-off-by: Girjesh Rajoria <grajoria@redhat.com>

---
+++ src/gtest/test_link_latency.cc
@@ -0,0 +1,439 @@
+// -*- mode:C; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Copyright (C) 2018 Red Hat, Inc.
+ * Contributor : Girjesh Rajoria <grajoria@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+#include <sys/types.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <thread>
+#include <random>
+#include "gtest/gtest.h"
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/exception.hpp>
+#include <boost/program_options.hpp>
+
+extern "C" {
+/* Manually forward this, an 9P is not C++ safe */
+void admin_halt(void);
+/* Ganesha headers */
+#include "nfs_lib.h"
+#include "export_mgr.h"
+#include "nfs_exports.h"
+#include "sal_data.h"
+#include "fsal.h"
+#include "common_utils.h"
+/* For MDCACHE bypass.  Use with care */
+#include "../FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_debug.h"
+}
+
+#define TEST_FILE "link_latency"
+#define TEST_FILE_LINK "link_to_link_latency"
+#define DIR_COUNT 100000
+#define LOOP_COUNT 1000000
+#define NAMELEN 16
+
+namespace {
+
+  char* ganesha_conf = nullptr;
+  char* lpath = nullptr;
+  int dlevel = -1;
+  uint16_t export_id = 77;
+
+  int ganesha_server() {
+    /* XXX */
+    return nfs_libmain(
+      ganesha_conf,
+      lpath,
+      dlevel
+      );
+  }
+
+  class Environment : public ::testing::Environment {
+  public:
+    Environment() : ganesha(ganesha_server) {
+      using namespace std::literals;
+      std::this_thread::sleep_for(5s);
+    }
+
+    virtual ~Environment() {
+      admin_halt();
+      ganesha.join();
+    }
+
+    virtual void SetUp() { }
+
+    virtual void TearDown() {
+    }
+
+    std::thread ganesha;
+  };
+
+  class LinkEmptyLatencyTest : public ::testing::Test {
+  protected:
+
+    virtual void SetUp() {
+      fsal_status_t status;
+      struct attrlist attrs_out;
+
+      a_export = get_gsh_export(export_id);
+      ASSERT_NE(a_export, nullptr);
+
+      status = nfs_export_get_root_entry(a_export, &root_entry);
+      ASSERT_EQ(status.major, 0);
+      ASSERT_NE(root_entry, nullptr);
+
+      /* Ganesha call paths need real or forged context info */
+      memset(&user_credentials, 0, sizeof(struct user_cred));
+      memset(&req_ctx, 0, sizeof(struct req_op_context));
+      memset(&attrs, 0, sizeof(attrs));
+
+      req_ctx.ctx_export = a_export;
+      req_ctx.fsal_export = a_export->fsal_export;
+      req_ctx.creds = &user_credentials;
+
+      /* stashed in tls */
+      op_ctx = &req_ctx;
+
+      // create file for test
+      FSAL_SET_MASK(attrs.valid_mask,
+                    ATTR_MODE | ATTR_OWNER | ATTR_GROUP);
+      attrs.mode = 0777; /* XXX */
+      attrs.owner = 667;
+      attrs.group = 766;
+      fsal_prepare_attrs(&attrs_out, 0);
+
+      status = fsal_create(root_entry, TEST_FILE, REGULAR_FILE, &attrs, NULL,
+                           &test_file, &attrs_out);
+      ASSERT_EQ(status.major, 0);
+      ASSERT_NE(test_file, nullptr);
+
+      fsal_release_attrs(&attrs_out);
+    }
+
+    virtual void TearDown() {
+      fsal_status_t status;
+
+      status = fsal_remove(root_entry, TEST_FILE);
+      EXPECT_EQ(0, status.major);
+
+      root_entry->obj_ops.put_ref(root_entry);
+      root_entry = NULL;
+
+      put_gsh_export(a_export);
+      a_export = NULL;
+    }
+
+    struct req_op_context req_ctx;
+    struct user_cred user_credentials;
+    struct attrlist attrs;
+
+    struct gsh_export* a_export = nullptr;
+    struct fsal_obj_handle *root_entry = nullptr;
+    struct fsal_obj_handle *test_file = nullptr;
+  };
+
+  class LinkFullLatencyTest : public LinkEmptyLatencyTest {
+  protected:
+
+    virtual void SetUp() {
+      fsal_status_t status;
+      char fname[NAMELEN];
+      struct fsal_obj_handle *obj;
+      struct attrlist attrs_out;
+
+      LinkEmptyLatencyTest::SetUp();
+
+      /* create a bunch of dirents */
+      for (int i = 0; i < DIR_COUNT; ++i) {
+        fsal_prepare_attrs(&attrs_out, 0);
+        sprintf(fname, "file-%08x", i);
+
+        status = fsal_create(root_entry, fname, REGULAR_FILE, &attrs, NULL, &obj,
+                             &attrs_out);
+        ASSERT_EQ(status.major, 0);
+        ASSERT_NE(obj, nullptr);
+
+        fsal_release_attrs(&attrs_out);
+        obj->obj_ops.put_ref(obj);
+      }
+    }
+
+    virtual void TearDown() {
+      fsal_status_t status;
+      char fname[NAMELEN];
+
+      for (int i = 0; i < DIR_COUNT; ++i) {
+        sprintf(fname, "file-%08x", i);
+
+        status = fsal_remove(root_entry, fname);
+        EXPECT_EQ(status.major, 0);
+      }
+
+      LinkEmptyLatencyTest::TearDown();
+      }
+
+  };
+
+} /* namespace */
+
+TEST_F(LinkEmptyLatencyTest, SIMPLE)
+{
+  fsal_status_t status;
+  struct fsal_obj_handle *link;
+  struct fsal_obj_handle *lookup;
+
+  status = root_entry->obj_ops.link(test_file, root_entry, TEST_FILE_LINK);
+  EXPECT_EQ(status.major, 0);
+  root_entry->obj_ops.lookup(root_entry, TEST_FILE_LINK, &link, NULL);
+  root_entry->obj_ops.lookup(root_entry, TEST_FILE, &lookup, NULL);
+  EXPECT_EQ(lookup, link);
+
+  link->obj_ops.put_ref(link);
+  lookup->obj_ops.put_ref(lookup);
+
+  /* Remove link created while running test */
+  status = fsal_remove(root_entry, TEST_FILE_LINK);
+  ASSERT_EQ(status.major, 0);
+}
+
+TEST_F(LinkEmptyLatencyTest, SIMPLE_BYPASS)
+{
+  fsal_status_t status;
+  struct fsal_obj_handle *sub_hdl;
+  struct fsal_obj_handle *link;
+  struct fsal_obj_handle *lookup;
+
+  sub_hdl = mdcdb_get_sub_handle(root_entry);
+  ASSERT_NE(sub_hdl, nullptr);
+
+  status = nfs_export_get_root_entry(a_export, &sub_hdl);
+  ASSERT_EQ(status.major, 0);
+
+  status = sub_hdl->obj_ops.link(test_file, sub_hdl, TEST_FILE_LINK);
+  EXPECT_EQ(status.major, 0);
+  root_entry->obj_ops.lookup(root_entry, TEST_FILE_LINK, &link, NULL);
+  root_entry->obj_ops.lookup(root_entry, TEST_FILE, &lookup, NULL);
+  EXPECT_EQ(lookup, link);
+
+  link->obj_ops.put_ref(link);
+  lookup->obj_ops.put_ref(lookup);
+
+  /* Remove link created while running test */
+  status = fsal_remove(root_entry, TEST_FILE_LINK);
+  ASSERT_EQ(status.major, 0);
+}
+
+TEST_F(LinkEmptyLatencyTest, LOOP)
+{
+  fsal_status_t status;
+  char fname[NAMELEN];
+  struct timespec s_time, e_time;
+
+  now(&s_time);
+
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = root_entry->obj_ops.link(test_file, root_entry, fname);
+    EXPECT_EQ(status.major, 0);
+  }
+
+  now(&e_time);
+
+  fprintf(stderr, "Average time per link: %" PRIu64 " ns\n",
+          timespec_diff(&s_time, &e_time) / LOOP_COUNT);
+
+  /* Remove link created while running test */
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = fsal_remove(root_entry, fname);
+    ASSERT_EQ(status.major, 0);
+  }
+}
+
+TEST_F(LinkEmptyLatencyTest, FSALLINK)
+{
+  fsal_status_t status;
+  char fname[NAMELEN];
+  struct timespec s_time, e_time;
+
+  now(&s_time);
+
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = fsal_link(test_file, root_entry, fname);
+    EXPECT_EQ(status.major, 0);
+  }
+
+  now(&e_time);
+
+  fprintf(stderr, "Average time per fsal_link: %" PRIu64 " ns\n",
+          timespec_diff(&s_time, &e_time) / LOOP_COUNT);
+
+  /* Remove link created while running test */
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = fsal_remove(root_entry, fname);
+    ASSERT_EQ(status.major, 0);
+  }
+}
+
+TEST_F(LinkFullLatencyTest, BIG)
+{
+  fsal_status_t status;
+  char fname[NAMELEN];
+  struct timespec s_time, e_time;
+
+  now(&s_time);
+
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = root_entry->obj_ops.link(test_file, root_entry, fname);
+    ASSERT_EQ(status.major, 0) << " failed to link " << fname;
+  }
+
+  now(&e_time);
+
+  fprintf(stderr, "Average time per link: %" PRIu64 " ns\n",
+          timespec_diff(&s_time, &e_time) / LOOP_COUNT);
+
+  /* Remove link created while running test */
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = fsal_remove(root_entry, fname);
+    ASSERT_EQ(status.major, 0);
+  }
+}
+
+TEST_F(LinkFullLatencyTest, BIG_BYPASS)
+{
+  fsal_status_t status;
+  char fname[NAMELEN];
+  struct fsal_obj_handle *sub_hdl;
+  struct timespec s_time, e_time;
+
+  sub_hdl = mdcdb_get_sub_handle(root_entry);
+  ASSERT_NE(sub_hdl, nullptr);
+
+  status = nfs_export_get_root_entry(a_export, &sub_hdl);
+  ASSERT_EQ(status.major, 0);
+  ASSERT_NE(sub_hdl, nullptr);
+
+  now(&s_time);
+
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = sub_hdl->obj_ops.link(test_file, sub_hdl, fname);
+    ASSERT_EQ(status.major, 0) << " failed to link " << fname;
+  }
+
+  now(&e_time);
+
+  fprintf(stderr, "Average time per link: %" PRIu64 " ns\n",
+          timespec_diff(&s_time, &e_time) / LOOP_COUNT);
+
+  /* Remove link created while running test */
+  for (int i = 0; i < LOOP_COUNT; ++i) {
+    sprintf(fname, "link-%08x", i);
+
+    status = fsal_remove(root_entry, fname);
+    ASSERT_EQ(status.major, 0);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int code = 0;
+
+  using namespace std;
+  using namespace std::literals;
+  namespace po = boost::program_options;
+
+  po::options_description opts("program options");
+  po::variables_map vm;
+
+  try {
+
+    opts.add_options()
+      ("config", po::value<string>(),
+        "path to Ganesha conf file")
+
+      ("logfile", po::value<string>(),
+        "log to the provided file path")
+
+      ("export", po::value<uint16_t>(),
+        "id of export on which to operate (must exist)")
+
+      ("debug", po::value<string>(),
+        "ganesha debug level")
+      ;
+
+    po::variables_map::iterator vm_iter;
+    po::command_line_parser parser{argc, argv};
+    parser.options(opts).allow_unregistered();
+    po::store(parser.run(), vm);
+    po::notify(vm);
+
+    // use config vars--leaves them on the stack
+    vm_iter = vm.find("config");
+    if (vm_iter != vm.end()) {
+      ganesha_conf = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+    vm_iter = vm.find("logfile");
+    if (vm_iter != vm.end()) {
+      lpath = (char*) vm_iter->second.as<std::string>().c_str();
+    }
+    vm_iter = vm.find("debug");
+    if (vm_iter != vm.end()) {
+      dlevel = ReturnLevelAscii(
+        (char*) vm_iter->second.as<std::string>().c_str());
+    }
+    vm_iter = vm.find("export");
+    if (vm_iter != vm.end()) {
+      export_id = vm_iter->second.as<uint16_t>();
+    }
+
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(new Environment);
+
+    code  = RUN_ALL_TESTS();
+  }
+
+  catch(po::error& e) {
+    cout << "Error parsing opts " << e.what() << endl;
+  }
+
+  catch(...) {
+    cout << "Unhandled exception in main()" << endl;
+  }
+
+  return code;
+}
