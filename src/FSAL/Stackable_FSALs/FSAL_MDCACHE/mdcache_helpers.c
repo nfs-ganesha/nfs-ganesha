9f4ef2c96b5b3427bb418fe59c10415891ea5a60
Daniel Gryniewicz <dang@redhat.com>
2018-03-06 09:50:25 -0500

    MDCACHE - Initialize dirent structs in entry early
    When creating a new entry, we speculatively create one, and then try
    again to find it.  If another thread raced, we throw away the
    speculative one.  This cleanup, however, assumes all data structures
    have been correctly initialized, but some dirent related ones were not
    initialized until later, causing crash.
    
    Move these initializations to early setup of the entry.
    
    Change-Id: Ib25cc483a5fe4dc21e0811559868c994cddd28c4
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_helpers.c
@@ -160,10 +160,19 @@ static mdcache_entry_t *mdcache_alloc_handle(
 	/* mdcache handlers */
 	mdcache_handle_ops_init(&result->obj_handle.obj_ops);
 	/* state */
-	if (sub_handle->type == DIRECTORY)
+	if (sub_handle->type == DIRECTORY) {
 		result->obj_handle.state_hdl = &result->fsobj.fsdir.dhdl;
-	else
+		/* init avl tree */
+		mdcache_avl_init(result);
+
+		/* init chunk list and detached dirents list */
+		glist_init(&result->fsobj.fsdir.chunks);
+		glist_init(&result->fsobj.fsdir.detached);
+		(void) pthread_spin_init(&result->fsobj.fsdir.spin,
+					 PTHREAD_PROCESS_PRIVATE);
+	} else {
 		result->obj_handle.state_hdl = &result->fsobj.hdl;
+	}
 	state_hdl_init(result->obj_handle.state_hdl, result->obj_handle.type,
 		       &result->obj_handle);
 
@@ -668,14 +677,6 @@ mdcache_new_entry(struct mdcache_fsal_export *export,
 						   MDCACHE_DIR_POPULATED);
 		}
 
-		/* init avl tree */
-		mdcache_avl_init(nentry);
-
-		/* init chunk list and detached dirents list */
-		glist_init(&nentry->fsobj.fsdir.chunks);
-		glist_init(&nentry->fsobj.fsdir.detached);
-		(void) pthread_spin_init(&nentry->fsobj.fsdir.spin,
-					 PTHREAD_PROCESS_PRIVATE);
 		break;
 
 	case SYMBOLIC_LINK:
