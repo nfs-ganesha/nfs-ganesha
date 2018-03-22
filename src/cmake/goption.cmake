843caadf9a7c21070503840d3ee02fc55085eae9
Daniel Gryniewicz <dang@redhat.com>
2018-03-22 14:32:27 -0400

    Add new CMake option code
    CMake options were either on or off, and no way to tell if they were
    using the default value or if they were specified on the command line.
    This made it extremely difficult to error out if something was specified
    on the command line but the dependencies were not installed.
    
    Add a new pair of macros for option processing:
    - goption() is a replacement for option(), and it records the default
      value specially
    - gopt_check() should be called before using the option, and it
      determins whether or not the default was used, and sets <foo>_REQUIRED
      as a result.  This can be used to error out if the option was given on
      the command line.
    
    Most options are converted to this new API.
    
    Change-Id: I83609375ddc27648bc6eb2bb9fe73848f571a309
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

---
+++ src/cmake/goption.cmake
@@ -0,0 +1,65 @@
+# Check to see if a FSAL is on or off, and required
+#
+# This is to work around the broken option() system, which doesn't indicate
+# whether or not an option was given on the command line, but only whether it's
+# on or off (but not defaulted on or off).
+#
+# This consists of several macros:
+#
+# goption(<OPTION_NAME> <description> [ON | OFF])
+#     This sets up an option, and it's default value
+#
+# gopt_test(<OPTION_NAME>)
+#     This tests the value of <OPTION_NAME> and sets it to ON or OFF, and sets
+#     <OPTION_NAME>_REQUIRED to "" or "REQUIRED"
+#
+# This should be used as follows:
+# goption(USE_FSAL_TEST ON)
+# gopt_test(USE_FSAL_TEST)
+# if(USE_FSAL_TEST)
+#   find_package(test ${USE_FSAL_TEST_REQUIRED})
+#   if(NOT TEST_FOUND)
+#     message(WARNING "libtest not found; disabling FSAL_TEST")
+#     set(USE_FSAL_TEST OFF)
+#   endif(NOT TEST_FOUND)
+# endif(USE_FSAL_TEST)
+#
+# Alternatively, if your option doesn't use find_package(), the test can look like this:
+# if(USE_FSAL_TEST)
+#   <Whatever tests you run>
+#   if(NOT TEST_FOUND)
+#     if (USE_FSAL_TEST_REQUIRED)
+#       message(FATAL_ERROR "libtest not found but requested on command line")
+#     elseif (USE_FSAL_TEST_REQUIRED)
+#       message(WARNING "libtest not found; disabling FSAL_TEST")
+#       set(USE_FSAL_TEST OFF)
+#     endif (USE_FSAL_TEST_REQUIRED)
+#   endif(NOT TEST_FOUND)
+# endif(USE_FSAL_TEST)
+
+macro (goption OPTNAME DESC DEFVAL)
+  set(${OPTNAME} DEFAULT_${DEFVAL} CACHE STRING "${DESC}")
+  set_property(CACHE ${OPTNAME} PROPERTY STRINGS ON OFF)
+endmacro()
+
+
+macro (gopt_test OPTNAME)
+  #message(WARNING "${OPTNAME}: ${${OPTNAME}}")
+  if (${${OPTNAME}} MATCHES "DEFAULT_ON")
+    #message(WARNING "Trying to set ${OPTNAME}: to ON")
+    set(${OPTNAME} ON CACHE PATH "Option for ${OPTNAME}" FORCE)
+    set(${OPTNAME}_REQUIRED "")
+  elseif (${${OPTNAME}} MATCHES "DEFAULT_OFF")
+    #message(WARNING "Trying to set ${OPTNAME}: to OFF")
+    set(${OPTNAME} OFF CACHE PATH "Option for ${OPTNAME}" FORCE)
+    set(${OPTNAME}_REQUIRED "")
+  elseif (${OPTNAME})
+    #message(WARNING "Trying to set ${OPTNAME}: to ON REQ")
+    set(${OPTNAME} ON CACHE PATH "Option for ${OPTNAME}" FORCE)
+    set(${OPTNAME}_REQUIRED "REQUIRED")
+  else(${OPTNAME} MATCHES "OFF")
+    #message(WARNING "Trying to set ${OPTNAME}: to OFF REQ")
+    set(${OPTNAME} OFF CACHE PATH "Option for ${OPTNAME}" FORCE)
+    set(${OPTNAME}_REQUIRED "REQUIRED")
+  endif()
+endmacro()
