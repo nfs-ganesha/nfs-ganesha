8a81a0a3d1546b1dca52279b7c78632258205a30
Patrice LUCAS <patrice.lucas@cea.fr>
2018-04-05 10:26:21 +0200

    FSAL_PROXY : fix open/create of existing file after a remount
    The "CREATE" flag is useless and improper in an open by handle
    and lead to a NFS4ERR_INVAL answer from the standard NFS linux
    server.
    
    Change-Id: I5c05dd4afc9f31a7efaf697350e4654c19a89933
    Signed-off-by: Patrice LUCAS <patrice.lucas@cea.fr>

--- src/FSAL/FSAL_PROXY/handle.c
+++ src/FSAL/FSAL_PROXY/handle.c
@@ -2143,13 +2143,16 @@ static fsal_status_t fill_openhow_OPEN4args(openflag4 *openhow,
 					    fattr4 inattrs,
 					    enum fsal_create_mode createmode,
 					    fsal_verifier_t verifier,
-					    bool *setattr_needed)
+					    bool *setattr_needed,
+					    const char *name,
+					    fsal_openflags_t openflags)
 {
 	if (openhow == NULL)
 		return fsalstat(ERR_FSAL_INVAL, -1);
 
 	/* openhow */
-	if (createmode != FSAL_NO_CREATE) {
+	/* not an open by handle and flag create */
+	if (name && createmode != FSAL_NO_CREATE) {
 		createhow4 *how = &(openhow->openflag4_u.how);
 
 		openhow->opentype = OPEN4_CREATE;
@@ -2205,6 +2208,10 @@ static fsal_status_t fill_openhow_OPEN4args(openflag4 *openhow,
 		}
 	} else
 		openhow->opentype = OPEN4_NOCREATE;
+
+	/* include open by handle and TRUNCATE case in setattr_needed */
+	if (!name && openflags & FSAL_O_TRUNC)
+		*setattr_needed = true;
 
 	return fsalstat(ERR_FSAL_NO_ERROR, 0);
 }
@@ -2307,7 +2314,8 @@ static fsal_status_t pxy_open2(struct fsal_obj_handle *obj_hdl,
 		owner_len = strnlen(owner_val, sizeof(owner_val));
 		/* inattrs and openhow */
 		st = fill_openhow_OPEN4args(&openhow, inattrs, createmode,
-					    verifier, &setattr_needed);
+					    verifier, &setattr_needed, name,
+					    openflags);
 		if (FSAL_IS_ERROR(st)) {
 			nfs4_Fattr_Free(&inattrs);
 			return st;
