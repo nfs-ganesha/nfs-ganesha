500c754df3a46d77f88036e922424e49af1f9b65
Frank S. Filz <ffilzlnx@mindspring.com>
2018-04-12 10:58:48 -0700

    Replace cache_fds parameter and handling with better logic
    We don't need a config param because we always "cache" fds...
    
    We must always deny opening an fd if the hard limit is hit.
    
    Improve logging when high water and hard limit are hit.
    
    Move mdcache_lru_fds_available into mdcache_lru.c.
    
    Change-Id: I430dc62f30e97359551e772fd9b54629d5339f13
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.h
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_lru.h
@@ -39,6 +39,13 @@
 #include "log.h"
 #include "mdcache_int.h"
 
+enum fd_states {
+	FD_LOW,
+	FD_MIDDLE,
+	FD_HIGH,
+	FD_LIMIT,
+};
+
 /**
  * @file mdcache_lru.h
  * @author Matt Benjamin
@@ -81,7 +88,7 @@ struct lru_state {
 	uint32_t biggest_window;
 	uint64_t prev_fd_count;	/* previous # of open fds */
 	time_t prev_time;	/* previous time the gc thread was run. */
-	bool caching_fds;
+	uint32_t fd_state;
 };
 
 extern struct lru_state lru_state;
@@ -155,7 +162,6 @@ void mdcache_lru_cleanup_try_push(mdcache_entry_t *entry);
 						__func__, __LINE__)
 bool _mdcache_lru_unref(mdcache_entry_t *entry, uint32_t flags,
 			const char *func, int line);
-void lru_wake_thread(void);
 void mdcache_lru_kill_for_shutdown(mdcache_entry_t *entry);
 
 /**
@@ -188,15 +194,6 @@ static inline fsal_status_t mdcache_get(mdcache_entry_t *entry)
 static inline void mdcache_put(mdcache_entry_t *entry)
 {
 	mdcache_lru_unref(entry);
-}
-
-/**
- * Return true if we are currently caching file descriptors.
- */
-
-static inline bool mdcache_lru_caching_fds(void)
-{
-	return lru_state.caching_fds;
 }
 
 void lru_remove_chunk(struct dir_chunk *chunk);
