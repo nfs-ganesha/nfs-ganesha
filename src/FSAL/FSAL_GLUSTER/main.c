ff286eeaf69f94fc440b316e3740f2c0a37bc24e
Soumya Koduri <skoduri@redhat.com>
2018-10-09 12:56:58 +0530

    Enable delegations for FSAL_GLUSTER
    Basic functionality of Get/Set/Recall/Revoke of delegations
    is working.
    
    Change-Id: I77696d49252c2377c8df4a2e1a4d1da13926eae5
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/FSAL/FSAL_GLUSTER/main.c
+++ src/FSAL/FSAL_GLUSTER/main.c
@@ -73,6 +73,7 @@ struct glusterfs_fsal_module GlusterFS = {
 			.pnfs_mds = false,
 			.pnfs_ds = true,
 			.link_supports_permission_checks = true,
+			.delegations = FSAL_OPTION_FILE_DELEGATIONS,
 		}
 	}
 };
