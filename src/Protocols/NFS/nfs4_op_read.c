cea93715a88fcb5d60da349ea8a8fc4ce0aa1ec0
Frank S. Filz <ffilzlnx@mindspring.com>
2018-10-26 08:07:18 -0700

    Move setting of resp->resop for nfs4_read for clarity
    nfs4_read with setting resp->resop, but since READ_PLUS uses a response
    structure on the stack to call nfs4_read it doesn't get overwritten
    but it's not entirely clear, so add clarity.
    
    Change-Id: Icf8edb8bdbf19ede75a4d6f624410db82d6876de
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/Protocols/NFS/nfs4_op_read.c
+++ src/Protocols/NFS/nfs4_op_read.c
@@ -295,8 +295,6 @@ static int nfs4_read(struct nfs_argop4 *op, compound_data_t *data,
 						sizeof(struct iovec));
 	uint32_t resp_size;
 
-	/* Say we are managing NFS4_OP_READ */
-	resp->resop = NFS4_OP_READ;
 	res_READ4->status = NFS4_OK;
 
 	/* Do basic checks on a filehandle Only files can be read */
@@ -596,11 +594,10 @@ static int nfs4_read(struct nfs_argop4 *op, compound_data_t *data,
 int nfs4_op_read(struct nfs_argop4 *op, compound_data_t *data,
 		 struct nfs_resop4 *resp)
 {
-	int err;
+	/* Say we are managing NFS4_OP_READ */
+	resp->resop = NFS4_OP_READ;
 
-	err = nfs4_read(op, data, resp, FSAL_IO_READ, NULL);
-
-	return err;
+	return nfs4_read(op, data, resp, FSAL_IO_READ, NULL);
 }
 
 /**
@@ -637,6 +634,9 @@ void nfs4_op_read_Free(nfs_resop4 *res)
 int nfs4_op_read_plus(struct nfs_argop4 *op, compound_data_t *data,
 		      struct nfs_resop4 *resp)
 {
+	/* Create a response frame on the stack to pass into nfs4_read
+	 * we will then copy out of it into actual response frame.
+	 */
 	struct nfs_resop4 res;
 	struct io_info info;
 	/* Response */
