5c1dfa776364e2c169f3e38037d4cef56f96f9c9
Sachin Punadikar <psachin@in.ibm.com>
2019-01-28 00:15:47 -0500

    Status of "v4_full" is missing
    "ganesha_stats status" do not display status of NFSv4 detailed
    statistic counting (v4_full). Rectified the code to display the
    status.
    
    Change-Id: I53069b8a898f94de94be3ff2c8ed24e82ea0a6e5
    Signed-off-by: Sachin Punadikar <psachin@in.ibm.com>

--- src/scripts/ganeshactl/Ganesha/glib_dbus_stats.py
+++ src/scripts/ganeshactl/Ganesha/glib_dbus_stats.py
@@ -418,18 +418,17 @@ class StatsStatus():
 		 output += "Stats counting for NFS server is currently disabled\n"
 	    if self.status[3][0]:
 		output += "Stats counting for FSAL is enabled since: \n\t"
-		output += time.ctime(self.status[3][1][0]) + str(self.status[3][1][1]) + " nsecs"
+		output += time.ctime(self.status[3][1][0]) + str(self.status[3][1][1]) + " nsecs\n"
 	    else:
 		 output += "Stats counting for FSAL is currently disabled \n"
 	    if self.status[4][0]:
 		output += "Stats counting for v3_full is enabled since: \n\t"
-		output += time.ctime(self.status[4][1][0]) + str(self.status[4][1][1]) + " nsecs"
+		output += time.ctime(self.status[4][1][0]) + str(self.status[4][1][1]) + " nsecs\n"
 	    else:
 		 output += "Stats counting for v3_full is currently disabled \n"
-	    return output
-	    if self.status[6][0]:
+	    if self.status[5][0]:
 		output += "Stats counting for v4_full is enabled since: \n\t"
-		output += time.ctime(self.status[6][1][0]) + str(self.status[6][1][1]) + " nsecs"
+		output += time.ctime(self.status[5][1][0]) + str(self.status[5][1][1]) + " nsecs\n"
 	    else:
 		 output += "Stats counting for v4_full is currently disabled \n"
 	    return output
