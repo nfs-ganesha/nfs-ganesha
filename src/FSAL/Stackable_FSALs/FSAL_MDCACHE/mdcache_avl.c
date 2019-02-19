6fe75249b2f6adeec6c6f6d5ffbfaf559daa72f1
Daniel Gryniewicz <dang@redhat.com>
2019-02-19 11:24:29 -0500

    MDCACHE - Ref the first chunk during readdir
    When doing a readdir, we know that we are going to have to return
    dirents from the first chunk to the client.  Protect this chunk from
    being freed by taking a ref.  To do this, take a ref in
    mdcache_populate_dir_chunk() and mdcache_avl_lookup_ck()
    
    Change-Id: I10d5e76bb1fc9c21b425f201271feab22132a400
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>
    (cherry picked from commit b624a1861cdc8bcc3b93a2d4c39abfc30866eb4d)

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_avl.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_avl.c
@@ -125,12 +125,16 @@ avl_dirent_set_deleted(mdcache_entry_t *entry, mdcache_dir_entry_t *v)
 				 *        the chunk...
 				 */
 
+
 				/* Look for the next chunk */
 				if (chunk->next_ck != 0 &&
 				    mdcache_avl_lookup_ck(parent,
 							  chunk->next_ck,
 							  &next)) {
 					chunk = next->chunk;
+					/* We don't need the ref, we have the
+					 * content lock */
+					mdcache_lru_unref_chunk(chunk);
 				}
 			}
 
@@ -506,6 +510,8 @@ out:
  *
  * Look up a dirent by FSAL cookie.
  *
+ * @note this takes a ref on the chunk containing @a dirent
+ *
  * @param[in] entry	Directory to search in
  * @param[in] ck	FSAL cookie to find
  * @param[out] dirent	Returned dirent, if found, NULL otherwise
@@ -542,6 +548,7 @@ bool mdcache_avl_lookup_ck(mdcache_entry_t *entry, uint64_t ck,
 			assert(chunk);
 			return false;
 		}
+		mdcache_lru_ref_chunk(chunk);
 		*dirent = ent;
 		return true;
 	}
