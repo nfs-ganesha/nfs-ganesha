b78896590c80e76926717971574100dc8a5b3a40
Malahal Naineni <malahal@us.ibm.com>
2019-04-23 13:01:53 +0530

    Fix memory leak in nlm4_Lock()
    When there is a conflict for non-blocking Lock request, we allocate
    memory using fill_netobj() but conflict information is not needed, so
    avoid allocation of such memory by passing NULL pointer to state_lock()
    call.
    
    Minor clean up replacing res_nlm4test with res_nlm4.
    
    Change-Id: Ib26c40b0b1ccc7933803d553c55dd1853397363f
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 191d5a2dd5cdaa9689ee272482a52989eaff3ad0)

--- src/Protocols/NLM/nlm_Lock.c
+++ src/Protocols/NLM/nlm_Lock.c
@@ -49,9 +49,9 @@ int nlm4_Lock(nfs_arg_t *args, struct svc_req *req, nfs_res_t *res)
 	char buffer[MAXNETOBJ_SZ * 2] = "\0";
 	state_nsm_client_t *nsm_client;
 	state_nlm_client_t *nlm_client;
-	state_owner_t *nlm_owner, *holder;
+	state_owner_t *nlm_owner;
 	state_t *nlm_state;
-	fsal_lock_param_t lock, conflict;
+	fsal_lock_param_t lock;
 	int rc;
 	bool grace = nfs_in_grace();
 	state_block_data_t *pblock_data = NULL;
@@ -86,7 +86,7 @@ int nlm4_Lock(nfs_arg_t *args, struct svc_req *req, nfs_res_t *res)
 		 (unsigned long long)arg->alock.l_len, buffer,
 		 arg->reclaim ? "yes" : "no");
 
-	copy_netobj(&res->res_nlm4test.cookie, &arg->cookie);
+	copy_netobj(&res->res_nlm4.cookie, &arg->cookie);
 
 	if (grace) {
 		/* allow only reclaim lock request during recovery */
@@ -159,8 +159,8 @@ int nlm4_Lock(nfs_arg_t *args, struct svc_req *req, nfs_res_t *res)
 					       STATE_NON_BLOCKING,
 				  arg->block ? &pblock_data : NULL,
 				  &lock,
-				  &holder,
-				  &conflict);
+				  NULL, /* We don't need conflict info */
+				  NULL);
 	PTHREAD_RWLOCK_unlock(&obj->state_hdl->state_lock);
 
 	/* We prevented delegations from being granted while trying to acquire
@@ -170,15 +170,8 @@ int nlm4_Lock(nfs_arg_t *args, struct svc_req *req, nfs_res_t *res)
 	(void) atomic_dec_uint32_t(&obj->state_hdl->file.anon_ops);
 
 	if (state_status != STATE_SUCCESS) {
-		res->res_nlm4test.test_stat.stat =
+		res->res_nlm4.stat.stat =
 				nlm_convert_state_error(state_status);
-
-		if (state_status == STATE_LOCK_CONFLICT) {
-			nlm_process_conflict(
-			    &res->res_nlm4test.test_stat.nlm4_testrply_u.holder,
-			    holder,
-			    &conflict);
-		}
 
 		if (state_status == STATE_IN_GRACE) {
 			res->res_nlm4.stat.stat = NLM4_DENIED_GRACE_PERIOD;
@@ -300,5 +293,5 @@ int nlm4_Lock_Message(nfs_arg_t *args, struct svc_req *req, nfs_res_t *res)
  */
 void nlm4_Lock_Free(nfs_res_t *res)
 {
-	netobj_free(&res->res_nlm4test.cookie);
+	netobj_free(&res->res_nlm4.cookie);
 }
