6a2a2a7676aa2b2536c672ef506cb41524b149c0
Daniel Gryniewicz <dang@redhat.com>
2019-08-12 14:54:36 -0400

    FSAL_RGW - Track openflags per state
    Change-Id: I4573a50eddc20a1c6943d4c0401fa3f348ac618e
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/FSAL/FSAL_RGW/handle.c
+++ src/FSAL/FSAL_RGW/handle.c
@@ -858,6 +858,12 @@ fsal_status_t rgw_fsal_open2(struct fsal_obj_handle *obj_hdl,
 			}
 		}
 
+		if (state) {
+			open_state->openflags = FSAL_O_NFS_FLAGS(openflags);
+		} else {
+			handle->openflags = FSAL_O_NFS_FLAGS(openflags);
+		}
+
 		if (createmode >= FSAL_EXCLUSIVE || truncated) {
 			/* refresh attributes */
 			rc = rgw_getattr(export->rgw_fs, handle->rgw_fh, &st,
@@ -1090,7 +1096,11 @@ fsal_status_t rgw_fsal_open2(struct fsal_obj_handle *obj_hdl,
 
 	my_fd->fd = fd;
 #endif
-	handle->openflags = FSAL_O_NFS_FLAGS(openflags);
+	if (state) {
+		open_state->openflags = FSAL_O_NFS_FLAGS(openflags);
+	} else {
+		handle->openflags = FSAL_O_NFS_FLAGS(openflags);
+	}
 
 	*new_obj = &obj->handle;
 
@@ -1513,6 +1523,7 @@ fsal_status_t rgw_fsal_close2(struct fsal_obj_handle *obj_hdl,
 	int rc;
 	struct rgw_open_state *open_state;
 	fsal_status_t status;
+	fsal_openflags_t *openflags;
 
 	struct rgw_export *export =
 	    container_of(op_ctx->fsal_export, struct rgw_export, export);
@@ -1544,9 +1555,13 @@ fsal_status_t rgw_fsal_close2(struct fsal_obj_handle *obj_hdl,
 					FSAL_O_CLOSED);
 
 		}
+
+		openflags = &open_state->openflags;
+	} else {
+		openflags = &handle->openflags;
 	}
 
-	if (unlikely(handle->openflags == FSAL_O_CLOSED)) {
+	if (unlikely(*openflags == FSAL_O_CLOSED)) {
 		status = fsalstat(ERR_FSAL_NOT_OPENED, 0);
 	} else {
 		rc = rgw_close(export->rgw_fs, handle->rgw_fh,
@@ -1554,7 +1569,7 @@ fsal_status_t rgw_fsal_close2(struct fsal_obj_handle *obj_hdl,
 		if (rc < 0) {
 			status = rgw2fsal_error(rc);
 		} else {
-			handle->openflags = FSAL_O_CLOSED;
+			*openflags = FSAL_O_CLOSED;
 			status = fsalstat(ERR_FSAL_NO_ERROR, 0);
 		}
 	}
