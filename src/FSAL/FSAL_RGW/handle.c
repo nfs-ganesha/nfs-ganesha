7dd27e990b90e58f85303e3776a1d8ab07f9bb14
Daniel Gryniewicz <dang@redhat.com>
2019-09-10 12:07:35 -0400

    FSAL_RGW - On open/create, use new handle not parent
    In the create-on-open case, handle is the parent handle, and obj is the
    new handle.  Store the openflags in the new handle.
    
    Change-Id: I1f8cdfb938f6990b26e7aa48c9040d7ae43ff025
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/FSAL/FSAL_RGW/handle.c
+++ src/FSAL/FSAL_RGW/handle.c
@@ -1099,7 +1099,7 @@ fsal_status_t rgw_fsal_open2(struct fsal_obj_handle *obj_hdl,
 	if (state) {
 		open_state->openflags = FSAL_O_NFS_FLAGS(openflags);
 	} else {
-		handle->openflags = FSAL_O_NFS_FLAGS(openflags);
+		obj->openflags = FSAL_O_NFS_FLAGS(openflags);
 	}
 
 	*new_obj = &obj->handle;
