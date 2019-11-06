5e72d5e0e991d6b3549192ec6b913926d79e7011
Daniel Gryniewicz <dang@redhat.com>
2019-11-06 14:14:54 -0500

    Pull up ntirpc to 3.0
    Change-Id: I6c6b8ad7598d6052676898b4f92eb34605c7a902
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/libganeshaNFS.pc.cmake
+++ src/libganeshaNFS.pc.cmake
@@ -6,6 +6,6 @@ includedir=${prefix}/include
 Name:  libganeshaNFS
 Description: NFS-GANESHA's "fuselike" library
 Requires:
-Version: @GANESHA_MAJOR_VERSION@.@GANESHA_MINOR_VERSION@.@GANESHA_PATCH_LEVEL@
+Version: @GANESHA_MAJOR_VERSION@@GANESHA_MINOR_VERSION@
 Libs: -L${exec_prefix}/lib@LIB_SUFFIX@ -lganeshaNFS
 Cflags: -I@INCLUDE_INSTALL_DIR@
