a3fc416a7d51cc957deac39625cfe506e76f7380
Madhu Thorat <madhu.punjabi@in.ibm.com>
2019-12-02 14:03:48 -0500

    IBM-only: Add ENV_OPTIONS in /etc/sysconfig/ganesha file to have ASAN options
    Added ASAN options to /etc/sysconfig/ganesha file with ENV_OPTIONS,
    but commented this line with ENV_OPTIONS because we don't build
    ganesha with ASAN by default.
    Note, in ASAN options as of now leak detection is disabled by using
    option "detect_leaks=false".
    
    Signed-off-by: Madhu Thorat <madhu.punjabi@in.ibm.com>
    (cherry picked from commit 4b6f4888ed4f2448e65917e7470515b160826e90)

--- src/scripts/systemd/sysconfig/nfs-ganesha-in.cmake
+++ src/scripts/systemd/sysconfig/nfs-ganesha-in.cmake
@@ -1,2 +1,3 @@
 OPTIONS="-L /var/log/ganesha.log -f /etc/ganesha/ganesha.conf -N NIV_EVENT"
 EPOCH_EXEC="@LIBEXECDIR@/ganesha/gpfs-epoch"
+#ENV_OPTIONS="ASAN_OPTIONS=alloc_dealloc_mismatch=true:print_stats=true:atexit=true:start_deactivated=false:print_cmdline=true:quarantine_size_mb=1:detect_leaks=false:abort_on_error=true:disable_coredump=false:verbosity=2:log_path=/tmp/asan.log:log_exe_name=1"
