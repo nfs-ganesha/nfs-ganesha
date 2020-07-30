dea3366f213f9cae4c5721928c6a566fb41cd549
Daniel Gryniewicz <dang@redhat.com>
2020-07-30 10:23:36 -0400

    RPM - gpfs-epoch is conditional
    gpfs-epoch is conditional on ADMIN_TOOLS now, but the spec file wasn't
    updated, causing "make rpm" to fail if ADMIN_TOOLS was off.  Fix the
    conditional in the spec file.
    
    Change-Id: I9ab5a9947f8e658eefcdab1cbd1afbe5cabbfa01
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -762,7 +762,9 @@ exit 0
 %config(noreplace) %{_sysconfdir}/ganesha/gpfs.ganesha.main.conf
 %config(noreplace) %{_sysconfdir}/ganesha/gpfs.ganesha.log.conf
 %config(noreplace) %{_sysconfdir}/ganesha/gpfs.ganesha.exports.conf
+%if %{with utils}
 %{_libexecdir}/ganesha/gpfs-epoch
+%endif
 %if %{with man_page}
 %{_mandir}/*/ganesha-gpfs-config.8.gz
 %endif
