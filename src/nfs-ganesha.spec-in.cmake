11efbd530e9dfafafd9d89d190e3b7a7fd6e6552
Malahal Naineni <malahal@us.ibm.com>
2020-08-12 18:54:41 +0530

    IBM-only: [Scale 5.1] use python2 with RHEL7.x/CentsOS7.x
    Some python3 modules (e.g. python3-dbus, python3-pyparsing) don't exist
    on RHEL7.x distro, so use python2 on RHEL7.x and CentOS7.x distros.
    
    Change-Id: I4a1341a1dbb481cfbcfa734b741419c5458e2916
    Signed-off-by: Malahal Naineni <malahal@us.ibm.com>
    (cherry picked from commit 988421a73566edc1028e4a75b354ac20da42b959)

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -255,6 +255,7 @@ Requires: 	gpfs.nfs-ganesha = %{version}-%{release}, python3
 BuildRequires:  python3-devel
 %endif
 %endif
+Requires: 	gpfs.nfs-ganesha = %{version}-%{release}
 
 %if %{with gui_utils}
 %if ( 0%{?suse_version} )
@@ -628,8 +629,13 @@ install -p -m 644 selinux/ganesha.if %{buildroot}%{_selinux_store_path}/devel/in
 install -m 0644 selinux/ganesha.pp.bz2 %{buildroot}%{_selinux_store_path}/packages
 %endif
 
+%if (0%{?rhel} && 0%{?rhel} < 8)
+rm -f %{buildroot}/%{python2_sitelib}/gpfs*
+rm -f %{buildroot}/%{python2_sitelib}/__init__.*
+%else
 rm -f %{buildroot}/%{python3_sitelib}/gpfs*
 rm -f %{buildroot}/%{python3_sitelib}/__init__.*
+%endif
 
 %post
 %if ( 0%{?suse_version} )
@@ -843,13 +849,14 @@ exit 0
 
 %if %{with utils}
 %files utils
-%if ( 0%{?rhel} && 0%{?rhel} < 8 )
-%{python_sitelib}/Ganesha/*
-%{python_sitelib}/ganeshactl-*-info
+%if (0%{?rhel} && 0%{?rhel} < 8)
+%{python2_sitelib}/Ganesha/*
+%{python2_sitelib}/ganeshactl-*-info
 %else
 %{python3_sitelib}/Ganesha/*
 %{python3_sitelib}/ganeshactl-*-info
 %endif
+
 %if %{with gui_utils}
 %{_bindir}/ganesha-admin
 %{_bindir}/manage_clients
