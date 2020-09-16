nfs-ganesha/nfs-ganesha

commit d049c1e632e4404837a24bd915e3024054885a6a

Initialize stat timestamps on process startup.
The stat's start time is only initialized during reset or enable
dbus calls. If enabled through config and restart ganesha,
timestamps on dbus shows up as zero.

Change-Id: I85a30c30085e1667494a0a1718d35529139255b1
Signed-off-by: Pradeep <pradeepthomas@gmail.com>

src/support/export_mgr.c
@@ -2298,6 +2298,21 @@ static void reset_fsal_stats(void)
 	}
 }
 
+
+/**
+ * Initialize all stats at startup time.
+ *
+ */
+void nfs_init_stats_time(void)
+{
+	now(&nfs_stats_time);
+	fsal_stats_time = v3_full_stats_time
+			= v4_full_stats_time
+			= auth_stats_time
+			= clnt_allops_stats_time
+			= nfs_stats_time;
+}
+
 /**
  * DBUS method to reset all ops statistics
  *
@@ -2321,9 +2336,7 @@ static bool stats_reset(DBusMessageIter *args,
 	reset_auth_stats();
 
 	/* update the stats counting time */
-	now(&nfs_stats_time);
-	fsal_stats_time = v3_full_stats_time = v4_full_stats_time =
-		auth_stats_time = nfs_stats_time;
+	nfs_init_stats_time();
 	return true;
 }
 

src/MainNFSD/nfs_init.c
@@ -944,7 +944,7 @@ void nfs_start(nfs_start_info_t *p_start_info)
 		 "-------------------------------------------------");
 
 	/* Set the time of NFS stat counting */
-	now(&nfs_stats_time);
+	nfs_init_stats_time();
 
 	/* Wait for dispatcher to exit */
 	LogDebug(COMPONENT_THREAD, "Wait for admin thread to exit");

src/include/export_mgr.h
@@ -373,5 +373,6 @@ void prune_defunct_exports(uint64_t generation);
 void remove_all_exports(void);
 
 extern struct timespec nfs_stats_time;
+void nfs_init_stats_time(void);
 #endif				/* !EXPORT_MGR_H */
 /** @} */

commit cb725625afe4cb1b886bbae32ed3d849e0d11a88

Move nfs_init_stats_time outside USE_DBUS.
d049c1e632e440 broke builds that don't have
USE_DBUS turned on (it's not compiled into
export_mgr.o otherwise).

Change-Id: Ie3ca0834b9af2c39ff77d984e7d5ead798d3a971
Signed-off-by: Solomon Boulos <boulos@google.com>

src/support/export_mgr.c
@@ -916,6 +916,25 @@ void prune_defunct_exports(uint64_t generation)
 	release_op_context();
 }
 
+/**
+ * @brief Initialize all stats at startup time.
+ *
+ * Note: This function needs to be in all builds, not just USE_DBUS enabled
+ * ones.
+ *
+ */
+
+void nfs_init_stats_time(void)
+{
+	now(&nfs_stats_time);
+	fsal_stats_time = v3_full_stats_time
+			= v4_full_stats_time
+			= auth_stats_time
+			= clnt_allops_stats_time
+			= nfs_stats_time;
+}
+
+
 #ifdef USE_DBUS
 
 /* DBUS interfaces
@@ -2296,21 +2315,6 @@ static void reset_fsal_stats(void)
 		if (m->stats != NULL)
 			m->m_ops.fsal_reset_stats(m);
 	}
-}
-
-
-/**
- * Initialize all stats at startup time.
- *
- */
-void nfs_init_stats_time(void)
-{
-	now(&nfs_stats_time);
-	fsal_stats_time = v3_full_stats_time
-			= v4_full_stats_time
-			= auth_stats_time
-			= clnt_allops_stats_time
-			= nfs_stats_time;
 }
 
 /**