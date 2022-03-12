41955f6f37617a9326d54850af1ec5f7f6777691
Frank S. Filz <ffilzlnx@mindspring.com>
2022-03-11 16:59:49 -0800

    Based on src/cmake/COPYING-CMAKE-SCRIPTS add BSD-3-Clause license to cmake files
    These files did not have a license specified, based on the text
    in src/cmake/COPYING-CMAKE-SCRIPTS, add a BSD 3 Clause license to
    each file,
    
    Change-Id: Id195102fd4e62400bcc74444a9e1f739ef1e3c2f
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/cmake/modules/FindReadline.cmake
+++ src/cmake/modules/FindReadline.cmake
@@ -1,3 +1,24 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#-------------------------------------------------------------------------------
+#
+# Copyright Panasas, 2012
+# Contributor: Jim Lieb <jlieb@panasas.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#-------------------------------------------------------------------------------
 FIND_PATH(READLINE_INCLUDE_DIR readline/readline.h)
 FIND_LIBRARY(READLINE_LIBRARY NAMES readline)
 
