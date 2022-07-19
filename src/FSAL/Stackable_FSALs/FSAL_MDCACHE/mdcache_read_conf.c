efb447b6aa4b1c190fd5ec6b89764e23813a802e
Frank S. Filz <ffilzlnx@mindspring.com>
2022-07-19 16:12:53 -0700

    MDCACHE: Use new altname for config block to correct use of either name
    The old mechanism was causing the mdcache parameters to be re-initialized
    after reading the cacheinode block. Now we correctly handle the block
    whichever name is used.
    
    Change-Id: Ifc750a49490a57001f074940e4c71f2a1b1a9229
    Signed-off-by: Frank S. Filz <ffilzlnx@mindspring.com>

--- src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_read_conf.c
+++ src/FSAL/Stackable_FSALs/FSAL_MDCACHE/mdcache_read_conf.c
@@ -101,18 +101,10 @@ static void *mdcache_param_init(void *link_mem, void *self_struct)
 		return NULL;
 }
 
-struct config_block cache_inode_param_blk = {
-	.dbus_interface_name = "org.ganesha.nfsd.config.cache_inode",
-	.blk_desc.name = "CacheInode",
-	.blk_desc.type = CONFIG_BLOCK,
-	.blk_desc.u.blk.init = mdcache_param_init,
-	.blk_desc.u.blk.params = mdcache_params,
-	.blk_desc.u.blk.commit = noop_conf_commit
-};
-
 struct config_block mdcache_param_blk = {
 	.dbus_interface_name = "org.ganesha.nfsd.config.cache_inode",
 	.blk_desc.name = "MDCACHE",
+	.blk_desc.altname = "CacheInode",
 	.blk_desc.type = CONFIG_BLOCK,
 	.blk_desc.u.blk.init = mdcache_param_init,
 	.blk_desc.u.blk.params = mdcache_params,
@@ -122,17 +114,6 @@ struct config_block mdcache_param_blk = {
 int mdcache_set_param_from_conf(config_file_t parse_tree,
 				struct config_error_type *err_type)
 {
-	(void) load_config_from_parse(parse_tree,
-				      &cache_inode_param_blk,
-				      NULL,
-				      true,
-				      err_type);
-	if (!config_error_is_harmless(err_type)) {
-		LogCrit(COMPONENT_INIT,
-			"Error while parsing CACHEINODE specific configuration");
-		return -1;
-	}
-
 	(void) load_config_from_parse(parse_tree,
 				      &mdcache_param_blk,
 				      NULL,
