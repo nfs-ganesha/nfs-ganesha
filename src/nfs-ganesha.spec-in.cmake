9ba3fd15fb5ca28caf7ded01983bd15231e186ca
Kaleb S. KEITHLEY <kkeithle@redhat.com>
2023-08-02 08:39:13 -0400

    revert IBM-specific winbind magic
    IMO this is something that should be in their downstream dist-git,
    not here in upstream.
    
    Change-Id: I5f1793e4ea0b0580216b493d6f2c854bbfc27e9b
    Signed-off-by: Kaleb S. KEITHLEY <kkeithle@redhat.com>

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -535,13 +535,7 @@ cmake .	-DCMAKE_BUILD_TYPE=Debug			\
 	-D_MSPAC_SUPPORT=%{use_mspac_support}		\
 	-DSANITIZE_ADDRESS=%{use_sanitize_address}	\
 %if %{with jemalloc}
-	-DALLOCATOR=jemalloc 				\
-%endif
-%if ( %{with gpfs} && %{with mspac_support} )
-	-DWBCLIENT_INCLUDE_DIR=/usr/lpp/mmfs/include/samba-4.0		\
-	-DWBCLIENT_LIBRARIES=/usr/lpp/mmfs/lib64/libwbclient.so.0	\
-	-DCMAKE_INSTALL_RPATH=/usr/lpp/mmfs/lib64/			\
-	-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+	-DALLOCATOR=jemalloc
 %endif
 %if %{with tcmalloc}
 	-DALLOCATOR=tcmalloc
