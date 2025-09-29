27e728f18e840d1395dffb99b718788f28e31828
Dillon Peng <pengcz.nwpu@gmail.com>
2025-09-29 16:10:47 +0800

    Fix GLUSTER FSAL resource release order issue
       Adjust the order of resource release in handle_release to prevent
       null pointer access when closing file descriptors.

--- src/FSAL/FSAL_GLUSTER/handle.c
+++ src/FSAL/FSAL_GLUSTER/handle.c
@@ -64,8 +64,6 @@ static void handle_release(struct fsal_obj_handle *obj_hdl)
 	now(&s_time);
 #endif
 
-	fsal_obj_handle_fini(&objhandle->handle, true);
-
 	if (obj_hdl->type == REGULAR_FILE) {
 		fsal_status_t st;
 
@@ -78,6 +76,8 @@ static void handle_release(struct fsal_obj_handle *obj_hdl)
 				st.minor);
 		}
 	}
+
+	fsal_obj_handle_fini(&objhandle->handle, true);
 
 	if (my_fd->creds.caller_garray) {
 		gsh_free(my_fd->creds.caller_garray);
