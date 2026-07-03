b7e25ed3f1b18b674e826943c3af8dc94a43d1ed
Marcus Watts <mwatts@redhat.com>
2026-07-03 00:18:03 -0400

    packaging : jumbo fixups for el9,10
    = src/nfs-ganesha.spec-in.cmake =
    
    buildrequires python3-setuptools
      Install python3-setuptools on el 9, 10.
    don't install bits not built
      If using the system libntirpc, the file libntirpcmonitoring.so
      will not be built and cannot be installed.
    do not remove __pycache__
      Removing __pycache__ at buld time won't work.
      It will be built by an automatic rule at %install time which
      happens after %build time.
    
      Secondly, removing __pycache__ is Bad.  What will happen then is:
      the first time root runs across this python module, a .pyc file will
      be made.  Since that .pyc file isn't being managed, it will interfere
      with later attempts to remove or update the package.
    must install /Ganesha/ bits for el9,10
      They get installed by cmake.  And I believe at least some of the bits
      are needed by some of the nfs ganesha utilities.
    Set WITH_PYTHON3 to ensure cmake will use the right
      python to put files where rpmbuild expects to find them.
      This matters because ci-tests installs python 3.11 along
      with python 3.9.
    Correct usage of @BCOND_LIZARDFS@ and %{use_fsal_saunafs}.
    Use @BCOND_LEGACY_PYTHON_INSTALL@ to get bcond_with/without or
      bcond according to version of rpmbuild.
    Use NTIRPC_COMMENT_IF_EQUAL to comment out duplciated libntirpc.so.
    Package /Ganeha/* files for utils for fc34+ too.
    "libacl-devel" is a core build requirement requirement, it's not
      just for ceph | gluster.
    In fc43+, python3-build is replaced by python3-build+virtualenv .
    
    = src/CMakeLists.txt =
    
    Update cmake minimum reuired to 3.10.  Remove support for <2.6.
      Also document which versions this was teted with.
    
    Default LizardFS off.
    
    Fix CMP0175 errors.  Corrects add_custom_command usage.
    
    Resurrect USE_LEGACY_PYTHON_INSTALL option, but automagic it
      to when python version < 3.12.  This can be used for fc37
      which has python 3.11, but requires without legacy_pythoh_install.
    
    Logic to set NTIRPC_COMMENT_IF_EQUAL_ELSE_PERCENT acording to whether
      NTIRPC_VERSION_EMBED == NTIRPC_ABI_EMBED.  This will
      expand out to suppress duplicating libntirpc.so
      if the versions are the same (which is currently always true).
    
    Detect version of rpmbuild and emit %bcond_with / %bcond_without
      for older rpmbuild that doesn't have %bcond.  This matters
      for fc34 and el9.
    
    = src/scripts/gpfs-epoch/CMakeLists.txt =
    
    install gpfs-epoch in el10
      Ganesha wants to put gpfs-epoch, a python script, into
      /usr/libexec/ganesha (or some logical variation thereof).
    
      In el10, we have python 3.12, which removes support for antique install logic.
      The replacement (python3 -m installer) lacks support for setting bindir
      and always puts scripts into /usr/bin.  Rather than fighting with
      the python installer, this commit just installs the script directly.
    
    = src/scripts/ganesha-top/CMakeLists.txt =
    
    fix ganesha-top install on el10
      This change works around antique python install logic
      for ganesha-top.
    
      "mkdir -p build/lib" invites the python installer to recursively search
      for all .py files, which results in really weird behavior when it finds
      build/lib.  We don't need this, so get rid of it.  The goal is that
      the wheel that comes out of this should not have setup.py or build/lib,
      or any variation thereof.
    
      package_dir isn't needed here, so get rid of it.
    
    Change-Id: I943ed9958bb1326acc21f3354f8d8ced297ded0a
    Signed-off-by: Marcus Watts <mwatts@redhat.com>

--- src/nfs-ganesha.spec-in.cmake
+++ src/nfs-ganesha.spec-in.cmake
@@ -51,6 +51,9 @@ Requires: openSUSE-release
 @BCOND_KVSFS@ kvsfs
 %global use_fsal_kvsfs %{on_off_switch kvsfs}
 
+@BCOND_LIZARDFS@ lizardfs
+%global use_fsal_lizardfs %{on_off_switch lizardfs}
+
 @BCOND_RDMA@ rdma
 %global use_rdma %{on_off_switch rdma}
 
@@ -62,7 +65,7 @@ Requires: openSUSE-release
 %global use_9P %{on_off_switch 9P}
 
 @BCOND_SAUNAFS@ saunafs
-%global use_saunafs %{on_off_switch saunafs}
+%global use_fsal_saunafs %{on_off_switch saunafs}
 
 @BCOND_JEMALLOC@ jemalloc
 
@@ -119,7 +122,7 @@ Requires: openSUSE-release
 @BCOND_GNUTLS@ gnutls
 %global use_gnutls %{on_off_switch gnutls}
 
-@BCOND_LEGACY_PYTHON_INSTALL@ legacy_python_install
+@BCOND_LEGACY_PYTHON_INSTALL@
 %global use_legacy_python_install %{on_off_switch legacy_python_install}
 
 @BCOND_NFSIDMAP@ nfsidmap
@@ -153,6 +156,7 @@ BuildRequires:	pkgconfig
 BuildRequires:	userspace-rcu-devel
 BuildRequires:	krb5-devel
 BuildRequires:	openssl-devel
+BuildRequires:	libacl-devel
 %if %{with rados_recov} || %{with rados_urls}
 BuildRequires: librados-devel >= 0.61
 %endif
@@ -313,8 +317,15 @@ Requires:	python3-dbus
 %endif
 %if ( ! 0%{?with_legacy_python_install} )
 BuildRequires:  python3-wheel
+%if ( 0%{?fedora} >= 43 )
+BuildRequires:  python3-build+virtualenv
+%else
 BuildRequires:  python3-build
+%endif
 BuildRequires:  python3-installer
+%if ( 0%{?rhel} > 9 )
+BuildRequires:  python3-setuptools
+%endif
 %endif
 %endif
 
@@ -421,7 +432,6 @@ Summary: The NFS-GANESHA CephFS FSAL
 Group: Applications/System
 Requires:	nfs-ganesha = %{version}-%{release}
 BuildRequires:	libcephfs-devel >= 10.2.0
-BuildRequires:	libacl-devel
 
 %description ceph
 This package contains a FSAL shared object to
@@ -490,7 +500,7 @@ Summary: The NFS-GANESHA GLUSTER FSAL
 Group: Applications/System
 Requires:	nfs-ganesha = %{version}-%{release}
 BuildRequires:        libgfapi-devel >= 7.0
-BuildRequires:        libattr-devel, libacl-devel
+BuildRequires:        libattr-devel
 
 %description gluster
 This package contains a FSAL shared object to
@@ -581,6 +591,7 @@ Development headers and auxiliary files for developing with %{name}.
 %build
 cmake3 .	-DCMAKE_BUILD_TYPE=Debug			\
 	-DBUILD_CONFIG=rpmbuild				\
+	-DWITH_PYTHON3=%{python3_version}		\
 	-DUSE_FSAL_NULL=%{use_fsal_null}		\
 	-DUSE_FSAL_MEM=%{use_fsal_mem}			\
 	-DUSE_FSAL_XFS=%{use_fsal_xfs}			\
@@ -589,6 +600,7 @@ cmake3 .	-DCMAKE_BUILD_TYPE=Debug			\
 	-DUSE_FSAL_RGW=%{use_fsal_rgw}			\
 	-DUSE_FSAL_GPFS=%{use_fsal_gpfs}		\
 	-DUSE_FSAL_KVSFS=%{use_fsal_kvsfs}		\
+	-DUSE_FSAL_LIZARDFS=%{use_fsal_lizardfs}	\
 	-DUSE_FSAL_GLUSTER=%{use_fsal_gluster}		\
 	-DUSE_FSAL_SAUNAFS=%{use_fsal_saunafs}		\
 	-DUSE_SYSTEM_NTIRPC=%{use_system_ntirpc}	\
@@ -724,10 +736,8 @@ rm -f %{buildroot}/%{python_sitelib}/__init__.*
 rm -rf %{buildroot}/%{python3_sitelib}/gpfs*
 rm -rf %{buildroot}/%{python3_sitelib}/ganeshactl*
 rm -f %{buildroot}/%{python3_sitelib}/__init__.*
-rm -rf %{buildroot}/%{python3_sitelib}/__pycache__
 rm -f %{buildroot}/%{python3_sitelib}/Ganesha/__init__.*
 rm -f %{buildroot}/%{python3_sitelib}/Ganesha/QtUI/__init__.*
-rm -rf %{buildroot}/%{python3_sitelib}/Ganesha/QtUI/__pycache__
 %endif
 
 %post
@@ -836,7 +846,9 @@ exit 0
 %if %{with monitoring}
 %files monitoring
 %{_libdir}/libganesha_monitoring*
+%if ! %{with system_ntirpc}
 %{_libdir}/libntirpcmonitoring*
+%endif
 %endif
 
 %files vfs
@@ -938,7 +950,7 @@ exit 0
 %if ! %{with system_ntirpc}
 %files -n libntirpc
 %{_libdir}/libntirpc.so.@NTIRPC_VERSION_EMBED@
-%{_libdir}/libntirpc.so.@NTIRPC_ABI_EMBED@
+@NTIRPC_COMMENT_IF_EQUAL_ELSE_PERCENT@{_libdir}/libntirpc.so.@NTIRPC_ABI_EMBED@
 %{_libdir}/libntirpc.so
 %{!?_licensedir:%global license %%doc}
 %license libntirpc/COPYING
@@ -978,6 +990,10 @@ exit 0
 %if ( 0%{?rhel} && 0%{?rhel} < 8 )
 %{python_sitelib}/Ganesha/*
 %{python_sitelib}/ganeshactl-*-info
+%endif
+%if ( 0%{?rhel} > 8 ||  0%{?fedora} >= 34 )
+%{python3_sitelib}/Ganesha/*
+%exclude %{python3_sitelib}/ganesha*-*-info
 %endif
 %if %{with gui_utils}
 %{_bindir}/ganesha-admin
